d8ac52f33d719a6e8316da2831e1e438d49451a2
René Gründig <rene.gruendig@gandalan.de>
2019-05-13 14:34:51 +0200

    Pfade für Buildprozess korrigiert

--- UpdateVersionInfo.ps1
+++ UpdateVersionInfo.ps1
@@ -8,7 +8,7 @@ $NewVersion = [regex]::matches($Env:Release_ReleaseName,$VersionRegex)
 Write-Host "Updating version of the application to the build version: " $NewVersion
 
 #AssemblyProjectInfo:
-$file = ".\_gandalan_idas-client-libs\AssemblyProjectInfo.cs" 
+$file = ".\AssemblyProjectInfo.cs" 
 $filecontent = Get-Content($file)
 attrib $file -r
 # Search in the "AssemblyProjectInfo.cs" file items that matches the version regex and replace them with the
@@ -18,7 +18,7 @@ $filecontent -replace $VersionRegex, $NewVersion | Out-File $file
 #GDL.IDAS.WebApi.Client.nuspec
 $VersionRegex = "\d+\.\d+.\d+\.\d+"
 $NewVersion = [regex]::matches($Env:Release_ReleaseName,$VersionRegex)
-$file = ".\_gandalan_idas-client-libs\Gandalan.IDAS.WebApi.Client\GDL.IDAS.WebApi.Client.nuspec" 
+$file = ".\Gandalan.IDAS.WebApi.Client\GDL.IDAS.WebApi.Client.nuspec" 
 $filecontent = Get-Content($file)
 attrib $file -r
 # Search in the "GDL.IDAS.WebApi.Client.nuspec" file items that matches the version regex and replace them with the
@@ -28,7 +28,7 @@ $filecontent -replace $VersionRegex, $NewVersion | Out-File $file
 #GDL.IDAS.WebApi.Data.nuspec
 $VersionRegex = "\d+\.\d+.\d+\.\d+"
 $NewVersion = [regex]::matches($Env:Release_ReleaseName,$VersionRegex)
-$file = ".\_gandalan_idas-client-libs\Gandalan.IDAS.WebApi.Data\GDL.IDAS.WebApi.Data.nuspec" 
+$file = ".\Gandalan.IDAS.WebApi.Data\GDL.IDAS.WebApi.Data.nuspec" 
 $filecontent = Get-Content($file)
 attrib $file -r
 # Search in the "GDL.IDAS.WebApi.Data.nuspec" file items that matches the version regex and replace them with the
