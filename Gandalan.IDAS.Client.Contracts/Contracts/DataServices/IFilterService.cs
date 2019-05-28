5edcd60c1b40aefdf0a86d6a561d53fe0c78861d
Denis König <denis.koenig@gandalan.de>
2019-05-28 09:01:50 +0200

    IFIlterService added

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IFilterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.DTO;
+using Gandalan.IDAS.WebApi.DTO.DTOs.Filter;
+
+namespace Gandalan.Client.Common.Contracts.DataServices
+{
+    public interface IFilterService
+    {
+        Task<FilterItemDTO[]> GetAllAsync();
+        Task<FilterItemDTO> GetFilterItemAsync(Guid id);
+        Task<FilterItemDTO[]> GetFilterItemsByContextAsync(string context);
+        Task SaveAsync(string context, string serializedFilterSetting);
+        Task DeleteAsync(Guid id);
+    }
+}
