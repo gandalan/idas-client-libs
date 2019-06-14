e29a63ce073ad04674046ac1d6b44182a89fb88d
Konstantin Tümmler <konstantin@tuemmler.org>
2019-06-14 09:03:12 +0200

    BelegArt Kopieren

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IBelegArtService.cs
@@ -0,0 +1,14 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.Client.Common.Contracts.DataServices
+{
+    public interface IBelegArtService
+    {
+        Task BelegKopieren(Guid bguid, string neueBelegArt, bool saldenKopieren = false);   
+    }
+}
