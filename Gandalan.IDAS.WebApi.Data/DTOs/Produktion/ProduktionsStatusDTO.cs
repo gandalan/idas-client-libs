1d7c41c97eb90bd6c97ae4d74be4904dbc3671a4
Philipp Reif <philipp.reif@gandalan.de>
2019-07-05 08:33:36 +0200

    added DTOs for production status

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Produktion/ProduktionsStatusDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    public class ProduktionsStatusDTO
+    {
+        public Guid ProduktionsStatusGuid { get; set; }
+        public Guid BelegPositionGuid { get; set; }
+        public DateTime Erstellt { get; set; }
+        public string Ersteller { get; set; }
+
+        public Guid SerieGuid { get; set; }
+        public string SerieBezeichnung { get; set; }
+
+        public ProduktionsStatiDTO AktuellerStatus { get; set; }
+
+        public List<ProduktionsStatusHistorieDTO> Historie { get; set; }
+    }
+}
