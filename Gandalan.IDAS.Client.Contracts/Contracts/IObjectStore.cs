64cb9f7c163080349eca6ebb950d52747f7b8613
Philipp Reif <philipp.reif@gandalan.de>
2019-07-08 11:51:37 +0200

    Fixed parameters in IObjectStore filter func

--- Gandalan.IDAS.Client.Contracts/Contracts/IObjectStore.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/IObjectStore.cs
@@ -7,7 +7,7 @@ namespace Gandalan.Client.Contracts
     public interface IObjectStore<T> where T : new()
     {
         T GetAll();
-        T Get(Func<bool, T> filter);
+        T Get(Func<T, bool> filter);
         T Put(T item);
         void Remove(T item);
     }
