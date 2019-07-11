f23e0d1b8b70d8796bb4e19bfc2983d3b6c1c9e9
René Gründig <rene.gruendig@gandalan.de>
2019-07-11 11:16:01 +0200

    AV DTOs und Webroutine hinzugefügt

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/AV/SerieDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    public class SerieDTO
+    {
+        public Guid SerieGuid { get; set; }
+        public string Name { get; set; }
+        public DateTime Start { get; set; } = new DateTime(2000, 1, 1);
+        public DateTime Ende { get; set; } = new DateTime(2099, 12, 31);
+        public bool StaendigeSerie { get; set; }
+        public bool ProduktionFreigegeben { get; set; }
+        public bool UnterlagenGedruckt { get; set; }
+        public IList<Guid> Elemente { get; set; } = new List<Guid>();
+    }
+}
