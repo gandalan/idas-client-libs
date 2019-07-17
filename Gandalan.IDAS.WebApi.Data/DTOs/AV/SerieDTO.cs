9a403f98a45071e034d95a0634e61d6247933cde
René Gründig <rene.gruendig@gandalan.de>
2019-07-17 15:24:51 +0200

    Renaming Elemente to AVBElegPositionen in SerieDTO

--- Gandalan.IDAS.WebApi.Data/DTOs/AV/SerieDTO.cs
+++ Gandalan.IDAS.WebApi.Data/DTOs/AV/SerieDTO.cs
@@ -13,6 +13,6 @@ namespace Gandalan.IDAS.WebApi.DTO
         public bool StaendigeSerie { get; set; }
         public bool ProduktionFreigegeben { get; set; }
         public bool UnterlagenGedruckt { get; set; }
-        public IList<Guid> Elemente { get; set; } = new List<Guid>();
+        public IList<Guid> AVBElegPositionen { get; set; } = new List<Guid>();
     }
 }
