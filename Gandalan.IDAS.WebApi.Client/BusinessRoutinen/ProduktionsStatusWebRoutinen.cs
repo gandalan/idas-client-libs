699573ef35d6c11e867a0d86b7f6544c0bad1e5f
René Gründig <rene.gruendig@gandalan.de>
2019-07-19 17:09:29 +0200

    ProduktionsStatusWebRoutinen hinzugefügt.

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/ProduktionsStatusWebRoutinen.cs
@@ -0,0 +1,44 @@
+﻿using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class ProduktionsStatusWebRoutinen : WebRoutinenBase
+    {
+        public ProduktionsStatusWebRoutinen(WebApiSettings settings) : base(settings)
+        {
+        }
+ 
+        public ProduktionsStatusDTO GetProduktionsStatus(Guid guid)
+        {
+            if (Login())
+            {
+                return Get<ProduktionsStatusDTO>("ProduktionsStatus/" + guid.ToString());
+            }
+            return null;
+        }
+
+        public string SaveProduktionsStatus(ProduktionsStatusDTO status)
+        {
+            if (Login())
+            {
+                return Put<string>("ProduktionsStatus", status);
+            }
+            return "Not logged in";
+        }
+
+        public async Task<ProduktionsStatusDTO> GetSerieAsync(Guid guid)
+        {
+            return await Task.Run(() => GetProduktionsStatus(guid));
+        }
+
+        public async Task<string> SaveSerieAsync(ProduktionsStatusDTO status)
+        {
+            return await Task.Run(() => SaveProduktionsStatus(status));
+        }
+    }
+}
