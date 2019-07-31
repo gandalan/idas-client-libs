3d4eecba289883498733c89d58251b5e4f6cee4f
Philipp Reif <philipp.reif@gandalan.de>
2019-07-31 09:08:16 +0200

    Introduce Clear methode to ICache

--- Gandalan.IDAS.Client.Contracts/Cache/ICache.cs
+++ Gandalan.IDAS.Client.Contracts/Cache/ICache.cs
@@ -7,5 +7,6 @@ namespace Gandalan.Client.Contracts.Cache
     {
         void PutItem(string identifier, object data);
         Task<T> GetItem<T>(string identifier, DateTime? expirationDate, Func<Task<T>> retrieveDataIfCacheMiss);
+        void Clear(string identifier);
     }
 }
