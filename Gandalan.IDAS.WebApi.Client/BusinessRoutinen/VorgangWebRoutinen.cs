ec55fa6f5b7109a5d0d29b9c087fe7986279946c
René Gründig <rene.gruendig@gandalan.de>
2019-09-16 10:21:47 +0200

    VorgangWebRoutinen Laden der Vorgangsliste um alle möglichen Parameter erweitert.

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangWebRoutinen.cs
@@ -37,6 +37,15 @@ namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
             }
             return null;
         }
+        
+        public VorgangListItemDTO[] LadeVorgangsListe(int jahr, string status, DateTime changedSince, string art = "", bool includeArchive = false, bool includeOthersData = false, string search = "")
+        {
+            if (Login())
+            {
+                return Get<VorgangListItemDTO[]>($"Vorgang/?status={status}&jahr={jahr}&changedSince={changedSince.ToString("yyyy-MM-ddTHH:mm:ss")}&art={art}&includeArchive={includeArchive}&includeOthersData={includeOthersData}&search={search}");
+            }
+            return null;
+        }
 
         public VorgangListItemDTO[] LadeVorgangsListe(Guid kundeGuid)
         {
@@ -124,6 +133,11 @@ namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
         public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(string status, int jahr, DateTime changedSince)
         {
             return await Task<VorgangListItemDTO[]>.Run(() => { return LadeVorgangsListe(status, jahr, changedSince); });
+        }
+
+        public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(int jahr, string status, DateTime changedSince, string art = "", bool includeArchive = false, bool includeOthersData = false, string search = "")
+        {
+            return await Task<VorgangListItemDTO[]>.Run(() => { return LadeVorgangsListe(jahr, status, changedSince, art, includeArchive, includeOthersData, search);});
         }
 
         public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(Guid kundeGuid)
