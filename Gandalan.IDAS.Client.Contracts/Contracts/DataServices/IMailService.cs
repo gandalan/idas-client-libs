6b48778d996f827dcd18110dbac71cdf037c3029
Philipp Reif <philipp.reif@gandalan.de>
2019-10-11 09:05:54 +0200

    Added IEtikettenDruckService, IMailService, ISaldenService

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IMailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.Client.Contracts.DataServices
+{
+    public interface IMailService
+    {
+        void SendMail(string from, string to, string content, List<string> attachments = null);
+    }
+}
