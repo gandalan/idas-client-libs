2ad20ab5342c11fe3ddc677ffd4b2e45f83011d8
Denis König <denis.koenig@gandalan.de>
2019-10-22 14:20:42 +0200

    IBestellmassRechner changed

--- Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IBestellmassRechner.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IBestellmassRechner.cs
@@ -4,26 +4,14 @@ using Gandalan.Client.Contracts.Lookups;
 
 namespace Gandalan.Client.Contracts
 {
-    public interface IBestellmassRechner : IFeldEditor<IBestellmassRechnerResult, IBestellmassRechnerParams>
+    public interface IBestellmassRechner : IFeldEditor<IBestellmassRechnerResult, BelegPositionDTO>
     {
-    }
-
-    public interface IBestellmassRechnerParams
-    {
-        int LichtesBreitenMass { get; }
-        int LichtesHoehenMass { get; }
     }
 
     public interface IBestellmassRechnerResult
     {
         int BerechnetesHoehenMass { get; }
         int BerechnetesBreitenMass { get; }
-    }
-
-    public class BestellmassRechnerParams : IBestellmassRechnerParams
-    {
-        public int LichtesBreitenMass { get; set; }
-        public int LichtesHoehenMass { get; set; }
     }
 
     public class BestellmassRechnerResult : IBestellmassRechnerResult
