f38fce33487b795e5a87e1f82e32134aaa66312e
Denis König <denis.koenig@gandalan.de>
2019-10-22 12:28:54 +0200

    IBestellmassRechner added

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IBestellmassRechner.cs
@@ -0,0 +1,40 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using Gandalan.Client.Contracts.Lookups;
+
+namespace Gandalan.Client.Contracts
+{
+    public interface IBestellmassRechner : IFeldEditor<IBestellmassRechnerResult, IBestellmassRechnerParams>
+    {
+    }
+
+    public interface IBestellmassRechnerParams
+    {
+        int LichtesBreitenMass { get; }
+        int LichtesHoehenMass { get; }
+    }
+
+    public interface IBestellmassRechnerResult
+    {
+        int BerechnetesHoehenMass { get; }
+        int BerechnetesBreitenMass { get; }
+    }
+
+    public class BestellmassRechnerParams : IBestellmassRechnerParams
+    {
+        public int LichtesBreitenMass { get; set; }
+        public int LichtesHoehenMass { get; set; }
+    }
+
+    public class BestellmassRechnerResult : IBestellmassRechnerResult
+    {        
+        public int BerechnetesHoehenMass { get; set; }
+        public int BerechnetesBreitenMass { get; set; }
+
+        public BestellmassRechnerResult(int berechnetesLichtesHoehenMass, int berechnetesLichtesBreitenMass)
+        {
+            BerechnetesHoehenMass = berechnetesLichtesHoehenMass;
+            BerechnetesBreitenMass = berechnetesLichtesBreitenMass;
+        }
+    }
+}
