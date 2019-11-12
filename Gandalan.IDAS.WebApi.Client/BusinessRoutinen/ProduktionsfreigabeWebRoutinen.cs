be283cf5d9f398370f967ba683010ebad1631d92
René Gründig <rene.gruendig@gandalan.de>
2019-11-12 14:33:03 +0100

    ProduktionsfreigabeWebRoutinen hinzugefügt

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/ProduktionsfreigabeWebRoutinen.cs
@@ -0,0 +1,30 @@
+﻿using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class ProduktionsfreigabeWebRoutinen : WebRoutinenBase
+    {
+        public ProduktionsfreigabeWebRoutinen(WebApiSettings settings) : base(settings)
+        {
+        }
+
+        public VorgangDTO AddProduktionsfreigabe(ProduktionsfreigabeDTO dto)
+        {
+            if (Login())
+            {
+                return Put<VorgangDTO>("Produktionsfreigabe", dto);
+            }
+            return null;
+        }
+
+        public async Task<VorgangDTO> AddProduktionsfreigabeAsync(ProduktionsfreigabeDTO dto)
+        {
+            return await Task.Run(() => AddProduktionsfreigabe(dto));
+        }
+    }
+}
