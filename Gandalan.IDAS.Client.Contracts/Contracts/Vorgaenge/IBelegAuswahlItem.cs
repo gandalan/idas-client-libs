5274d4a5d99dda5800d426368c52e6d7020f2793
Denis König <denis.koenig@gandalan.de>
2019-11-21 14:33:08 +0100

    IBelegItemCommand implemented

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IBelegAuswahlItem.cs
@@ -0,0 +1,19 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.Vorgaenge
+{
+    public interface IBelegAuswahlItem
+    {
+        BelegDTO Beleg { get; }
+        VorgangDTO Vorgang { get; }
+
+        string Titel { get; }
+        DateTime ErstellDatum { get; }
+        IList<BelegSaldoDTO> Salden { get; }
+        IList<BelegPositionDTO> Positionen { get; }
+        bool IstKopiert { get; }
+    }
+}
---
+++ Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IBelegItemCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.Vorgaenge
+{
+    public interface IBelegItemCommand : ICommand
+    {
+        void CanExecute(IBelegAuswahlItem parameter);
+        void Execute(IBelegAuswahlItem parameter);
+        string Caption { get; }
+        string Tooltip { get; }
+    }
+}
