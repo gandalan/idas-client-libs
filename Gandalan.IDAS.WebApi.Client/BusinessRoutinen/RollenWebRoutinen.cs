57698b951abe9a0fa24accfbfe4e927ffaf7fabf
Denis König <denis.koenig@gandalan.de>
2019-11-27 15:33:01 +0100

    RollenWebRoutinen added

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/RollenWebRoutinen.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class RollenWebRoutinen : WebRoutinenBase
+    {
+        public RollenWebRoutinen(WebApiSettings settings) : base(settings)
+        {
+        }
+
+        public RolleDTO[] GetAll()
+        {
+            if (Login())
+            {
+                return Get<RolleDTO[]>("Rollen");
+            }
+            return null;
+        }
+
+        public async Task<RolleDTO[]> GetAllAsync()
+        {
+            return await Task.Run(() => GetAll());
+        }
+
+    }
+}
