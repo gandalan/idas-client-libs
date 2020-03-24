f191b23e657932a33e7ad4b9ccf1bcf20f321fd3
Konstantin Tümmler <konstantin@tuemmler.org>
2020-03-24 07:29:26 +0100

    MenuManager

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/UIServices/IMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using Gandalan.Plugins.Common.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Gandalan.Client.Contracts.UIServices
+{
+    public interface IMenuManager
+    {
+        void AddNavigationItem(INavigationItem data);
+        List<INavigationGroup> GetNavigationGroups();
+        event EventHandler NavigationItemsChanged;
+
+        void SetGroupOrder(string groupName, int order);
+    }
+}
