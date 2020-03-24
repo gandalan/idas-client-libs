f9ce52dc9efb5d5c20b6a0c2426828086d977e6b
Konstantin Tümmler <konstantin@tuemmler.org>
2020-03-24 07:29:26 +0100

    MenuManager

---
+++ Gandalan.IDAS.Client.Contracts/Menu/IMenuGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.Plugins.Common.Contracts
+{
+    public interface IMenuGroup
+    {
+        string Caption { get; set; }
+        IList<IMenuItem> Items { get; set; }
+        int Order { get; set; }
+    }
+}
