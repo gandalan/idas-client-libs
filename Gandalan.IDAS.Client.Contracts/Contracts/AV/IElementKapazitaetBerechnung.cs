eaf3e3d2dbdfcf3b674c2bd58aa8692e991362f6
Denis König <denis.koenig@gandalan.de>
2020-03-25 09:42:36 +0100

    IElementKapazitaetBerechnung public

--- Gandalan.IDAS.Client.Contracts/Contracts/AV/IElementKapazitaetBerechnung.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/AV/IElementKapazitaetBerechnung.cs
@@ -3,9 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
+namespace Gandalan.IDAS.Client.Contracts.AV
 {
-    interface IElementKapazitaetBerechnung
+    public interface IElementKapazitaetBerechnung
     {
         decimal GetKapazitaetsBedarf(BelegPositionDTO belegPosition, DateTime datum, SerieDTO serie);
     }
