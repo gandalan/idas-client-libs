dec8689284cda7e5f2f4f3a6c73e018377aba1df
René Gründig <rene.gruendig@gandalan.de>
2020-03-27 09:34:12 +0100

    ITagesKapazitaetBerechnung hinzugefügt

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/AV/TagesKapazitaetInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.Client.Contracts.AV
+{
+    public class TagesKapazitaetInfo
+    {
+        decimal GesamtKapazitaet { get; set; }
+        decimal BenoetigteKapazitaet { get; set; }
+    }
+}
