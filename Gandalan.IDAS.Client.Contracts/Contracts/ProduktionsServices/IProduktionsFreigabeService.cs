d1fdadaa18628185026dc991abf40c9ded75cb81
Konstantin Tümmler <konstantin@tuemmler.org>
2020-04-20 09:42:33 +0200

    IProduktionsFreigabeService contract

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/IProduktionsFreigabeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.Client.Contracts.ProduktionsServices
+{
+	public interface IProduktionsFreigabeService
+	{
+		void BelegFreigabe(BelegDTO belegDTO);
+		void PositionsFreigabe(BelegPositionDTO pos);
+	}
+}
