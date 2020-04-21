fe986c57df34b03ca767f8b502bfbfca2a61f54f
René Gründig <rene.gruendig@gandalan.de>
2020-04-21 11:35:06 +0200

    IMaterialListeDisplay hinzugefügt.

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/UIServices/IMaterialListeDisplay.cs
@@ -0,0 +1,12 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.UIServices
+{
+    public interface IMaterialListeDisplay
+    {
+        void Execute(BelegPositionAVDTO dto);
+    }
+}
