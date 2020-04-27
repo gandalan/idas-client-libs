b90b2e983c2bc6c38cf600d78ae9060866927bad
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2020-04-27 16:02:23 +0200

    Anpassungen für Einbindung profilbezogener Sägemaßkorrekturen (Work Item 3668)

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegemassKorrekturService.cs
@@ -0,0 +1,38 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.ProduktionsServices
+{
+    /// <summary>
+    /// Abrufen von profilabhängigen Sägemaßkorrekturen
+    /// </summary>
+    public interface ISaegemassKorrekturService
+    {
+        /// <summary>
+        /// Ruft das (summierte) Korrekturmaß für ein MaterialbedarfDTO, abhängig von Winkel links/rechts, ab.
+        /// </summary>
+        /// <param name="korrekturSatz">Korrektursatz der genutzt werden soll</param>
+        /// <param name="material">MaterialbedarfDTO, für das das Korrekturmaß abgerufen werden soll</param>
+        /// <returns></returns>
+        float GetKorrekturmass(string korrekturSatz, MaterialbedarfDTO material);
+
+        /// <summary>
+        /// Ruft das Korrekturmaß für ein Profil beim angegebenen Winkel ab
+        /// </summary>
+        /// <param name="korrekturSatz">Korrektursatz der genutzt werden soll</param>
+        /// <param name="katalogNummer">Katalognummer des Profils</param>
+        /// <param name="winkel">Winkel, für den das Korrekturmaß abgerufen werden soll</param>
+        /// <returns></returns>
+        float GetKorrekturmass(string korrekturSatz, string katalogNummer, int winkel);
+
+        /// <summary>
+        /// Liefert das korrigierte Zuschnittsmaß für ein MaterialbedarfDTO (ZuschnittLaenge + Korrekturmaß)
+        /// </summary>
+        /// <param name="korrekturSatz">Korrektursatz der genutzt werden soll</param>
+        /// <param name="material">MaterialbedarfDTO, für das die korrigierte Zuschnittslänge abgerufen werden soll</param>
+        /// <returns></returns>
+        float GetZuschnittKorrigiert(string korrekturSatz, MaterialbedarfDTO material);
+    }
+}
---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Produktion/SaegemassKorrekturSatzDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    /// <summary>
+    /// DTO für die Verwaltung der Sägemaßkorrekturen
+    /// </summary>
+    public class SaegemassKorrekturSatzDTO
+    {
+        /// <summary>
+        /// Bezeichnung des Sägemaßkorrektursatzes
+        /// </summary>
+        public string Bezeichnung { get; set; } 
+
+        /// <summary>
+        /// Liste mit profilbezogenen Sägemaßkorrekturen
+        /// </summary>
+        public IList<SaegemassKorrekturDTO> SaegemassKorrekturen { get; set; }
+
+    }
+}
