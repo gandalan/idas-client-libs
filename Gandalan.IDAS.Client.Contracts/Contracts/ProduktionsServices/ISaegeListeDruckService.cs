3bd99d2cc66e3dfa70f47e8160ee978a9903b012
Konstantin Tümmler <konstantin@tuemmler.org>
2020-04-28 15:40:20 +0200

    added Task return value

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegeListeDruckService.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegeListeDruckService.cs
@@ -1,12 +1,13 @@
 using Gandalan.IDAS.Client.Contracts.Contracts.ReportData;
 using Gandalan.IDAS.WebApi.DTO;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Gandalan.Client.Contracts.ProduktionsServices
 {
     public interface ISaegeListeDruckService {
-        void Prepare(List<BelegPositionAVDTO> reportData);
-        void Prepare(List<MaterialBeschaffungsJobDTO> reportData);
+        Task Prepare(List<BelegPositionAVDTO> reportData);
+        Task Prepare(List<MaterialBeschaffungsJobDTO> reportData);
         List<IMaterialReportData> GetData(); //returns calculated data
         void Print(List<IMaterialReportData> reportData);
     }
