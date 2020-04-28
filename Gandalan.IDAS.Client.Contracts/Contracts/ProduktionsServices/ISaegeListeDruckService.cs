9801a298aa6ee598038f6f8780256bee72d59751
Konstantin Tümmler <konstantin@tuemmler.org>
2020-04-28 15:11:30 +0200

    type

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegeListeDruckService.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegeListeDruckService.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 namespace Gandalan.Client.Contracts.ProduktionsServices
 {
     public interface ISaegeListeDruckService {
-        List<IMaterialReportData> Prepare(List<BelegPositionAVDTO> reportData);
-        List<IMaterialReportData> Prepare(List<MaterialBeschaffungsJobDTO> reportData);
+        void Prepare(List<BelegPositionAVDTO> reportData);
+        void Prepare(List<MaterialBeschaffungsJobDTO> reportData);
         List<IMaterialReportData> GetData(); //returns calculated data
         void Print(List<IMaterialReportData> reportData);
     }
