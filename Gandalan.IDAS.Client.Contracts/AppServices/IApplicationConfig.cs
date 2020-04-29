f7f6eade328f8421453246162e0d0da72b21de97
René Gründig <rene.gruendig@gandalan.de>
2020-04-29 11:47:12 +0200

    ApplicationName in IApplicationCOnfig hinzugefügt, TargetEnvironment setter hinzugefügt.

--- Gandalan.IDAS.Client.Contracts/AppServices/IApplicationConfig.cs
+++ Gandalan.IDAS.Client.Contracts/AppServices/IApplicationConfig.cs
@@ -6,9 +6,10 @@ namespace Gandalan.Client.Contracts.AppServices
     {
         Guid AppToken { get; }
         string UpdateUrl { get; }
-        string TargetEnvironment { get; }
+        string TargetEnvironment { get; set; }
         Guid InstallationId { get; }
         string LocalDataDir { get; }
         bool DeveloperModeEnabled { get; }
+        string ApplicationName { get; }
     }
 }
