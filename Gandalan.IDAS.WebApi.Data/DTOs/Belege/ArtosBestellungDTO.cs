433e243074c9d80b9130bb0635b6f78ed6df59a5
gdl-mahalsky <64786316+gdl-mahalsky@users.noreply.github.com>
2020-05-14 13:24:32 +0200

    Neu: ArtosBestellungDTO

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Belege/ArtosBestellungDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    public class ArtosBestellungDTO : BaseListItemDTO
+    {
+    }
+}
