e9e280f169af6bf2a3648fc2defc79437eb5e240
gdl-mahalsky <64786316+gdl-mahalsky@users.noreply.github.com>
2020-05-14 19:25:42 +0200

    Neu: ArtosBestellungenRoutinen.cs

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/ArtosBestellungenRoutinen.cs
@@ -0,0 +1,29 @@
+﻿using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client
+{
+    public class ArtosBestellungenRoutinen : WebRoutinenBase
+    {
+        public ArtosBestellungenRoutinen(WebApiSettings settings) : base(settings)
+        {
+        }
+
+        public void SaveBenutzer(List<ArtosBestellungDTO> list)
+        {
+            if (!Login())
+                return;
+
+
+            foreach(var artosBestellung in list)
+            {
+                string responseStr = Put<string>("ArtosBestellung", artosBestellung);
+                Debug.WriteLine(responseStr);
+            }
+        }
+    }
+}
