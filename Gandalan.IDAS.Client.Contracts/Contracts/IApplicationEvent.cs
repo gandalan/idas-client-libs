6baaf9d739e6bdd32d34bdfef84c8e84a5f85435
Phil Reif <philipp.reif@gandalan.de>
2020-05-19 14:00:08 +0200

    Typo

--- Gandalan.IDAS.Client.Contracts/Contracts/IApplicationEvent.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/IApplicationEvent.cs
@@ -22,7 +22,7 @@ namespace Gandalan.Client.Contracts
     /// Register an event handler for a certain type of application event
     /// </summary>
     /// <typeparam name="TEvent">what kind of event do we want to be informed of</typeparam>
-    public interface IApplicationEventHandler<TEvent> : IApplicationEvent where TEvent : IApplicationEvent 
+    public interface IApplicationEventHandler<TEvent> : IApplicationEventHandler where TEvent : IApplicationEvent 
     {
         /// <summary>
         /// Handles a certain event that is dispatched to this instance
