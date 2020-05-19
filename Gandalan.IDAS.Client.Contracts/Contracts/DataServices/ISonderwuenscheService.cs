0e4bde232f8fa25493fecff0039423630ea550b1
DenisKoenig <denis.koenig@gandalan.de>
2020-05-19 12:58:48 +0200

    ISonderWuenscheService

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/ISonderwuenscheService.cs
@@ -0,0 +1,15 @@
+﻿using Gandalan.IDAS.WebApi.Data.DTOs.SonderWuensche;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.DataServices
+{
+    public interface ISonderWuenscheService
+    {
+        Task<VarianteSonderWunschDTO[]> GetAllAsync();
+        Task<VarianteSonderWunschDTO> GetAllVarianteSonderWuensche(string variantenName);
+    }
+}
