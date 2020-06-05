09861d5bde605daab8aa2eb6e96dc72562085704
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2020-06-05 08:35:24 +0200

    IBelegArtLookup aus IBOS3.Lookups übernommmen

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IBelegArtLookup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.Client.Contracts
+{
+    public interface IBelegArtLookup : ILookupDialog<IBelegArtLookupResult, IBelegArtLookupParams>
+    {
+    }
+
+    public interface IBelegArtLookupParams
+    {
+        string[] BelegArten { get; }
+    }
+
+    public interface IBelegArtLookupResult
+    {
+        string BelegArt { get; }
+    }
+
+    public class BelegArtLookupParams : IBelegArtLookupParams
+    {
+        public string[] BelegArten { get; set; }
+    }
+
+    public class BelegArtLookupResult : IBelegArtLookupResult
+    {
+        public static BelegArtLookupResult Empty { get; }
+        public string BelegArt { get; set; }
+    }
+}
