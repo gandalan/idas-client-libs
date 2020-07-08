2bb4868e62b9777cc97b690bab52d817b33b48dc
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2020-07-08 16:11:38 +0200

    LagerbuchungDTO erstellt

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Produktion/LagerbuchungDTO.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    /// <summary>
+    /// DTO für eine Lagerbuchung
+    /// </summary>
+    public class LagerbuchungDTO
+    {
+        public Guid KatalogArtikelGuid { get; set; }
+
+        public Guid FarbGuid { get; set; }
+
+        public float Betrag { get; set; }
+    }
+}
