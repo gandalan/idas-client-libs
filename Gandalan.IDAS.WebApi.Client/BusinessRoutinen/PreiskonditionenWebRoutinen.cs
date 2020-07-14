f885d7c10574b8042b99dc375b4ae33dd3f3aca4
René Gründig <rene.gruendig@gandalan.de>
2020-07-14 18:01:10 +0200

    PreiskonditionenWebRoutinen added für IGOR2

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/PreiskonditionenWebRoutinen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class PreiskonditionenWebRoutinen : WebRoutinenBase
+    {
+        public PreiskonditionenWebRoutinen(WebApiSettings settings) : base(settings)
+        {
+        }
+
+        public string SavePreiskonditionen(string konditionen)
+        {
+            if (Login())
+            {
+                return Put<string>("Preiskonditionen/", konditionen);
+            }
+            return null;
+        }
+
+        public async Task SavePreiskonditionenAsync(string konditionen)
+        {
+            await Task.Run(() => SavePreiskonditionen(konditionen));
+        }
+    }
+}
