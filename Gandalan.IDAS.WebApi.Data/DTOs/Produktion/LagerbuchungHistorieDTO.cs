6a4746c58a633017a46e69f3d5f5959c5382e748
gdl-mahalsky <64786316+gdl-mahalsky@users.noreply.github.com>
2020-07-17 10:58:38 +0200

    added LagerbuchungHistorieDTO

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Produktion/LagerbuchungHistorieDTO.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    /// <summary>
+    /// DTO für die Lagerbuchungshistorie
+    /// </summary>
+    public class LagerbuchungHistorieDTO
+    {
+        public Guid LagerbestandGuid { get; set; }
+        public Guid KatalogArtikelGuid { get; set; }
+        public Guid FarbGuid { get; set; }
+        public decimal MengeBestand { get; set; }
+        public decimal MengeReservierung { get; set; }
+        public decimal MengeMindestbestand { get; set; }
+        public decimal MengeEisernerBestand { get; set; }
+        public decimal MengeMaximalbestand { get; set; }
+        public string Einheit { get; set; }
+        public string User { get; set; }
+        public DateTime ChangedDate { get; set; }
+    }
+}
