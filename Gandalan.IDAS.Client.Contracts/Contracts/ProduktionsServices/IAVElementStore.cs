22cd604106ea2edf72f6c076bbfb4b004df215f3
René Gründig <rene.gruendig@gandalan.de>
2020-07-20 16:18:09 +0200

    IAVElementStore angepasst, dass bei GET Liste zurückgegeben werden kann.

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/IAVElementStore.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/IAVElementStore.cs
@@ -7,6 +7,8 @@ namespace Gandalan.Client.Contracts.ProduktionsServices
 {
     public interface IAVElementStore : IStore<BelegPositionAVDTO>
     {
+        Task<IList<BelegPositionAVDTO>> GetAsync(Guid guid);
+
         Task<IList<BelegPositionAVDTO>> GetAllAsync(DateTime changedSince);
 
         Task<IList<BelegPositionAVDTO>> AddOrUpdateAsync(List<BelegPositionAVDTO> data);
