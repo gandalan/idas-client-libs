e7240aa6cd305e5e21773b82dd25e93c7b10c255
Phil Reif <philipp.reif@gandalan.de>
2020-07-23 13:58:15 +0200

    Correct generic constraints

--- Gandalan.IDAS.Client.Contracts/Cache/ICache.cs
+++ Gandalan.IDAS.Client.Contracts/Cache/ICache.cs
@@ -3,10 +3,10 @@ using System.Threading.Tasks;
 
 namespace Gandalan.Client.Contracts.Cache
 {
-    public interface ICache<T> where T : new()
+    public interface ICache
     {
-        void PutItem(string identifier, T data);
-        Task<T> GetItem<T>(string identifier, DateTime? expirationDate, Func<Task<T>> retrieveDataIfCacheMiss);
+        void PutItem(string identifier, object data);
+        Task<T> GetItem<T>(string identifier, DateTime? expirationDate, Func<Task<T>> retrieveDataIfCacheMiss) where T : class, new();
         void Clear(string identifier);
     }
 }
