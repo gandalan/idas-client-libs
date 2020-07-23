6ee744f609cc8c899f00903fbf037c34323be820
Phil Reif <philipp.reif@gandalan.de>
2020-07-23 13:03:51 +0200

    Make ICache generic T with new constraint

--- Gandalan.IDAS.Client.Contracts/Cache/ICache.cs
+++ Gandalan.IDAS.Client.Contracts/Cache/ICache.cs
@@ -3,9 +3,9 @@ using System.Threading.Tasks;
 
 namespace Gandalan.Client.Contracts.Cache
 {
-    public interface ICache
+    public interface ICache<T> where T : new()
     {
-        void PutItem(string identifier, object data);
+        void PutItem(string identifier, T data);
         Task<T> GetItem<T>(string identifier, DateTime? expirationDate, Func<Task<T>> retrieveDataIfCacheMiss);
         void Clear(string identifier);
     }
