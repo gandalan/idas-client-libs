652e8a6dd9de15db7732bd101718d63e362efd27
Phil Reif <philipp.reif@gandalan.de>
2020-07-24 16:46:58 +0200

    Introduced QuickCommands

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/UIServices/IQuickCommandManager.cs
@@ -0,0 +1,13 @@
+﻿using Gandalan.Client.Contracts.Navigation;
+using System;
+using System.Collections.Generic;
+
+namespace Gandalan.Client.Contracts.UIServices
+{
+    public interface IQuickCommandManager
+    {
+        void AddQuickCommand(IQuickCommand command);
+        List<IQuickCommand> GetAllCommands();
+        event EventHandler QuickCommandsChanged;
+    }
+}
