90c220c5ed2ba3e370d249d3b1e8524dbc379762
Konstantin Tümmler <konstantin.tuemmler@gandalan.de>
2020-07-27 12:27:19 +0200

    AddressComparer Interface

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/Comparer/IAddressComparer.cs
@@ -0,0 +1,10 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.Client.Contracts.Comparer
+{
+    interface IBeleganschriftComparer
+    {
+        bool Compare(BeleganschriftDTO beleganschrift, ZusatzanschriftDTO zusatzanschrift);
+        //bool Compare(BeleganschriftDTO beleganschrift, AdresseDTO adresseDTO);
+    }
+}
