f4a40811f0d417cf5a68087af5e1807cdee8e060
Phil Reif <philipp.reif@gandalan.de>
2020-08-03 12:07:17 +0200

    Deprecation

--- Gandalan.IDAS.Client.Contracts/Contracts/Flux/IFluxStore.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/Flux/IFluxStore.cs
@@ -8,8 +8,14 @@ namespace Gandalan.IDAS.Client.Contracts.Flux
     public interface IFluxStore
     {
         void AddTransientEventHandler(EventHandler<IFluxAction> eventHandler);
-        void AddPermanentEventHandler(EventHandler<IFluxAction> eventHandler);
+        void AddPersistentEventHandler(EventHandler<IFluxAction> eventHandler);
         void ClearTransientEventHandlers();
+
+        /// <summary>
+        /// Obsoleted - use AddPersistentEventHandler
+        /// </summary>
+        [Obsolete("use AddPersistentEventHandler")]
+        event EventHandler<IFluxAction> DataChanged;
 
         Task Handle(Flux.IFluxAction action);
         Task Initialize();
