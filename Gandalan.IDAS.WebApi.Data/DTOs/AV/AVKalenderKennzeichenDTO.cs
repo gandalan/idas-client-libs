a7a3b879e9b7809783f1c65c3371c4182e934207
René Gründig <rene.gruendig@gandalan.de>
2020-08-17 10:09:28 +0200

    AVKalenderkennzeichenDTO eingeführt.

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/AV/AVKalenderKennzeichenDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    public class AVKalenderKennzeichenDTO
+    {
+        public Guid AVKalenderKennzeichenGuid { get; set; }
+        
+        public DateTime Datum { get; set; }
+        public string TagesKennzeichen { get; set; }
+        public string WochenKennzeichen { get; set; }
+
+        public long Version { get; set; }
+        public DateTime ChangedDate { get; set; }
+    }
+}
