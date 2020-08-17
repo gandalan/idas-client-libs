703d8879b7e1a780c2c174d29efc00baa3dc4e35
René Gründig <rene.gruendig@gandalan.de>
2020-08-17 10:25:22 +0200

    AVKalenderKennzeichenWebRoutinen implementiert. IAVKalenderKennzeichenService erstellt.

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IAVKalenderKennzeichenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.Client.Contracts.DataServices
+{
+    public interface IAVKalenderKennzeichenService
+    {
+        Task<List<AVKalenderKennzeichenDTO>> GetAllAsync(DateTime fromDate, DateTime toDate);
+        Task<AVKalenderKennzeichenDTO> SaveAsync(AVKalenderKennzeichenDTO kennzeichen);
+        Task DeleteAsync(Guid guid);        
+    }
+}
