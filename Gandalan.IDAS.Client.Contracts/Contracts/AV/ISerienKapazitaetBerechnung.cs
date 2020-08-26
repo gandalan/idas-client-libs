f48d98ef83937dc26975f743f6158557091239ca
René Gründig <rene.gruendig@gandalan.de>
2020-08-26 10:57:13 +0200

    ISerienKapazitaetBerechnung hinzugefügt

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/AV/ISerienKapazitaetBerechnung.cs
@@ -0,0 +1,12 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.Client.Contracts.AV
+{
+    public interface ISerienKapazitaetBerechnung
+    {
+        SerienKapazitaetInfo GetKapazitaetsBedarf(SerieDTO datum);
+    }
+}
