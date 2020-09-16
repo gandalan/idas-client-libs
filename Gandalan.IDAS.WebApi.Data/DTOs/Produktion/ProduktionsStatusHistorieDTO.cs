2bed7d2adb23f68c41521535d1fa9ef97aed65d6
DenisKoenig <denis.koenig@gandalan.de>
2020-09-16 15:14:36 +0200

    ProduktionsStatusDTO ChangedDate, ProduktionsStatiWerteDTO renamed

--- Gandalan.IDAS.WebApi.Data/DTOs/Produktion/ProduktionsStatusHistorieDTO.cs
+++ Gandalan.IDAS.WebApi.Data/DTOs/Produktion/ProduktionsStatusHistorieDTO.cs
@@ -5,7 +5,7 @@ namespace Gandalan.IDAS.WebApi.DTO
     public class ProduktionsStatusHistorieDTO
     {
         public Guid ProduktionsStatusHistorieGuid { get; set; }
-        public virtual ProduktionsStatiDTO Status { get; set; }
+        public virtual ProduktionsStatiWerteDTO Status { get; set; }
         public virtual string Text { get; set; }
         public virtual DateTime Zeitstempel { get; set; }
         public virtual string Benutzer { get; set; }
