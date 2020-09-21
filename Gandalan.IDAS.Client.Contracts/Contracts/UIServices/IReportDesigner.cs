e62edd06e3d6498c34115a3376a15cbeb01acde5
DenisKoenig <denis.koenig@gandalan.de>
2020-09-21 14:25:06 +0200

    IReportDesigner added

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/UIServices/IReportDesigner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.DTO.DTOs.Reports;
+
+namespace Gandalan.Client.Contracts.UIServices
+{
+    public interface IReportDesigner
+    {
+        void EditReport(Guid guid);
+        bool canHandle(Type type);
+    }
+}
