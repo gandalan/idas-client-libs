5f3cff18bb0b4b7a1fe7dec02fd048107efc62d1
Konstantin Tümmler <konstantin.tuemmler@gandalan.de>
2020-09-22 15:28:09 +0200

    added CanHandle

--- Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IBelegPositionCommand.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IBelegPositionCommand.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Input;
 
 namespace Gandalan.IDAS.Client.Contracts.Vorgaenge
@@ -11,6 +9,20 @@ namespace Gandalan.IDAS.Client.Contracts.Vorgaenge
         
         public abstract bool CanExecute(IBelegPositionItem parameter);
         public bool CanExecute(object parameter)
+        {
+            var pos = parameter as IBelegPositionItem;
+            if (pos == null)
+                return false;
+            return CanExecute(pos);
+        }
+        /// <summary>
+        /// Returns wherever this command should be shown or not. 
+        /// Sometimes a Positions should not show some commands if they are not allowed
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public abstract bool CanHandle(IBelegPositionItem parameter);
+        public bool CanHandle(object parameter)
         {
             var pos = parameter as IBelegPositionItem;
             if (pos == null)
