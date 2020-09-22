0716a6770d75357a71922a8240d17f6c753a0995
Konstantin Tümmler <konstantin.tuemmler@gandalan.de>
2020-09-22 15:32:25 +0200

    command

--- Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IBelegPositionCommand.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IBelegPositionCommand.cs
@@ -1,28 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
 using System.Windows.Input;
 
 namespace Gandalan.IDAS.Client.Contracts.Vorgaenge
 {
-    public abstract class IBelegPositionCommand : ICommand
+    public abstract class IBelegPositionCommand : ICommand, INotifyPropertyChanged
     {
-        public abstract event EventHandler CanExecuteChanged;       
-        
+        public abstract event EventHandler CanExecuteChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public abstract bool CanExecute(IBelegPositionItem parameter);
         public bool CanExecute(object parameter)
-        {
-            var pos = parameter as IBelegPositionItem;
-            if (pos == null)
-                return false;
-            return CanExecute(pos);
-        }
-        /// <summary>
-        /// Returns wherever this command should be shown or not. 
-        /// Sometimes a Positions should not show some commands if they are not allowed
-        /// </summary>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
-        public abstract bool CanHandle(IBelegPositionItem parameter);
-        public bool CanHandle(object parameter)
         {
             var pos = parameter as IBelegPositionItem;
             if (pos == null)
