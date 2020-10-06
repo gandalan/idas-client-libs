370df0c614b8227ce4adfa2c49833f3bae062a30
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2020-10-06 10:55:00 +0200

    GetPositionRelatedFachNr hinzugefügt

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ICalcAblageFachBedarf.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ICalcAblageFachBedarf.cs
@@ -9,6 +9,18 @@ namespace Gandalan.Client.Contracts.ProduktionsServices
 {
     public interface ICalcAblageFachBedarf
     {        
+        /// <summary>
+        /// Berechnet den Gesamtbedarf an Ablagefächern für eine Position
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
         int CalcBedarf(BelegPositionAVDTO position);
+
+        /// <summary>
+        /// Berechnet die positionsbezogene Fachnummer für ein Material (z.B. {Fr} = 1, {Fr}2 = 2, etc.)
+        /// </summary>
+        /// <param name="material"></param>
+        /// <returns></returns>
+        int GetPositionRelatedFachNr(MaterialbedarfDTO material);
     }
 }
