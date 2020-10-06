07dbe886cb167f63d39ad40e89fcdb4dd72c0508
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2020-10-06 10:28:08 +0200

    Namespace richtig korrigiert

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ICalcAblageFachBedarf.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ICalcAblageFachBedarf.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gandalan.Client.Contracts.Contracts.ProduktionsServices
+namespace Gandalan.Client.Contracts.ProduktionsServices
 {
     public interface ICalcAblageFachBedarf
     {        
