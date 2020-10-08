94f35010e741ce595ab2273749226c1d18a86349
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2020-10-08 12:24:58 +0200

    Erweiterung Contracts für Sägeliste/Etikettendruck

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegemassKorrekturService.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/ISaegemassKorrekturService.cs
@@ -19,6 +19,13 @@ namespace Gandalan.IDAS.Client.Contracts.Contracts.ProduktionsServices
         float GetKorrekturmass(string korrekturSatz, MaterialbedarfDTO material);
 
         /// <summary>
+        /// Ruft das (summierte) Korrekturmaß für ein MaterialbedarfDTO, abhängig von Winkel links/rechts, ab. Nutzt den Korrektursatz, der an der Säge "Serie" hinterlegt ist-
+        /// </summary>
+        /// <param name="material">MaterialbedarfDTO, für das das Korrekturmaß abgerufen werden soll</param>
+        /// <returns></returns>
+        float GetKorrekturmass(MaterialbedarfDTO material);
+
+        /// <summary>
         /// Ruft das Korrekturmaß für ein Profil beim angegebenen Winkel ab
         /// </summary>
         /// <param name="korrekturSatz">Korrektursatz der genutzt werden soll</param>
@@ -28,11 +35,26 @@ namespace Gandalan.IDAS.Client.Contracts.Contracts.ProduktionsServices
         float GetKorrekturmass(string korrekturSatz, string katalogNummer, int winkel);
 
         /// <summary>
+        /// Ruft das Korrekturmaß für ein Profil beim angegebenen Winkel ab. Nutzt den Korrektursatz, der an der Säge "Serie" hinterlegt ist.
+        /// </summary>
+        /// <param name="katalogNummer">Katalognummer des Profils</param>
+        /// <param name="winkel">Winkel, für den das Korrekturmaß abgerufen werden soll</param>
+        /// <returns></returns>
+        float GetKorrekturmass(string katalogNummer, int winkel);
+
+        /// <summary>
         /// Liefert das korrigierte Zuschnittsmaß für ein MaterialbedarfDTO (ZuschnittLaenge + Korrekturmaß)
         /// </summary>
         /// <param name="korrekturSatz">Korrektursatz der genutzt werden soll</param>
         /// <param name="material">MaterialbedarfDTO, für das die korrigierte Zuschnittslänge abgerufen werden soll</param>
         /// <returns></returns>
         float GetZuschnittKorrigiert(string korrekturSatz, MaterialbedarfDTO material);
+
+        /// <summary>
+        /// Liefert das korrigierte Zuschnittsmaß für ein MaterialbedarfDTO (ZuschnittLaenge + Korrekturmaß). Nutzt den Korrektursatz, der an der Säge "Serie" hinterlegt ist.
+        /// </summary>
+        /// <param name="material">MaterialbedarfDTO, für das die korrigierte Zuschnittslänge abgerufen werden soll</param>
+        /// <returns></returns>
+        float GetZuschnittKorrigiert(MaterialbedarfDTO material);
     }
 }
