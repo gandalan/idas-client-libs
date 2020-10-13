f6b0eeec646805c5c5b09f3ceb43a9d4d036d48b
Konstantin Tümmler <konstantin.tuemmler@gandalan.de>
2020-10-13 16:05:08 +0200

    fixed AssetsService interface

--- Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IArtikelAssetsService.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IArtikelAssetsService.cs
@@ -9,6 +9,6 @@ namespace Gandalan.Client.Contracts.DataServices
 {
     public interface IArtikelAssetsService
     {        
-        Task<byte[]> GetAssetAsync(string katalognummer, string extension, int width = 0, int height = 0);
+        Task<byte[]> GetAssetAsync(string katalognummer, string extension, int width = 0, int height = 0, int stroke = 20);
     }
 }
