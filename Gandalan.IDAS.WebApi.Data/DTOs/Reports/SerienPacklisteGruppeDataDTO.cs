ae161a7ccb247181ea59487aeb693d841b81f54c
DenisKoenig <denis.koenig@gandalan.de>
2020-10-14 16:18:38 +0200

    SerienPackliste properties added

--- Gandalan.IDAS.WebApi.Data/DTOs/Reports/SerienPacklisteGruppeDataDTO.cs
+++ Gandalan.IDAS.WebApi.Data/DTOs/Reports/SerienPacklisteGruppeDataDTO.cs
@@ -8,9 +8,11 @@ namespace Gandalan.IDAS.WebApi.Data.DTOs.Reports
 {
     public class SerienPacklisteGruppeDataDTO
     {
-        public string LieferscheinNummer { get; set; }
+        public string VorgangsNummer { get; set; }
+        public string PositionsNummer { get; set; }
+        public string Einbauort { get; set; }
         public string SerienKennzeichen { get; set; }
-        public string SerienauftragsNummer { get; set; }
+        public string VorgangsGruppenKennzeichen { get; set; }
         public List<SerienPacklisteGruppeItemDataDTO> Items { get; set; } = new List<SerienPacklisteGruppeItemDataDTO>();
     }
 }
