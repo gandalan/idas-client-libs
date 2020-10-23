eb1ddc27ba26b67e8e52bbb395a72e3d9ce1443c
René Gründig <rene.gruendig@gandalan.de>
2020-10-23 14:17:51 +0200

    Added IHelpPage

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/UIServices/IHelpPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.UIServices
+{
+    public interface IHelpPage
+    {
+        string Name { get; set; }
+        string ContentURL { get; set; }
+    }
+}
