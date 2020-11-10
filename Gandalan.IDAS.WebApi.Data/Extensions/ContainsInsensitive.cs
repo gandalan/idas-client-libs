579dac792aabfc979d4e9fd94b266c6b498c042f
Konstantin Tümmler <konstantin.tuemmler@gandalan.de>
2020-11-10 12:18:40 +0100

    StringExtension Contains case insensitive

---
+++ Gandalan.IDAS.WebApi.Data/Extensions/ContainsInsensitive.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class ContainsInsensitive
+    {
+        public static bool Contains(this string source, string needle, StringComparison comp)
+        {
+            return source?.IndexOf(needle, comp) >= 0;
+        }
+    }
+}
