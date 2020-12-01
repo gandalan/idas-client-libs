3ffb3d556aa1fb51528b03d341f38649f624ba57
DenisKoenig <denis.koenig@gandalan.de>
2020-12-01 11:24:53 +0100

    IMaterialBestellungExport Task<bool>

--- Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IMaterialBestellungExport.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IMaterialBestellungExport.cs
@@ -8,7 +8,7 @@ namespace Gandalan.IDAS.Client.Contracts.Contracts.DataServices
 {
     public interface IMaterialBestellungExport
     {
-        Task Export(IList<MaterialBeschaffungsJobDTO> jobs, string extension, string path);
-        Task Send(IList<MaterialBeschaffungsJobDTO> jobs, string extension, string email);
+        Task<bool> Export(IList<MaterialBeschaffungsJobDTO> jobs, string extension, string path);
+        Task<bool> Send(IList<MaterialBeschaffungsJobDTO> jobs, string extension, string email);
     }
 }
