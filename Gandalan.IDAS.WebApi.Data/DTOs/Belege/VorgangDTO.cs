6f53c9082ee687a2975a2e749aa27c1a5a354ad2
René Gründig <rene.gruendig@gandalan.de>
2020-12-02 14:37:43 +0100

    Vorgang WarteAufZahlungseingang property hinzugefügt

--- Gandalan.IDAS.WebApi.Data/DTOs/Belege/VorgangDTO.cs
+++ Gandalan.IDAS.WebApi.Data/DTOs/Belege/VorgangDTO.cs
@@ -72,6 +72,7 @@ namespace Gandalan.IDAS.WebApi.DTO
         public bool IstZustimmungErteilt { get; set; }
 
         public string InterneNotiz { get; set; }
+        public bool WarteAufZahlungseingang { get; set; }
         public string BemerkungFuerKunde { get; set; }
         public Guid OriginalVorgangGuid { get; set; }
         public Guid OriginalMandantGuid { get; set; }
