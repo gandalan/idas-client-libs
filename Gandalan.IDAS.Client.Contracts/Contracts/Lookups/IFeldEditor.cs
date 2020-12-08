1e25c57b40669bf8ce01f9a21baa026b21cdeb5a
Phil Reif <philipp.reif@gandalan.de>
2020-12-08 16:15:28 +0100

    Extend IFeldEditor

--- Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IFeldEditor.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IFeldEditor.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace Gandalan.Client.Contracts.Lookups
 {
-    public interface IFeldEditor<T,U> : ILookupDialog<T, U>
+    public interface IFeldEditor
     {
         bool CanHandle(string tag);
+    }
+
+    public interface IFeldEditor<T,U> : IFeldEditor, ILookupDialog<T, U>
+    {   
     }
 }
