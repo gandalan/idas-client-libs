20ef1d98061367927e023c1d3f7b3ea07f1ccaa7
Phil Reif <philipp.reif@gandalan.de>
2020-12-09 08:21:43 +0100

    Removed IFarbLookup, IBestellmassRechner
    Refa IFeldEditor

--- Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IFeldEditor.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/Lookups/IFeldEditor.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.DTO;
 
 namespace Gandalan.Client.Contracts.Lookups
 {
     public interface IFeldEditor
     {
         bool CanHandle(string tag);
-    }
-
-    public interface IFeldEditor<T,U> : IFeldEditor, ILookupDialog<T, U>
-    {   
+        void Execute(BelegPositionDTO belegPosition, out Dictionary<string, string> newValues);
     }
 }
