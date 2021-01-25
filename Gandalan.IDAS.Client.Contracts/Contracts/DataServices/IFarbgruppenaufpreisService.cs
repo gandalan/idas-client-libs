db9616af05fb364230a0602f6c5199fb8a0e5c22
René Gründig <rene.gruendig@gandalan.de>
2021-01-25 11:43:20 +0100

    IFarbgruppenaufpreisService erstellt

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IFarbgruppenaufpreisService.cs
@@ -0,0 +1,15 @@
+﻿using Gandalan.IDAS.WebApi.Data.DTOs.Farben;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.DataServices
+{
+    public interface IFarbgruppenaufpreisService
+    {
+        Task<FarbgruppenaufpreiseDTO> GetAsync();
+        Task SaveAsync(FarbgruppenaufpreiseDTO aufpreis);
+    }
+}
