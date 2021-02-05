a7628379cce1de6245db90e03e457c974e2e9c13
Sebastian Ratzek <Sebastian.Ratzek@ewu-it.de>
2021-02-05 08:39:49 +0100

    Einführung BerechnungParameterDTO

--- Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/IAVBerechnungService.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/ProduktionsServices/IAVBerechnungService.cs
@@ -1,10 +1,13 @@
-﻿using Gandalan.IDAS.WebApi.DTO;
+﻿using Gandalan.IDAS.WebApi.Data.DTOs.Produktion;
+using Gandalan.IDAS.WebApi.DTO;
 using System.Threading.Tasks;
 
 namespace Gandalan.Client.Contracts.ProduktionsServices
 {
     public interface IAVBerechnungService
     {
-        Task Execute(BelegPositionAVDTO model);
+        Task Execute(BerechnungParameterDTO parameter);
+
+        Task Execute(BerechnungParameterDTO parameter, bool force = false);
     }
 }
