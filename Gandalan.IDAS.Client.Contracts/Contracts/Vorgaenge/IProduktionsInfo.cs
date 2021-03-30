dd136803b9a9ac399a3f4c00ad5a5cb8889b7c79
DenisKoenig <denis.koenig@gandalan.de>
2021-03-30 09:51:19 +0200

    IBelegPositionItem

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/Vorgaenge/IProduktionsInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.Vorgaenge
+{
+    public interface IProduktionsInfo
+    {
+        public string PCode { get; set; }
+        public string Serie { get; set; }
+        public DateTime ProduktionsDatum { get; set; }
+        public DateTime LieferDatum { get; set; }
+    }
+}
