f18ac9a10356a1ddd1a9c89097279dd2215d0c84
DenisKoenig <denis.koenig@gandalan.de>
2021-04-09 12:36:55 +0200

    AVReserviertWebRoutinen added

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/AVReserviertWebRoutinen.cs
@@ -0,0 +1,56 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.Data.DTOs.AV;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class AVReserviertWebRoutinen : WebRoutinenBase
+    {
+        public AVReserviertWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public AVReserviertItemDTO[] GetAllAVReserviertItems()
+        {
+            if (Login())
+            {
+                return Get<AVReserviertItemDTO[]>("AVReserviert");
+            }
+            return null;
+        }
+
+        public AVReserviertItemDTO[] GetAllAVReserviertItemsBySerie(Guid serieGuid)
+        {
+            if (Login())
+            {
+                return Get<AVReserviertItemDTO[]>($"AVReserviert/?serieGuid={serieGuid}");
+            }
+            return null;
+        }
+
+
+
+        public async Task<AVReserviertItemDTO[]> GetAllAVReserviertItemsAsync()
+        {
+            if (Login())
+            {
+                return await GetAsync<AVReserviertItemDTO[]>("AVReserviert");
+            }
+            return null;
+        }
+
+        public async Task<AVReserviertItemDTO[]> GetAllAVReserviertItemsBySerieAsync(Guid serieGuid)
+        {
+            if (Login())
+            {
+                return await GetAsync<AVReserviertItemDTO[]>($"AVReserviert/?serieGuid={serieGuid}");
+            }
+            return null;
+        }
+    }
+}
