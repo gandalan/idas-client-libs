c2fd057bc2870307514877cca255dfd3168fde2a
René Gründig <rene.gruendig@gandalan.de>
2021-04-27 10:52:16 +0200

    Added IProduktionsfreigabeInfoService

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IProduktionsfreigabeInfoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.DataServices
+{
+    public interface IProduktionsfreigabeInfoService
+    {
+        Task<Dictionary<Guid, DateTime>> GetProduktionsfreigabeInfoAsync(IList<Guid> belegGuids);
+
+        Dictionary<Guid, DateTime> GetProduktionsfreigabeInfo(IList<Guid> belegGuids);
+    }
+}
