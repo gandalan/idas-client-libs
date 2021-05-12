962233104bc2c29a26e8e8cc25cffe5ddc114b13
Phil Reif <philipp.reif@gandalan.de>
2021-05-12 12:14:06 +0200

    LieferzusageDTO, LieferzusageWebRoutinen

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/LieferzusageWebRoutinen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.Data.DTOs.Produktion;
+using Gandalan.IDAS.WebApi.DTO;
+using Newtonsoft.Json;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class LieferzusageWebRoutinen : WebRoutinenBase
+    {
+        public LieferzusageWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public string MaterialZusagen(LieferzusageDTO lieferzusage)
+        {
+            if (Login())
+            {
+                return Post<string>("Lieferzusage", JsonConvert.SerializeObject(lieferzusage));
+            }
+            return null;
+        }
+        
+        public string ResetZusage(Guid lieferzusageGuid)
+        {
+            if (Login())
+            {
+                return Get<string>("Lieferzusage?zusageGuid=" + lieferzusageGuid.ToString());
+            }
+            return null;
+        }
+
+
+        public async Task<string> MaterialZusagenAsync(LieferzusageDTO lieferzusage)
+        {
+            return await Task<string>.Run(() => { return MaterialZusagen(lieferzusage); });
+        }
+
+        public async Task<string> ResetZusageAsync(Guid lieferzusageGuid)
+        {
+            return await Task<string>.Run(() => { return ResetZusage(lieferzusageGuid); });
+        }        
+    }
+}
---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Produktion/LieferzusageDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Data.DTOs.Produktion
+{
+    public class LieferzusageDTO
+    {
+        public Guid LieferzusageGuid { get; set; }
+        public Guid MaterialBedarfGuid { get; set; }
+        public decimal Stueckzahl { get; set; }
+        public string Lieferant { get; set; }
+        public long Version { get; set; }
+        public DateTime ChangedDate { get; set; }
+    }
+}
