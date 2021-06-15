38a65f41e6d840d94bf8338e7e2b972f3219e581
Rafał Grądziel <rafal.gradziel@fingo.pl>
2021-06-15 12:33:50 +0200

    New helper class for calling AVBerechnungCloudService

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/AVBerechnungCloudRoutinen.cs
@@ -0,0 +1,24 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.Data.DTOs.Produktion;
+using System;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class AVBerechnungCloudRoutinen : WebRoutinenBase
+    {
+        public AVBerechnungCloudRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public BerechnungResultDTO Process(string varianteName, BerechnungParameterDTO parameter)
+        {
+            if (Login())
+            {
+                string variante = Uri.EscapeDataString(varianteName);
+                return Post<BerechnungResultDTO>($"ProcessIbos/Process?varianteName={variante}", parameter);
+            }
+
+            return null;
+        }
+    }
+}
