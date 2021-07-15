d9fcc62d9e57f470264572bdc8689a9d8a710796
Phil Reif <philipp.reif@gandalan.de>
2021-07-15 15:56:59 +0200

    Add IFileService
    Make IProfilKuerzel, IBearbeitungsKuerzel async

--- Gandalan.IDAS.Client.Contracts/Contracts/AV/IProfilKuerzel.cs
+++ Gandalan.IDAS.Client.Contracts/Contracts/AV/IProfilKuerzel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Gandalan.IDAS.WebApi.Data;
 
 namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
 {
     public interface IProfilKuerzel
     {
-        IList<ProfilKuerzelDTO> GetListe();
+        Task<IList<ProfilKuerzelDTO>> GetListe();
     }
 }
---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IFileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.DataServices
+{
+    public interface IFileService
+    {
+        byte[] GetFile(string name);
+        Task<byte[]> GetFileAsync(string name);
+        FileInfoDTO[] GetFileList(string directory = "/");
+        Task<FileInfoDTO[]> GetFileListAsync();
+        byte[] SaveFile(string fileName, byte[] data);
+        Task SaveFileAsync(string fileName, byte[] data);
+    }
+}
