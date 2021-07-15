068f7e66fc752fb6ad69c8469b249ad6a4e2b7df
Phil Reif <philipp.reif@gandalan.de>
2021-07-15 15:10:18 +0200

    Add IProfilKuerzel, IBearbeitungsKuerzel

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/AV/IBearbeitungsKuerzel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gandalan.IDAS.WebApi.Data;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
+{
+    public interface IBearbeitungsKuerzel
+    {
+        public IList<BearbeitungsKuerzelDTO> GetListe { get; set; }
+    }
+}
---
+++ Gandalan.IDAS.Client.Contracts/Contracts/AV/IProfilKuerzel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gandalan.IDAS.WebApi.Data;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
+{
+    public interface IProfilKuerzel
+    {
+        public IList<ProfilKuerzelDTO> GetListe { get; set; }
+    }
+}
