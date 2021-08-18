517960f2a167378cb3619e340de2b057be847bde
Rafał Grądziel <rafal.gradziel@fingo.pl>
2021-08-18 09:11:43 +0200

    Another set of WebRoutinen changes

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/BelegPositionenAVWebRoutinen.cs
@@ -0,0 +1,20 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using System;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class BelegPositionenAVWebRoutinen : WebRoutinenBase
+    {
+        public BelegPositionenAVWebRoutinen(IWebApiConfig settings) : base(settings) { }
+
+        public string RunAVBerechnung(Guid id, long mandantId)
+        {
+            return Post("BelegPositionenAV/GetForWebJob/" + id + "?mandantId=" + mandantId, null);
+        }
+
+        public string CalculateItems()
+        {
+            return Post("BelegPositionenAV/CalculateItems", null);
+        }
+    }
+}
