92b35c583a537509004316ab08e7adbcb9429d16
Rafał Grądziel <rafal.gradziel@fingo.pl>
2021-08-25 14:05:49 +0200

    New BackupWebRoutinen

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/BackupWebRoutinen.cs
@@ -0,0 +1,21 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class BackupWebRoutinen : WebRoutinenBase
+    {
+        public BackupWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public void Run(string email, bool onlyBills, int year)
+        {
+            Post("Backup/Post?email=" + email + "&nurRechnungen=" + onlyBills + "&jahr=" + year, null);
+        }
+
+        public void RequestBackup(string email, bool onlyBills, int year)
+        {
+            Post("Backup/RequestBackup?email=" + email + "&nurRechnungen=" + onlyBills + "&jahr=" + year, null);
+        }
+    }
+}
