04d19e85ff6bbbe71b82e93e998065ffd3992ff3
Rafał Grądziel <rafal.gradziel@fingo.pl>
2021-08-31 16:53:22 +0200

    Fix WebJobHistorieDTO nested class

--- Gandalan.IDAS.WebApi.Data/DTOs/WebJob/WebJobHistorieDTO.cs
+++ Gandalan.IDAS.WebApi.Data/DTOs/WebJob/WebJobHistorieDTO.cs
@@ -4,17 +4,14 @@ namespace Gandalan.IDAS.WebApi.Data.DTOs.WebJob
 {
     public class WebJobHistorieDTO
     {
-        public class WebJobHistorie
-        {
-            public Guid WebJobHistorieGuid { get; set; }
+        public Guid WebJobHistorieGuid { get; set; }
 
-            public string WebJobName { get; set; }
+        public string WebJobName { get; set; }
 
-            public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
 
-            public string Status { get; set; }
+        public string Status { get; set; }
 
-            public string Text { get; set; }
-        }
+        public string Text { get; set; }
     }
 }
