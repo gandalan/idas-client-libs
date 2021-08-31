ab93dfbad3860710b967974ba12e951858c497f8
Rafał Grądziel <rafal.gradziel@fingo.pl>
2021-08-31 13:49:50 +0200

    DTO & WebRoutinen for WebJobHistorie

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/WebJob/WebJobHistorieDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gandalan.IDAS.WebApi.Data.DTOs.WebJob
+{
+    public class WebJobHistorieDTO
+    {
+        public class WebJobHistorie
+        {
+            public Guid WebJobHistorieGuid { get; set; }
+
+            public string WebJobName { get; set; }
+
+            public DateTime Timestamp { get; set; }
+
+            public string Status { get; set; }
+
+            public string Text { get; set; }
+        }
+    }
+}
