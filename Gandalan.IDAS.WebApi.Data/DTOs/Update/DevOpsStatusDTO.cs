5616d54618ff2509d99691e4dc316dd49438e729
Adam Stachowicz <adam.stachowicz@fingo.info>
2021-09-08 13:21:06 +0200

    DevOpsStatusDTO

---
+++ Gandalan.IDAS.WebApi.Data/DTOs/Update/DevOpsStatusDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Gandalan.IDAS.WebApi.Data.DTOs.Update
+{
+    public class DevOpsStatusDTO
+    {
+        public string Env { get; set; }
+        public string DbInfo { get; set; }
+        public string CurrentMigration { get; set; }
+        public List<string> PendingMigrations { get; set; }
+    }
+}
