e3fa7961482f29e2322c152acb2bc0945657c5d9
DenisKoenig <denis.koenig@gandalan.de>
2021-10-11 14:09:14 +0200

    IPDFToImageConverter

---
+++ Gandalan.IDAS.Client.Contracts/Contracts/DataServices/IPDFToImageConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.Client.Contracts.Contracts.DataServices
+{
+    public interface IPDFToImageConverter
+    {
+        Task<List<string>> ConvertPDFListToImages(List<string> pdfList);
+    }
+}
