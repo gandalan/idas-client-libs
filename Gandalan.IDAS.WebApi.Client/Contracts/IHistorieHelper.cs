6bcb25650c4873d6e5d9fa556c845d94e8a06958
Rafał Grądziel <rafal.gradziel@fingo.pl>
2021-10-13 13:40:57 +0200

    New interface for IHistorieHelper

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/IHistorieHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts
+{
+    public interface IHistorieHelper
+    {
+        void ProgressInfo(Action action);
+        void ProgressInfo(string label, Action action);
+        void Info(string text);
+        void Warning(string text);
+        void Error(string text);
+        void Fatal(string text);
+        void Exception(Exception e);
+    }
+}
