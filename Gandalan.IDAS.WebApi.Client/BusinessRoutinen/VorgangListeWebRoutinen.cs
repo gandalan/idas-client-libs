a6dc3ec91413763164b22f85f872b35065b985fd
Phil Reif <philipp.reif@gandalan.de>
2021-10-29 10:41:31 +0200

    Add parameter includeASP to VorgangListe methods

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangListeWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangListeWebRoutinen.cs
@@ -13,38 +13,38 @@ namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
         {
         }
 
-        public VorgangListItemDTO[] LadeVorgangsListe(int jahr)
+        public VorgangListItemDTO[] LadeVorgangsListe(int jahr, bool includeASP = false)
         {
             if (Login())
             {
-                return Get<VorgangListItemDTO[]>("VorgangListe/?jahr=" + jahr);
+                return Get<VorgangListItemDTO[]>($"VorgangListe/?jahr={jahr}&includeASP={includeASP}");
             }
             return null;
         }
 
-        public VorgangListItemDTO[] LadeVorgangsListe(string status, int jahr)
+        public VorgangListItemDTO[] LadeVorgangsListe(string status, int jahr, bool includeASP = false)
         {
             if (Login())
             {
-                return Get<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}");
+                return Get<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&includeASP={includeASP}");
             }
             return null;
         }
 
-        public VorgangListItemDTO[] LadeVorgangsListe(string status, int jahr, DateTime changedSince)
+        public VorgangListItemDTO[] LadeVorgangsListe(string status, int jahr, DateTime changedSince, bool includeASP = false)
         {
             if (Login())
             {
-                return Get<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince.ToString("o")}");
+                return Get<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince.ToString("o")}&includeASP={includeASP}");
             }
             return null;
         }
 
-        public VorgangListItemDTO[] LadeVorgangsListe(int jahr, string status, DateTime changedSince, string art = "", bool includeArchive = false, bool includeOthersData = false, string search = "")
+        public VorgangListItemDTO[] LadeVorgangsListe(int jahr, string status, DateTime changedSince, string art = "", bool includeArchive = false, bool includeOthersData = false, string search = "", bool includeASP = false)
         {
             if (Login())
             {
-                return Get<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince.ToString("o")}&art={art}&includeArchive={includeArchive}&includeOthersData={includeOthersData}&search={search}");
+                return Get<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince.ToString("o")}&art={art}&includeArchive={includeArchive}&includeOthersData={includeOthersData}&search={search}&includeASP={includeASP}");
             }
             return null;
         }
