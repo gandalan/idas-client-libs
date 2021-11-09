b5b433c0554ba0dc6d7a30f2f49bc44356dc287e
DenisKoenig <denis.koenig@gandalan.de>
2021-11-09 12:18:28 +0100

    ProduktionsfreigabeItemDTO

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Produktion/ProduktionsfreigabeItemDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.Produktion
+{
+    public class ProduktionsfreigabeItemDTO
+    {
+        public Guid VorgangGuid { get; set; }
+        public long VorgangsNummer { get; set; }
+        public Guid BelegGuid { get; set; }
+        public DateTime Belegdatum { get; set; }
+        public DateTime FreigabeDatum { get; set; }
+        public string Kundenname { get; set; }
+        public string Kommission { get; set; }
+        public string Besitzer { get; set; }
+        public string Besteller { get; set; }
+        public string Bearbeiter { get; set; }
+    }
+}
