7e92c6e6b4d661304e0c85819cd87c64fe29c75f
Phil Reif <philipp.reif@gandalan.de>
2021-11-09 10:27:48 +0100

    IAddonCommand added

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IAddonCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.UIServices
+{
+    public abstract class IAddonCommand : ICommand, INotifyPropertyChanged
+    {
+        public abstract event EventHandler CanExecuteChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Caption { get; set; } = "?";
+        public string Tooltip { get; set; } = "";
+        public string Context { get; set; } = "";
+
+        public virtual bool CanExecute(object parameter)
+        {
+            return parameter != null;
+        }
+
+        /// <summary>
+        /// Returns wherever this command should be shown/can be used at all or not.
+        /// </summary>
+        /// <param name="parameter">Some object in the command's context</param>
+        /// <returns></returns>
+        public virtual bool CanHandle(object parameter)
+        {
+            return CanExecute(parameter);
+        }
+
+        public virtual void Execute(object parameter)
+        {        
+        }
+    }
+
+    public abstract class IAddonCommand<T> : IAddonCommand
+    {
+        public virtual bool CanExecute(T parameter)
+        {
+            return base.CanExecute(parameter);
+        }
+
+        public virtual bool CanHandle(T parameter)
+        {
+            return base.CanHandle(parameter);
+        }
+
+        public virtual void Execute(T parameter)
+        {
+            base.Execute(parameter);
+        }
+    }
+}
