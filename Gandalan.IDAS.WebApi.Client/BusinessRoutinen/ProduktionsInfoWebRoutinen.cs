758095a10d3e1cac81be7d096d4ab523287ec899
René Gründig <rene.gruendig@gandalan.de>
2021-11-10 14:50:31 +0100

    Added ProduktionsInfoWebRoutinen

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/ProduktionsInfoWebRoutinen.cs
@@ -0,0 +1,46 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.Client.DTOs.Produktion;
+using Gandalan.IDAS.WebApi.Client.Settings;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class ProduktionsInfoWebRoutinen : WebRoutinenBase
+    {
+        public ProduktionsInfoWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public ProduktionsInfoDTO GetProduktionsInfo(Guid vorgangGuid)
+        {
+            if (Login())
+            {
+                try
+                {
+                    return Get<ProduktionsInfoDTO>("ProduktionsInfo/" + vorgangGuid.ToString());
+                }
+                catch (WebException wex)
+                {
+                    if (wex.Response is HttpWebResponse)
+                    {
+                        HttpStatusCode code = (wex.Response as HttpWebResponse).StatusCode;
+                        if (code == HttpStatusCode.NotFound)
+                            return null;
+                    }
+                    throw;
+                }
+            }
+            return null;
+        }
+
+        public async Task<ProduktionsInfoDTO> GetProduktionsInfoAsync(Guid vorgangGuid)
+        {
+            return await Task.Run(() => GetProduktionsInfo(vorgangGuid));
+        }
+    }
+}
