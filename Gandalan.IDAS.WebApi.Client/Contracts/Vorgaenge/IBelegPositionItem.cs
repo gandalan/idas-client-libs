0a9bb2e48a0735ca5450a910f9eb92c30b23c1a6
René Gründig <rene.gruendig@gandalan.de>
2021-11-11 08:23:05 +0100

    fix build error

--- Gandalan.IDAS.WebApi.Client/Contracts/Vorgaenge/IBelegPositionItem.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Vorgaenge/IBelegPositionItem.cs
@@ -1,5 +1,4 @@
-﻿using Gandalan.IDAS.Client.Contracts.Contracts.Vorgaenge;
-using Gandalan.IDAS.WebApi.DTO;
+﻿using Gandalan.IDAS.WebApi.DTO;
 using System;
 using System.Collections.Generic;
 using System.Text;
