72f5d33800b7487de0d104be7d3c8ac712090da9
Phil Reif <philipp.reif@gandalan.de>
2021-11-12 08:36:28 +0100

    Remove showPercentage param from IProgressNotify methods

--- Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IProgressNotify.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IProgressNotify.cs
@@ -9,8 +9,26 @@ namespace Gandalan.Client.Contracts.UIServices
 {
     public interface IProgressNotify
     {
-        Task ShowProgress(string title, bool showPercentage, Func<Task> toExecute);
-        Task ShowProgress(string title, bool showPercentage, Func<IProgressNotify, Task> toExecute);
+        /// <summary>
+        /// Show a progress indicator for an indeterminate Task (i.e. no 
+        /// percentage for progress)
+        /// </summary>
+        /// <param name="title">Title to show</param>
+        /// <param name="toExecute">Function to run</param>
+        /// <returns></returns>
+        Task ShowProgress(string title, Func<Task> toExecute);
+        /// <summary>
+        /// Show a progress indicator for a Task that updates progress
+        /// </summary>
+        /// <param name="title">Title to show</param>
+        /// <param name="toExecute">Function to run, IProgressNotify is passed as param for updating</param>
+        /// <returns></returns>
+        Task ShowProgress(string title, Func<IProgressNotify, Task> toExecute);
+        /// <summary>
+        /// Function to display the progress value for determinate progress tasks
+        /// </summary>
+        /// <param name="percent">value</param>
+        /// <returns></returns>
         Task UpdateProgress(int percent);
     }
 }
