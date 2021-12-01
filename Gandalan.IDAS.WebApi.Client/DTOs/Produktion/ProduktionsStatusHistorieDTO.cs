3bd6598a80d5fe3057503a696b9fcecd33282c8d
René Gründig <rene.gruendig@gandalan.de>
2021-12-01 15:05:23 +0100

    added props to ProduktionsStatusHistorieDTO and ProduktionsStatusDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/Produktion/ProduktionsStatusHistorieDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Produktion/ProduktionsStatusHistorieDTO.cs
@@ -5,9 +5,12 @@ namespace Gandalan.IDAS.WebApi.DTO
     public class ProduktionsStatusHistorieDTO
     {
         public Guid ProduktionsStatusHistorieGuid { get; set; }
-        public virtual ProduktionsStatiWerteDTO Status { get; set; }
-        public virtual string Text { get; set; }
-        public virtual DateTime Zeitstempel { get; set; }
-        public virtual string Benutzer { get; set; }
+        public ProduktionsStatiWerteDTO Status { get; set; }
+        public string Text { get; set; }
+        public int Produktionsminuten { get; set; }
+        public string ProduktionsStatusInfoText { get; set; }
+        public int ProduktionsStatusInProzent { get; set; }
+        public DateTime Zeitstempel { get; set; }
+        public string Benutzer { get; set; }
     }
 }
