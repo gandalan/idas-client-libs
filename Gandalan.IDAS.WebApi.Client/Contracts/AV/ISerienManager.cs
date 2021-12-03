82f444a39dcf7b45eb5b1c82bf42790c3761a183
Phil Reif <philipp.reif@gandalan.de>
2021-12-03 10:23:23 +0100

    Obsoleted ISerienManager

--- Gandalan.IDAS.WebApi.Client/Contracts/AV/ISerienManager.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/AV/ISerienManager.cs
@@ -8,20 +8,9 @@ using System.Threading.Tasks;
 
 namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
 {
+    [Obsolete("Add to and remove from Serie must be performed on the backend!")]
     public interface ISerienManager
     {
-        Task AddToSerie(BelegPositionDTO position, SerieDTO serie);
-        Task AddToSerie(IList<BelegPositionDTO> position, SerieDTO serie);
-        Task AddToSerie(BelegPositionAVDTO position, SerieDTO serie);
-        Task AddToSerie(IList<BelegPositionAVDTO> positionen, SerieDTO serie);
-        Task AddToSerie(Guid avPositionsGuid, SerieDTO serie);
-        Task AddToSerie(IList<Guid> avPositionenGuids, SerieDTO serie);
-
-        Task RemoveFromSerie(BelegPositionDTO position);
-        Task RemoveFromSerie(IList<BelegPositionDTO> position);
-        Task RemoveFromSerie(BelegPositionAVDTO position);
-        Task RemoveFromSerie(IList<BelegPositionAVDTO> position);
-        Task RemoveFromSerie(Guid avPositionsGuid);
-        Task RemoveFromSerie(IList<Guid> avPositionenGuids);
+        
     }
 }
