d908739e11d170c8f0d5367df94fd83f5a1277f4
ewu-sra <57943755+ewu-sra@users.noreply.github.com>
2022-01-18 11:25:49 +0100

    Settings für Etiketten hinzugefügt

--- Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionsSettingsDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionsSettingsDTO.cs
@@ -10,6 +10,11 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.Settings
         public bool SprossenfreiEnabled { get; set; }
         public bool SaegedatenAufEtiketten { get; set; } = false;
         public bool MaschinenkederVerwenden { get; set; } = false;
+        public bool PrintPCodeASQRCode { get; set; } = true;
+        public bool HaendlerKommAufEtikett { get; set; } = false;
+        public bool VorgangKommAufEtikett { get; set; } = true;
+        public bool PositionKommAufEtikett { get; set; } = false;
+        public bool EinbauOrtAufEtikett { get; set; } = true;
         public DateTime ChangedDate { get; set; }
     }
 }
