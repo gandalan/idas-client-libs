168a007d64071bf8bf364b8addc2befb3facfa76
ewu-sra <57943755+ewu-sra@users.noreply.github.com>
2022-02-18 14:49:35 +0100

    #6565 - BearbeitungsKuerzel für farbige Rahmen erweitert

--- Gandalan.IDAS.WebApi.Client/DTOs/AV/BearbeitungsKuerzelDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/AV/BearbeitungsKuerzelDTO.cs
@@ -9,5 +9,7 @@ namespace Gandalan.IDAS.WebApi.Data
         public string BearbeitungsKuerzel { get; set; }
         public string Beschreibung { get; set; }
         public string[] VerfuegbarFuer { get; set; }
+        public string FarbCode { get; set; }
+        public string RegularExpression { get; set; }
     }
 }
