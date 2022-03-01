a6d0d02e1b0de15508d94be5806349fd7919aa3e
René Gründig <rene.gruendig@gandalan.de>
2022-03-01 10:55:16 +0100

    Added IBelegpositionPreisService and ISonderwunschBerechnenService

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/Vorgaenge/IBelegpositionPreisService.cs
@@ -0,0 +1,12 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.Vorgaenge
+{
+    public interface IBelegpositionPreisService
+    {
+        bool RecalculatePreis(BelegPositionDTO pos);
+    }
+}
