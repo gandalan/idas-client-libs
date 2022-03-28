509ba28046d2a8b686d2ba32bc8c6319695fbbe5
DenisKoenig <denis.koenig@gandalan.de>
2022-03-28 14:38:31 +0200

    ProduktionSettings MoeglicheVariantenVorbiegen

--- Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionProduktfamilieSettingsDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionProduktfamilieSettingsDTO.cs
@@ -9,6 +9,7 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.Settings
         public string ProduktfamilienName { get; set; }
         public bool SprossenFrei { get; set; }
         public bool Vorbiegen { get; set; }
+        public List<string> MoeglicheVariantenVorbiegen { get; set; }
         public string Buerste { get; set; }
         public bool FederkraftErhoeht { get; set; }
         public bool VerschlussGegenstueckVor2022 { get; set; }
