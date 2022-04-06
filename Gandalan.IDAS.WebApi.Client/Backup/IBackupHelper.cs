0c5f45a3439f0b1c4f7789dcc69b768be05f2d7d
René Gründig <rene.gruendig@gandalan.de>
2022-04-06 15:22:17 +0200

    added IBackupHelper, MailJobInfo, BelegDruckJobData

---
+++ Gandalan.IDAS.WebApi.Client/Backup/IBackupHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gandalan.IDAS.WebApi.Client.Backup
+{
+    public interface IBackupHelper
+    {
+        void GenerateBackup(List<Guid> vorgangGuids, string email, bool nurRechnungen);
+        bool IsBackupPossible(string email);
+    }
+}
