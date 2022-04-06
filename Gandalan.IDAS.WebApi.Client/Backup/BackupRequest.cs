98607f4ac99f5ef8fad4a7ec37d9652472f4a301
René Gründig <rene.gruendig@gandalan.de>
2022-04-06 10:04:00 +0200

    Added MQSettings, BackupRequest

---
+++ Gandalan.IDAS.WebApi.Client/Backup/BackupRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Gandalan.IDAS.WebApi.Client.Backup
+{
+    public class BackupRequest
+    {
+        public string Email { get; set; }
+        public bool OnlyBills { get; set; }
+        public int Year { get; set; }
+    }
+}
