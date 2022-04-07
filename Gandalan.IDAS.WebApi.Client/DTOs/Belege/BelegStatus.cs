92e639a845b483cb6579c29f4842dd2952b5b943
Simon Mahalsky <simon.mahalsky@gandalan.de>
2022-04-07 15:05:38 +0200

    BelegStatus aus dem Backend kopiert

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Belege/BelegStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.Belege
+{
+    public enum BelegStatus
+    {
+        Unbekannt = 0,
+        Erfasst = 10,
+        Geloescht = 20,
+        AngebotAngefragt = 30,
+        Bestellt = 40,
+        AuftragBestaetigt = 50,
+        InProduktion = 60,
+        ProduktionAbgeschlossen = 70,
+        VersandVorbereitet = 80,
+        VersandAusgefuehrt = 90,
+        WareAusgeliefert = 100,
+        Reklamation = 110,
+        BestellungAbgeschlossen = 120,
+        Fakturiert = 130,
+        Storniert = 140,
+        Importiert = 150,
+        BelegKopiert = 160
+    }
+}
