aa25d2d77e2f3365fdf9b5b2f54d7a37ce1a84a4
Simon Mahalsky <simon.mahalsky@gandalan.de>
2022-04-08 08:35:51 +0200

    Description für BelegStatus

--- Gandalan.IDAS.WebApi.Client/DTOs/Belege/BelegStatus.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Belege/BelegStatus.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
 
 namespace Gandalan.IDAS.WebApi.Client.DTOs.Belege
 {
@@ -8,20 +6,30 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.Belege
     {
         Unbekannt = 0,
         Erfasst = 10,
+        [Description("Gelöscht")]
         Geloescht = 20,
+        [Description("Angebot angefragt")]
         AngebotAngefragt = 30,
         Bestellt = 40,
+        [Description("Auftrag bestätigt")]
         AuftragBestaetigt = 50,
+        [Description("in Produktion")]
         InProduktion = 60,
+        [Description("Produktion abgeschlossen")]
         ProduktionAbgeschlossen = 70,
+        [Description("Versand vorbereitet")]
         VersandVorbereitet = 80,
+        [Description("Versand ausgeführt")]
         VersandAusgefuehrt = 90,
+        [Description("Ware ausgeliefert")]
         WareAusgeliefert = 100,
         Reklamation = 110,
+        [Description("Bestellung abgeschlossen")]
         BestellungAbgeschlossen = 120,
         Fakturiert = 130,
         Storniert = 140,
         Importiert = 150,
+        [Description("Beleg kopiert")]
         BelegKopiert = 160
     }
 }
