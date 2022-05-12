daa5489180a3383ea2be3d859f3719830cc3c3b1
René Gründig <rene.gruendig@gandalan.de>
2022-05-12 11:56:27 +0200

    #7754 Added new Vorgang/Beleg/BelegPositionen-HistorienDTOs

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Belege/VorgangHistorienDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    public class VorgangHistorienDTO
+    {
+        public List<VorgangHistorieDTO> VorgangHistorien { get; set; }
+        public Dictionary<Guid, List<BelegHistorieDTO>> BelegHistorien { get; set; }
+        public Dictionary<Guid, List<BelegPositionHistorieDTO>> BelegPositionHistorien { get; set; }
+    }
+}
