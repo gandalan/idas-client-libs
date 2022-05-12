2bb8672ab3f07e37c70c3f9c151ef7f00d81f544
René Gründig <rene.gruendig@gandalan.de>
2022-05-12 09:19:51 +0200

    undo remove status from BelegHistorieDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/Belege/BelegHistorieDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Belege/BelegHistorieDTO.cs
@@ -10,6 +10,8 @@ namespace Gandalan.IDAS.WebApi.DTO
     public class BelegHistorieDTO
     {
         public Guid BelegHistorieGuid { get; set; }
+        public virtual string Status { get; set; }
+
         public virtual string Text { get; set; }
         public virtual DateTime Zeitstempel { get; set; }
         public virtual string Benutzer { get; set; }
