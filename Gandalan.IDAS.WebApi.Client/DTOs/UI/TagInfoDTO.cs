93c90abb033bb2cede264fbbec54036da9dd2390
René Gründig <rene.gruendig@gandalan.de>
2022-05-13 11:31:01 +0200

    #7732 Added TagInfoDTO

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/UI/TagInfoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.UI
+{
+    public class TagInfoDTO
+    {
+        public Guid ObjectGuid { get; set; }
+        public string Text { get; set; }
+
+        public bool CanRemove { get; set; }
+        public string ToolTip { get; set; }
+        public string IconName { get; set; }
+        public string BackgroundColorCode { get; set; }
+        public string TextColorCode { get; set; }
+
+        public long Version { get; set; }
+        public DateTime ChangedDate { get; set; }
+    }
+}
