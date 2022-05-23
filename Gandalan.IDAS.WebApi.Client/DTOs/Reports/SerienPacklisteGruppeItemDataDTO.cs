37186ea6a6ec45ebc8e4e901f11b931b21cac422
DenisKoenig <denis.koenig@gandalan.de>
2022-05-23 10:12:17 +0200

    SerienPackListeGruppeItemDataDTO Farbe added

--- Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienPacklisteGruppeItemDataDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienPacklisteGruppeItemDataDTO.cs
@@ -11,7 +11,10 @@ namespace Gandalan.IDAS.WebApi.Data.DTOs.Reports
         public int Anzahl { get; set; }
         public string Katalognummer { get; set; }
         public string Bezeichnung { get; set; }
-        public string Laenge { get; set; }
+        public string FarbKuerzel { get; set; }
+        public string FarbCode { get; set; }
+        public string FarbBezeichnung { get; set; }
+        public float Laenge { get; set; }
         public string Winkel { get; set; }
 
     }
