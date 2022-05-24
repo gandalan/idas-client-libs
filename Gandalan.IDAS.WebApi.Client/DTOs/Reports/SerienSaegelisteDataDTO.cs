e083a5d1a77822de6aa46ec42f63528b2690eeeb
DenisKoenig <denis.koenig@gandalan.de>
2022-05-24 16:46:48 +0200

    SerienSaegelisteDataDTO erweitert

--- Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienSaegelisteDataDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienSaegelisteDataDTO.cs
@@ -10,6 +10,8 @@ namespace Gandalan.IDAS.WebApi.Data.DTOs.Reports
     {
         public string Titel { get; set; }
         public string Serienkennzeichen { get; set; }
+        public bool SaegemaßeOhneKorrektur { get; set; } = true;
+        public bool EtikettenZugehoerigkeit { get; set; } = false;
         public List<SerienSaegelisteProfilgruppeDataDTO> Profilgruppen { get; set; } = new List<SerienSaegelisteProfilgruppeDataDTO>();
     }
 }
