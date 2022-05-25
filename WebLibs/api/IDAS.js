10dab8e7625ad451fedb7cc92246be6408b70542
Phil Reif <philipp.reif@gandalan.de>
2022-05-25 07:44:43 +0200

    Refa IDAS

--- WebLibs/api/IDAS.js
+++ WebLibs/api/IDAS.js
@@ -1,41 +1,45 @@
 import { get, writable } from "svelte/store";
 import { RESTClient } from "./RESTClient";
 
-export const APP_TOKEN = "66B70E0B-F7C4-4829-B12A-18AD309E3970";
-export const AuthToken = writable(localStorage.getItem("AuthToken"));
-export const MandantGuid = writable(localStorage.getItem("MandantGuid"));
-export const ApiBaseUrl = writable(localStorage.getItem("ApiBaseUrl") || "https://api.dev.idas-cloudservices.net/api");
-export const SiteBaseUrl = writable(window.location.protocol + "//" + window.location.host);
-export const SSOAuthUrl = writable(get(ApiBaseUrl).replace("/api", "") + "/SSO?a=" + APP_TOKEN + "&r=%target%?t=%token%%26m=%mandant%");
+let appToken = localStorage.getItem("IDAS_AppToken") || "66B70E0B-F7C4-4829-B12A-18AD309E3970";
+let authToken = localStorage.getItem("IDAS_AuthToken");
+let mandantGuid = localStorage.getItem("IDAS_MandantGuid");
+let apiBaseUrl = localStorage.getItem("IDAS_ApiBaseUrl") || "https://api.dev.idas-cloudservices.net/api";
+let siteBaseUrl = localStorage.get("SiteBaseUrl") || window.location.protocol + "//" + window.location.host;
+let ssoAuthUrl = apiBaseUrl.replace("/api", "") + "/SSO?a=" + appToken + "&r=%target%?t=%token%%26m=%mandant%";
+let restClient = new RESTClient(apiBaseUrl, authToken);
 
 export class IDAS 
 {
-    authToken = {};
-    api = new RESTClient(get(ApiBaseUrl), get(AuthToken));
-
     async authenticate(authDTO) { 
-        var { data } = await this.api.post("/Login/Authenticate", authDTO); 
-        this.authToken = data;
+        authDTO.AppToken = appToken;
+        var { data } = await restClient.post("/Login/Authenticate", authDTO); 
+        if (data?.Token)
+        {
+            authToken = data.Token;
+            restClient = new RESTClient(apiBaseUrl, authToken);
+        }
         return data;
     }
 
     async authenticateWithSSO() { 
-        var ssoURL = get(SSOAuthUrl).replace("%target%", get(SiteBaseUrl)).replace("%mandant%", get(MandantGuid));
+        var ssoURL = get(ssoAuthUrl).replace("%target%", get(siteBaseUrl)).replace("%mandant%", get(mandantGuid));
         window.location = ssoURL;
     }
 
     mandanten = {
-        async getAll() { return await this.api.get("/Mandanten"); },
-        async save(m) { await this.api.put("/Mandanten", m); }
+        async getAll() { return await restClient.get("/Mandanten"); },
+        async save(m) { await restClient.put("/Mandanten", m); }
     };
 
     benutzer = {
-        async getAll() { return await this.api.get("/Benutzer"); },
-        async save(m) { await this.api.put("/Benutzer", m); }
+        async getAll() { return await restClient.get("/BenutzerListe"); },
+        async get(guid) { return await restClient.get("/Benutzer/" + guid); },
+        async save(m) { await restClient.put("/Benutzer", m); }
     };
 
     rollen = {
-        async getAll() { return await this.api.get("/Rollen"); },
-        async save(m) { await this.api.put("/Rollen", m); }
+        async getAll() { return await restClient.get("/Rollen"); },
+        async save(m) { await restClient.put("/Rollen", m); }
     };
 }
