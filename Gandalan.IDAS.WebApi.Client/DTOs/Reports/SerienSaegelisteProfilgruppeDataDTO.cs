ec1a79aa4a86d0f04b8c14307a0a9544be827781
DenisKoenig <denis.koenig@gandalan.de>
2022-05-30 11:14:30 +0200

    SägelisteProfilgruppeDataDTO RGB added

--- Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienSaegelisteProfilgruppeDataDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienSaegelisteProfilgruppeDataDTO.cs
@@ -10,6 +10,9 @@ namespace Gandalan.IDAS.WebApi.Data.DTOs.Reports
     {
         public string Titel { get; set; }
         public string Farbe { get; set; }
+        public string FarbeR { get; set; }
+        public string FarbeG { get; set; }
+        public string FarbeB { get; set; }
         public string Oberflaeche { get; set; }
         public string ProfilSchnittBild { get; set; }
         public string Gesamtbedarf { get; set; }
