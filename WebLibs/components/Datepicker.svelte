383f9b527104578392fec41c19dddfeb2ba9bf04
Adam Stachowicz <adam.stachowicz@fingo.info>
2022-08-05 15:07:46 +0200

    Add ESLint and fix warnings and errors. Add ESLint check to Actions

--- WebLibs/components/Datepicker.svelte
+++ WebLibs/components/Datepicker.svelte
@@ -1,14 +1,14 @@
 <script>
     export let Height = 30;
-    export let Placeholder = "";
-    export let Value = "";
+    export let Placeholder = '';
+    export let Value = '';
     export let Width = 120;
 
-    const backgroundNormal = "#FFFFFF";
-    const backgroundFalschesDatum = "#FF0000";
-    
-    let monate = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
-    let tageKurz = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"];
+    const backgroundNormal = '#FFFFFF';
+    const backgroundFalschesDatum = '#FF0000';
+
+    let monate = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+    let tageKurz = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
 
     let buttonStyle, divStyle, inputStyle;
     let background = backgroundNormal;
@@ -19,197 +19,156 @@
     let inputHeight = Height - 2;
     let inputWidth = Width - Height - 10;
 
-    let allowedNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    let allowedNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     let allowedTage = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let allowedSonderzeichen = ".";
-    let allowedFunctionalKeys = ["ArrowLeft", "ArrowRight", "Backspace", "Delete"];
+    let allowedSonderzeichen = '.';
+    let allowedFunctionalKeys = ['ArrowLeft', 'ArrowRight', 'Backspace', 'Delete'];
     let currentJahr = new Date().getFullYear();
     let currentMonat = monate[new Date().getMonth()];
     let wochenImMonat = [];
     let monatIndex = 0;
 
-    function alertFalschesDatum()
-    {
+    function alertFalschesDatum() {
         background = backgroundFalschesDatum;
         errorHidden = false;
         setFieldStyle();
     }
-    function backToNormal()
-    {
+    function backToNormal() {
         background = backgroundNormal;
         errorHidden = true;
         setFieldStyle();
     }
-    function checkGueltigesDatum()
-    {
+    function checkGueltigesDatum() {
         let error = false;
         let inhalt = Value.split(allowedSonderzeichen);
         let localTag = inhalt[0];
         let localMonat = inhalt[1];
 
-        if(inhalt.length == 1 && Value.length == 2)
-        {
+        if (inhalt.length == 1 && Value.length == 2) {
             Value = Value + allowedSonderzeichen;
         }
-        if(inhalt.length == 2 && localMonat.toLocaleString().length >= 2)
-        {
+        if (inhalt.length == 2 && localMonat.toLocaleString().length >= 2) {
             Value = Value + allowedSonderzeichen;
         }
 
         // Prüfung, ob der Monat korrekt eingegeben wurde
-        if(localMonat != "undefined" && (localMonat < 1 || localMonat > 12))
-        {
+        if (localMonat != 'undefined' && (localMonat < 1 || localMonat > 12)) {
             error = true;
-        }
-        else
-        {
+        } else {
             error = false;
         }
 
         // Prüfung, ob der Tag korrekt eingegeben wurde
-        if(localTag < 1 || localTag > 31)
-        {
+        if (localTag < 1 || localTag > 31) {
             error = true;
         }
 
-        if(localMonat != "undefined")
-        {
+        if (localMonat != 'undefined') {
             let localAllowedTage = allowedTage[inhalt[1]];
-            if(localAllowedTage == "undefined")
-            {
+            if (localAllowedTage == 'undefined') {
                 error = true;
             }
-            if(localTag > localAllowedTage)
-            {
+            if (localTag > localAllowedTage) {
                 error = true;
             }
         }
 
-        if(error)
-        {
+        if (error) {
             alertFalschesDatum();
-        }
-        else
-        {
+        } else {
             backToNormal();
         }
     }
-    function daysInMonth()
-    {
+    function daysInMonth() {
         wochenImMonat = [];
         monatIndex = monate.findIndex(item => item == currentMonat) + 1;
         let tageImMonat = new Date(currentJahr, monatIndex, 0).getDate();
         let localTagIndex = 0;
         let woche = [];
 
-        for(let counter = 0; counter < tageImMonat; counter++)
-        {
-            localTagIndex = new Date(currentJahr, monatIndex-1, counter).getDay();
-            if(counter == 0)
-            {
-                // Am Anfang müssen erstmal x Leertage in die Woche eingefügt werden, damit der Monat 
+        for (let counter = 0; counter < tageImMonat; counter++) {
+            localTagIndex = new Date(currentJahr, monatIndex - 1, counter).getDay();
+            if (counter == 0) {
+                // Am Anfang müssen erstmal x Leertage in die Woche eingefügt werden, damit der Monat
                 // am passenden Wochentag startet => das macht es in der Anzeigeschleife leichter
-                for(let bufferCounter = 0; bufferCounter < localTagIndex; bufferCounter++)
-                {
+                for (let bufferCounter = 0; bufferCounter < localTagIndex; bufferCounter++) {
                     woche = [...woche, ''];
                 }
             }
-            woche = [...woche, counter+1];
+            woche = [...woche, counter + 1];
 
-            if(woche.length >= 7)
-            {
+            if (woche.length >= 7) {
                 wochenImMonat = [...wochenImMonat, woche]
                 woche = [];
             }
-            if(counter == tageImMonat-1)
-            {
+            if (counter == tageImMonat - 1) {
                 wochenImMonat = [...wochenImMonat, woche]
                 woche = [];
             }
         }
     }
-    function ignoreInput(e)
-    {
+    function ignoreInput(e) {
         e.preventDefault();
         e.returnValue = false;
     }
-    function setFieldStyle()
-    {
-        buttonStyle = "background: transparent; border: 0px; height: " + buttonHeight + "px; margin-left: 10px; margin-right: 8px; margin-top: 0px; width: " + buttonHeight + "px;";
-        divStyle = "background: white; border: 0.5px solid lightgray; border-radius: 5px; display: flex; height: " + Height + "px; width: " + Width + "px;";
-        inputStyle = "background: " + background + "; border: 0px; height: " + inputHeight + "px; width: " + inputWidth + "px;";
+    function setFieldStyle() {
+        buttonStyle = `background: transparent; border: 0px; height: ${buttonHeight}px; margin-left: 10px; margin-right: 8px; margin-top: 0px; width: ${buttonHeight}px;`;
+        divStyle = `background: white; border: 0.5px solid lightgray; border-radius: 5px; display: flex; height: ${Height}px; width: ${Width}px;`;
+        inputStyle = `background: ${background}; border: 0px; height: ${inputHeight}px; width: ${inputWidth}px;`;
     }
-    function setJahr(selectedJahr)
-    {
+    function setJahr(selectedJahr) {
         currentJahr = selectedJahr.currentJahr;
         daysInMonth();
     }
-    function setMonat(selectedMonat)
-    {
+    function setMonat(selectedMonat) {
         currentMonat = selectedMonat.currentMonat;
         daysInMonth();
     }
-    function setPlaceholder(tag)
-    {
-        if(tag != "")
-        {
+    function setPlaceholder(tag) {
+        if (tag != '') {
             //Placeholder = getFormattedDate(tag);
         }
     }
-    function setValue(tag)
-    {
-        Value = new Date(currentJahr+"-"+currentMonat+"-"+tag);
+    function setValue(tag) {
+        Value = new Date(`${currentJahr}-${currentMonat}-${tag}`);
         datePickerHidden = true;
         backToNormal();
     }
-    function thisKeyDown(e)
-	{
-        if(allowedNumbers.includes(e.key) == true)
-        {
-            if(Value.length >= 10)
-            {
+    function thisKeyDown(e) {
+        if (allowedNumbers.includes(e.key) == true) {
+            if (Value.length >= 10) {
                 ignoreInput(e);
             }
             checkGueltigesDatum();
-        }
-        else if (e.key == allowedSonderzeichen)
-        {
+        } else if (e.key == allowedSonderzeichen) {
             // Kann nicht mit einer && Verknüpfung in die else if-Bedingung gepackt werden, da sonst gar kein Sonderzeichen mehr erlaubt ist... warum auch immer.
-            if(Value.split(allowedSonderzeichen).length >= 3)
-            {
+            if (Value.split(allowedSonderzeichen).length >= 3) {
                 ignoreInput(e);
             }
-        }
-        else if (allowedFunctionalKeys.includes(e.key) == true) { }
-        else 
-        {
+        } else if (allowedFunctionalKeys.includes(e.key) == true) {
+			return;
+		} else {
             ignoreInput(e);
         }
 	}
-    function getValueFormatted(oldValue)
-    {
+    function getValueFormatted(oldValue) {
         let localTag = (new Date(oldValue).getUTCDate()).toString();
-        let localMonat = (new Date(oldValue).getMonth()+1).toString();
+        let localMonat = (new Date(oldValue).getMonth() + 1).toString();
         let localJahr = new Date(oldValue).getUTCFullYear().toString();
 
-        if(localMonat.length < 2)
-        {
-            localMonat = "0" + localMonat;
+        if (localMonat.length < 2) {
+            localMonat = `0${localMonat}`;
         }
-        if(localTag.length < 2)
-        {
-            localTag = "0" + localTag;
+        if (localTag.length < 2) {
+            localTag = `0${localTag}`;
         }
-        return localTag + "." + localMonat + "." + localJahr;
+        return `${localTag }.${localMonat}.${localJahr}`;
     }
 
-    $:if(true)
-    {
-        setFieldStyle();
-        daysInMonth(currentMonat, currentJahr);
-    }
-    $:if(Value)
-    {
+    setFieldStyle();
+    daysInMonth(currentMonat, currentJahr);
+
+    $:if (Value) {
         Value = getValueFormatted(Value);
     }
 </script>
--- WebLibs/components/Inputbox.svelte
+++ WebLibs/components/Inputbox.svelte
@@ -1,6 +1,6 @@
 <script>
     export let AllowedDecimals = 2;
-    export let DecimalTrenner = ",";
+    export let DecimalTrenner = ',';
     export let KeyDownFunctionOnEnter, KeyDownFunctionOnTab;
     export let Height = 30;
     export let IsPflichtfeld = false;
@@ -8,255 +8,184 @@
     export let MinValue = 0;
     export let MaxValue = 0;
     export let Multiline = false;
-    export let Type = "text";
-    export let Value = "";
+    export let Type = 'text';
+    export let Value = '';
     export let Width = 120;
 
     let errorHidden = true;
-    let errorMessage = "";
-    let style = "height: " + Height + "px; width: " + Width + "px;";
-    let styleError = "width: " + Width + "px;";
+    let errorMessage = '';
+    let style = `height: ${Height}px; width: ${Width}px;`;
+    let styleError = `width: ${Width}px;`;
 
-    let allowedNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8,", "9"];
-    let allowedDecimalTrenner = [",", "."];
-    let allowedFunctionalKeys = ["ArrowLeft", "ArrowRight", "Backspace", "Delete"];
-    let allowedVorzeichen = "-";
+    let allowedNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let allowedDecimalTrenner = [',', '.'];
+    let allowedFunctionalKeys = ['ArrowLeft', 'ArrowRight', 'Backspace', 'Delete'];
+    let allowedVorzeichen = '-';
 
-    function checkInput(e)
-    {
-        switch(Type)
-        {
-            case "currency":
-            case "number":
+    function checkInput(e) {
+        switch (Type) {
+            case 'currency':
+            case 'number':
                 checkInputNumber(e);
                 break;
 
-            case "email":
+            case 'email':
                 checkInputEMail(e)
                 break;
         }
 
         executeAdditionalFunctions(e);
     }
-    function checkInputNumber(e)
-    {
+    function checkInputNumber(e) {
         let localValueString = Value.toLocaleString();
-        
+
         // Prüfung auf Ziffern
-        if(allowedNumbers.includes(e.key) == true)
-        {
-            if(isBetweenMinMax(e))
-            {
+        if (allowedNumbers.includes(e.key) == true) {
+            if (isBetweenMinMax(e)) {
                 let positionDezimalTrenner = localValueString.indexOf(DecimalTrenner)
-                if(positionDezimalTrenner > -1)
-                {
+                if (positionDezimalTrenner > -1) {
                     let decimals = localValueString.substring(positionDezimalTrenner);
-                    if(decimals.length > AllowedDecimals || (Type == "currency" && decimals.length > 2))
-                    {
+                    if (decimals.length > AllowedDecimals || (Type == 'currency' && decimals.length > 2)) {
                         ignoreInput(e);
                     }
                 }
-            }
-            else
-            {
+            } else {
                 ignoreInput(e);
             }
-        }
-        
-        // Prüfung auf Dezimaltrenner
-        else if (allowedDecimalTrenner.includes(e.key) == true)
-        {
-            if(localValueString.split(DecimalTrenner).length >= 2)
-            {
+        } else if (allowedDecimalTrenner.includes(e.key) == true) { // Prüfung auf Dezimaltrenner
+            if (localValueString.split(DecimalTrenner).length >= 2) {
+                ignoreInput(e);
+            } else if (e.key != DecimalTrenner) {
                 ignoreInput(e);
             }
-            else if(e.key != DecimalTrenner)
-            {
+        } else if (IsVorzeichenErlaubt && e.key == allowedVorzeichen) { // Prüfung auf Vorzeichen
+            if (!isBetweenMinMax(e)) {
                 ignoreInput(e);
-            }
-        }
-
-        // Prüfung auf Vorzeichen
-        else if (IsVorzeichenErlaubt && e.key == allowedVorzeichen)
-        {
-            if(!isBetweenMinMax(e))
-            {
+            } else if (localValueString.startsWith(e.key)) {
                 ignoreInput(e);
-            }
-            else if(localValueString.startsWith(e.key))
-            {
-                ignoreInput(e);
-            }
-            else
-            {
+            } else {
                 Value = e.key + Value;
                 ignoreInput(e);
             }
-        }
-
-        // Prüfung auf Funktionstasten wie [ENTF], [DEL], usw.
-        else if (allowedFunctionalKeys.includes(e.key) == true) { }
-
-        // Alles andere soll nicht erlaubt sein
-        else
-        {
+        } else if (allowedFunctionalKeys.includes(e.key) == true) { // Prüfung auf Funktionstasten wie [ENTF], [DEL], usw.
+			return;
+		} else { // Alles andere soll nicht erlaubt sein
             ignoreInput(e);
         }
     }
-    function checkInputEMail(e)
-    {
-        let mailParts = Value.split("@");
+    function checkInputEMail(e) {
+        let mailParts = Value.split('@');
         errorHidden = false;    // Pauschal einen Fehler anzeigen lassen - spart Codezeilen
 
-        if(mailParts[0].length > 64)
-        {
-            errorMessage = "Der Lokalteil der E-Mail Adresse (vor dem @-Zeichen) darf eine Maximallänge von 64 Zeichen nicht überschreiten."
-        }
-        else if(mailParts.length > 1 && mailParts[0].length < 1)
-        {
-            errorMessage = "Der Lokalteil der E-Mail Adresse (vor dem @-Zeichen) muss eine Mindestlänge von 1 Zeichen besitzen."
-        }
-        else if(mailParts.length > 1 && !mailParts[1].includes("."))
-        {
-            errorMessage = "Der Domainteil der E-Mail Adresse (nach dem @-Zeichen) muss einen Punkt (.) enthalten."
-        }
-        else if(Value.startsWith(".") || Value.endsWith("."))
-        {
-            errorMessage = "Die E-Mail Adresse darf mit einem Punkt weder beginnen noch enden."            
-        }
-        else if(Value.startsWith("@") || Value.endsWith("@"))
-        {
-            errorMessage = "Die E-Mail Adresse darf mit einem @-Zeichen weder beginnen noch enden."            
-        }
-        else if(!Value.includes("@") && e.key != "@")
-        {
-            errorMessage = "@-Zeichen muss enthalten sein."
-        }
-        else if(Value.length > 253)
-        {
-            errorMessage = "Maximallänge: 254 Zeichen.";
-        }
-        else if(Value.length < 6)
-        {
-            errorMessage = "Mindestlänge: 6 Zeichen.";
-        }
-        else
-        {
+        if (mailParts[0].length > 64) {
+            errorMessage = 'Der Lokalteil der E-Mail Adresse (vor dem @-Zeichen) darf eine Maximallänge von 64 Zeichen nicht überschreiten.'
+        } else if (mailParts.length > 1 && mailParts[0].length < 1) {
+            errorMessage = 'Der Lokalteil der E-Mail Adresse (vor dem @-Zeichen) muss eine Mindestlänge von 1 Zeichen besitzen.'
+        } else if (mailParts.length > 1 && !mailParts[1].includes('.')) {
+            errorMessage = 'Der Domainteil der E-Mail Adresse (nach dem @-Zeichen) muss einen Punkt (.) enthalten.'
+        } else if (Value.startsWith('.') || Value.endsWith('.')) {
+            errorMessage = 'Die E-Mail Adresse darf mit einem Punkt weder beginnen noch enden.'
+        } else if (Value.startsWith('@') || Value.endsWith('@')) {
+            errorMessage = 'Die E-Mail Adresse darf mit einem @-Zeichen weder beginnen noch enden.'
+        } else if (!Value.includes('@') && e.key != '@') {
+            errorMessage = '@-Zeichen muss enthalten sein.'
+        } else if (Value.length > 253) {
+            errorMessage = 'Maximallänge: 254 Zeichen.';
+        } else if (Value.length < 6) {
+            errorMessage = 'Mindestlänge: 6 Zeichen.';
+        } else {
             errorHidden = true;
-            errorMessage = ""; // einfach für die Sauberkeit
+            errorMessage = ''; // einfach für die Sauberkeit
         }
     }
-    function executeAdditionalFunctions(e)
-    {
-        switch(e.key)
-		{
-			case "Enter":
-                if(typeof(KeyDownFunctionOnEnter) != 'undefined')
-                {
+    function executeAdditionalFunctions(e) {
+        switch (e.key) {
+			case 'Enter':
+                if (typeof(KeyDownFunctionOnEnter) != 'undefined') {
                     KeyDownFunctionOnEnter();
                 }
 				break;
-			case "Tab":
-                if(typeof(KeyDownFunctionOnTab) != 'undefined')
-                {
+			case 'Tab':
+                if (typeof(KeyDownFunctionOnTab) != 'undefined') {
                     KeyDownFunctionOnTab();
                 }
 				break;
 		}
     }
-    function ignoreInput(e)
-    {
+    function ignoreInput(e) {
         e.preventDefault();
         e.returnValue = false;
     }
-    function isBetweenMinMax(e)
-    {
+    function isBetweenMinMax(e) {
         let isBetween = true;
         let localValueString = Value.toLocaleString()
 
-        if(e.key == allowedVorzeichen)
-        {
+        if (e.key == allowedVorzeichen) {
             localValueString = e.key + localValueString;
-        }
-        else
-        {
+        } else {
             localValueString = localValueString + e.key;
         }
 
         // Replace wird benötigt, da sonst der Vergleich das deutsche "," als Dezimaltrenner nicht erkennt und ignoriert.
-        localValueString = localValueString.replaceAll(",",".");
+        localValueString = localValueString.replaceAll(',', '.');
 
-        if(MinValue == MaxValue || MinValue > MaxValue)
-        {
+        if (MinValue == MaxValue || MinValue > MaxValue) {
             return isBetween;
-        }
-        else if(localValueString < MinValue)
-        {
+        } if (localValueString < MinValue) {
             Value = MinValue;
             isBetween = false;
-        }
-        else if(localValueString > MaxValue)
-        {
+        } else if (localValueString > MaxValue) {
             Value = MaxValue;
             isBetween = false;
         }
         return isBetween;
     }
-    function thisKeyUp()
-    {
+    function thisKeyUp() {
         setFieldStyle();
     }
-    function setFieldStyle()
-    {
-        console.log(Value);
-        if(IsPflichtfeld && Value != "")
-        {
-            style = style + " background: #f5fc99;"
-        }
-        else if(IsPflichtfeld && Value == "")
-        {
-            style = style + " background: #fc5d5d;"
+    function setFieldStyle() {
+        if (IsPflichtfeld && Value != '') {
+            style = `${style} background: #f5fc99;`
+        } else if (IsPflichtfeld && Value == '') {
+            style = `${style} background: #fc5d5d;`
         }
     }
 
-    $:if(Type)
-    {
+    $:if (Type) {
         Type = Type.toLocaleLowerCase();
-        switch(Type)
-        {
-            case "currency":
-            case "number":
-                style = style + " text-align: right;"
+        switch (Type) {
+            case 'currency':
+            case 'number':
+                style = `${style} text-align: right;`
                 break;
         }
     }
-    $:if(IsPflichtfeld)
-    {
+    $:if (IsPflichtfeld) {
         setFieldStyle();
     }
 </script>
 
 <!-- Datum -->
 {#if (Type == 'date')}
-<input type="date" style={style} on:keydown={checkInput} on:keyup={thisKeyUp} on bind:value={Value}/>
+    <input type="date" style={style} on:keydown={checkInput} on:keyup={thisKeyUp} on bind:value={Value}/>
 {/if}
 
 <!-- Nummerisch -->
 {#if (Type == 'number')}
-<input style={style} on:keydown={checkInput} on:keyup={thisKeyUp} bind:value={Value}/>
+    <input style={style} on:keydown={checkInput} on:keyup={thisKeyUp} bind:value={Value}/>
 {/if}
 
 <!-- Text -->
 {#if (Type == 'text' && !Multiline) || (Type == 'email')}
-<input style={style} on:keydown={checkInput} on:keyup={thisKeyUp} bind:value={Value}/>
+    <input style={style} on:keydown={checkInput} on:keyup={thisKeyUp} bind:value={Value}/>
 {/if}
 {#if (Type == 'text' && Multiline)}
-<textarea style={style} on:keydown={checkInput} bind:value={Value}/>
+    <textarea style={style} on:keydown={checkInput} bind:value={Value}/>
 {/if}
 
 <!-- Währung -->
 {#if (Type == 'currency')}
-<input style={style} on:keydown={checkInput} on:keyup={thisKeyUp} bind:value={Value}/>
+    <input style={style} on:keydown={checkInput} on:keyup={thisKeyUp} bind:value={Value}/>
 {/if}
 
 <div class="card" hidden={errorHidden} style={styleError}>
