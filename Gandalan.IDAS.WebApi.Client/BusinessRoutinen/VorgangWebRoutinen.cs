4383e0764781e429247b9ba5ee3ecfd5ec486e7b
René Gründig <rene.gruendig@gandalan.de>
2022-09-28 11:07:22 +0200

    Added UpdateVorgangStatusTableForFunction in VorgangWebRoutinen

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangWebRoutinen.cs
@@ -239,5 +239,14 @@ namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
             }
             return null;
         }
+        
+        public string UpdateVorgangStatusTableForFunction(Guid vorgangGuid, long mandantId, bool kundeGeaendert)
+        {
+            if (Login())
+            {
+                return Post("UpdateVorgangStatusTableForFunction?id=" + vorgangGuid.ToString() + "&mandantID=" + mandantId.ToString() + "&kundeGeaendert=" + kundeGeaendert, null);
+            }
+            return null;
+        }
     }
 }
