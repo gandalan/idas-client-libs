d7ec14baa07345d357415e75ea3cd6b39b3ccdd5
René Gründig <rene.gruendig@gandalan.de>
2022-10-19 11:31:18 +0200

    ISerienPositionsItemHelper um Parameter erweitert

--- Gandalan.IDAS.WebApi.Client/Contracts/AV/ISerienPositionsItemHelper.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/AV/ISerienPositionsItemHelper.cs
@@ -12,7 +12,7 @@ namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
     {
         Task<List<PositionSerieItemDTO>> GetSeriePositionItemsFromBeleg(BelegDTO beleg);
         Task<List<PositionSerieItemDTO>> GetSeriePositionItemsFromModus(BelegDTO beleg, List<PositionSerieItemDTO> items, SerieSuchenModus serieSuchenModus);
-        Task SaveSeriePositionItemsToSerie(BelegDTO beleg, List<PositionSerieItemDTO> items, bool autoFreigabeErlaubt, bool showDialog = true);
-        Task SaveSeriePositionItemsToSerieAuto(BelegDTO beleg, bool autoFreigabeErlaubt);
+        Task SaveSeriePositionItemsToSerie(BelegDTO beleg, List<PositionSerieItemDTO> items, bool autoFreigabeErlaubt, bool showDialog = true, bool timerReset = true);
+        Task SaveSeriePositionItemsToSerieAuto(BelegDTO beleg, bool autoFreigabeErlaubt, bool timerReset = true);
     }
 }
