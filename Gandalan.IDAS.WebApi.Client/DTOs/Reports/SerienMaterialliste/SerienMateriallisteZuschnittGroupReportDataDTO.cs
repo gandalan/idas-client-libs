e598a25d2ac6100c60f23d69959fca20e96c80bf
Denis König <denis.koenig@gandalan.de>
2022-10-24 09:36:09 +0200

    #7004 ISerienMateriallisteAufbereitenService

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienMaterialliste/SerienMateriallisteZuschnittGroupReportDataDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.Reports.SerienMaterialliste
+{
+    public class SerienMateriallisteZuschnittGroupReportDataDTO
+    {
+        public string Farbe { get; set; }
+        public List<SerienMateriallisteZuschnittReportDataDTO> ZuschnittItems { get; set; }
+    }
+}
---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienMaterialliste/SerienMateriallisteZuschnittReportDataDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.Reports.SerienMaterialliste
+{
+    public class SerienMateriallisteZuschnittReportDataDTO
+    {
+        public string Katalognummer { get; set; }
+        public string FarbKuerzel { get; set; }
+        public string FarbCode { get; set; }
+        public string FarbBezeichnung { get; set; }
+        public decimal Gesamtlaenge { get; set; }
+        public decimal PufferLaenge { get; set; }
+        public decimal Laenge { get; set; }
+        public decimal VerschnittLaenge { get; set; }
+        public int StangenCount { get; set; }
+        public string StangenLaenge { get; set; }
+        public string OberflaecheBezeichnung { get; set; }
+    }
+}
