6659edd18c8b21f07f364ea0a316bf4c12cdb1d1
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-12 16:49:07 +0100

    Add ApiVersionDTO

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.API
+{
+    public class ApiVersionDTO
+    {
+        public string Version { get; set; }
+        public string Environment { get; set; }
+    }
+}
