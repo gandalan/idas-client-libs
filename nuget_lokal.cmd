7472796c93c4ab71689d11a9db38dca5155ed280
Adam Stachowicz <adam.stachowicz@fingo.info>
2023-01-17 12:24:27 +0100

    Delete nuget.exe

--- nuget_lokal.cmd
+++ nuget_lokal.cmd
@@ -1,4 +1,5 @@
 @echo off
+echo You need to have recommended latest nuget.exe in your PATH environment variable. Download it from https://www.nuget.org/downloads
 nuget init Gandalan.IDAS.Crypto\bin c:\projects\0nuget\
 nuget init Gandalan.IDAS.Logging\bin c:\projects\0nuget\
 nuget init Gandalan.IDAS.WebApi.Client\bin c:\projects\0nuget\
