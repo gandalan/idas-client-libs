d4620c49dbd29a972084ad40b75c20e9b66ce520
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-18 10:14:52 +0100

    Add static generation of ApiVersionDTO from calling assembly

--- Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace Gandalan.IDAS.WebApi.Client.DTOs.API
@@ -8,5 +9,14 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.API
     {
         public string Version { get; set; }
         public string Environment { get; set; }
+        
+        /// <returns>The ApiVersionDTO of the object/method that invoked this method.</returns>
+        public static ApiVersionDTO FromThis()
+        {
+            var assembly = Assembly.GetCallingAssembly();
+            var env = System.Environment.GetEnvironmentVariable("GDL_ENVIRONMENT") ?? "GDL_ENVIRONMENT not found";
+            var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? $"No informational version found for {assembly.FullName}";
+            return new ApiVersionDTO() { Version = version, Environment = env };
+        }
     }
 }
