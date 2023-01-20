29107dc5f4f4095fd482f26a5f34eff7a463fb51
René Gründig <rene.gruendig@gandalan.de>
2023-01-20 08:48:26 +0100

    Added FakturierbarerBelegeDTO

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Rechnung/FakturierbarerBelegeDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.Rechnung
+{
+    public class FakturierbarerBelegeDTO
+    {
+        public Guid VorgangGuid { get; set; }
+        public Guid BelegGuid { get; set; }
+        public Guid KontaktGuid { get; set; }
+
+        public long Vorgangsnummer { get; set; }
+        public int PosAnzahl { get; set; }
+        public double GesamtBetrag { get; set; }
+        public string Kundennummer { get; set; }
+        public string Kundenname { get; set; }
+
+    }
+}
