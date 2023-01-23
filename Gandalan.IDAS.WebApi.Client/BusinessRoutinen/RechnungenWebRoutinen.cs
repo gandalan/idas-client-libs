c812ec8c821efac48c120bb23cc1ee862dcd72ac
René Gründig <rene.gruendig@gandalan.de>
2023-01-23 09:48:20 +0100

    added RechnungenWebRoutinen and IRechnungenService

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/RechnungenWebRoutinen.cs
@@ -0,0 +1,28 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.Client.DTOs.Rechnung;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class RechnungenWebRoutinen : WebRoutinenBase
+    {
+        public RechnungenWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public List<FakturierbarerBelegeDTO> GetAllABFakturierbar()
+        {
+            if (Login())
+            {
+                return Get<List<FakturierbarerBelegeDTO>>("Rechnungen/GetABFakturierbar");
+            }
+            return null;
+        }
+
+        public async Task<List<FakturierbarerBelegeDTO>> GetAllABFakturierbarAsync()
+        {
+            return await Task.Run(() => GetAllABFakturierbar());
+        }
+    }
+}
