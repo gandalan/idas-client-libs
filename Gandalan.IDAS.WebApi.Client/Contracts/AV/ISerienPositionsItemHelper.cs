a9d9d05dcdf8988fab0aa0b6fc0db0e32453f823
ewu-nma <niklas.martin@ewu-it.de>
2023-01-23 13:01:21 +0100

    #8737 #8738 #8739 ISerienPositionsItemHelper remove unnecessary parameters

--- Gandalan.IDAS.WebApi.Client/Contracts/AV/ISerienPositionsItemHelper.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/AV/ISerienPositionsItemHelper.cs
@@ -12,7 +12,7 @@ namespace Gandalan.IDAS.Client.Contracts.Contracts.AV
     {
         Task<List<PositionSerieItemDTO>> GetSeriePositionItemsFromBeleg(BelegDTO beleg);
         Task<List<PositionSerieItemDTO>> GetSeriePositionItemsFromModus(BelegDTO beleg, List<PositionSerieItemDTO> items, SerieSuchenModus serieSuchenModus);
-        Task SaveSeriePositionItemsToSerie(BelegDTO beleg, List<PositionSerieItemDTO> items, bool autoFreigabeErlaubt, bool showDialog = true, bool timerReset = true);
-        Task SaveSeriePositionItemsToSerieAuto(BelegDTO beleg, bool autoFreigabeErlaubt, bool timerReset = true);
+        Task SaveSeriePositionItemsToSerie(BelegDTO beleg, List<PositionSerieItemDTO> items);
+        Task SaveSeriePositionItemsToSerieAuto(BelegDTO beleg);
     }
 }
