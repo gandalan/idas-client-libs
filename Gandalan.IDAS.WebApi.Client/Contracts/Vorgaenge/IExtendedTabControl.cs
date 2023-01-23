887b413f6954613d31bc2ec19a7ca979e54608dd
ewu-nma <niklas.martin@ewu-it.de>
2023-01-23 13:03:09 +0100

    #8123 #8738 IExtendedTabControl to mark extended TabControls

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/Vorgaenge/IExtendedTabControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.Vorgaenge
+{
+    internal interface IExtendedTabControl
+    {
+    }
+}
