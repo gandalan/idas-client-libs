44d45e1108d7e91825b2cb6aeda14f9e04a4c6a1
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-24 16:21:28 +0100

    #8610 Add INachrichtenService and move NachrichtenDTO to DTOs

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Nachrichten/NachrichtenDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gandalan.IDAS.WebApi.Client.DTOs.Nachrichten
+{
+    public class NachrichtenDTO
+    {
+        public Guid NachrichtGuid { get; set; }
+        public Guid MandantGuid { get; set; }
+        public Guid BesitzerMandantGuid { get; set; }
+        public string Context { get; set; }
+        public string Nachricht { get; set; }
+        public bool IstAktiv { get; set; }
+        public DateTime? GueltigAb { get; set; }
+        public DateTime? GueltigBis { get; set; }
+    }
+}
