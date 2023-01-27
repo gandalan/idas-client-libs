8d4e708c0e67798ee0e6b7a1ef7a5beb716f4524
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-27 17:02:39 +0100

    Add custom AssemblyBuildDateAttribute

---
+++ Gandalan.IDAS.WebApi.Client/Extensions/AssemblyBuildDateAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gandalan.IDAS.WebApi.Client.Extensions
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public sealed class AssemblyBuildDateAttribute : Attribute
+    {
+        private readonly string buildDate;
+        public string BuildDate => buildDate;
+        public AssemblyBuildDateAttribute(string buildDate)
+        {
+            this.buildDate = buildDate;
+        }
+    }
+}
