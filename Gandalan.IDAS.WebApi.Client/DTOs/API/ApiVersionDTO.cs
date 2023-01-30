e72562d9ecd401c86cb2e17c49705af525eb2387
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-30 09:58:44 +0100

    #8991 Add BuildDate to ApiVersion

--- Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace Gandalan.IDAS.WebApi.Client.DTOs.API
 {
@@ -6,12 +7,33 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.API
     {
         public string Version { get; set; }
         public string Environment { get; set; }
-        
+        public string BuildDate { get; set; }
+
         public static ApiVersionDTO FromAssembly(Assembly assembly)
         {
             var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? $"No informational version found for {assembly.FullName}";
             var env = System.Environment.GetEnvironmentVariable("GDL_ENVIRONMENT") ?? "Development";
-            return new ApiVersionDTO() { Version = version, Environment = env };
+            var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
+
+            // BuildDate format: "BuildDate=2023-01-30T09:00:00;"
+            var buildDate = "Build-Datum nicht verfügbar";
+            try
+            {
+                if (!string.IsNullOrEmpty(description) && description.Contains("BuildDate="))
+                {
+                    var startIndex = description.IndexOf("BuildDate=");
+                    var endIndex = description.IndexOf(';', startIndex);
+                    var substring = description.Substring(startIndex, endIndex - startIndex);
+                    var split = substring.Split('=')[1].Split(';')[0];
+                    var date = DateTime.Parse(split);
+                    buildDate = date.ToString("dd.MM.yyyy hh:mm");
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return new ApiVersionDTO() { Version = version, Environment = env, BuildDate = buildDate};
         }
     }
 }
