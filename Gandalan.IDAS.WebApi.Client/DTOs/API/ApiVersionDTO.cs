44d68259c772ca8d5df8b0a32fd7648ac8235935
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-30 11:19:27 +0100

    #Refa BuildDate extraction in ApiVersionDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
@@ -11,31 +11,36 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.API
 
         public static ApiVersionDTO FromAssembly(Assembly assembly)
         {
-            var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? $"No informational version found for {assembly.FullName}";
+            var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? $"Keine Version gefunden für: {assembly.FullName}";
             var env = System.Environment.GetEnvironmentVariable("GDL_ENVIRONMENT") ?? "Development";
-            var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
-
-            // BuildDate format: "BuildDate=2023-01-30T09:00:00;"
-            string buildDate;
-            try
-            {
-                if (string.IsNullOrEmpty(description) || !description.Contains("BuildDate="))
-                {
-                    throw new Exception("BuildDate not found in assembly description");
-                }
-                var startIndex = description.IndexOf("BuildDate=");
-                var endIndex = description.IndexOf(';', startIndex);
-                var substring = description.Substring(startIndex, endIndex - startIndex);
-                var split = substring.Split('=')[1].Split(';')[0];
-                var date = DateTime.Parse(split);
-                buildDate = date.ToString("dd.MM.yyyy hh:mm");
-            }
-            catch (Exception)
-            {
-                buildDate = "Build-Datum nicht verfügbar";
-            }
+            var buildDate = ExtractBuildDateFromAssembly(assembly) ?? "Build-Datum nicht verfügbar";
 
             return new ApiVersionDTO() { Version = version, Environment = env, BuildDate = buildDate };
         }
+
+        #region private methods
+        /// <summary>
+        /// Assembly description should contain the build date in following format: <code>"BuildDate=2023-01-30T09:00:00;"</code>
+        /// </summary>
+        private static string ExtractBuildDateFromAssembly(Assembly assembly)
+        {
+            var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
+
+            if (string.IsNullOrEmpty(description) || !description.Contains("BuildDate="))
+                return null;
+
+            var startIndex = description.IndexOf("BuildDate=");
+            var endIndex = description.IndexOf(';', startIndex);
+            if (startIndex < 0 || endIndex < 0)
+                return null;
+
+            var substring = description.Substring(startIndex, endIndex - startIndex);
+            var split = substring.Split('=')[1].Split(';')[0];
+            if (!DateTime.TryParse(split, out var date))
+                return null;
+
+            return date.ToString("dd.MM.yyyy hh:mm"); ;
+        }
+        #endregion
     }
 }
