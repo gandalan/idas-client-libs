e848250e816bc578219d2222bd524e5fa4ee484e
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-01-30 10:43:41 +0100

    Update buildDate read in ApiVersionDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/API/ApiVersionDTO.cs
@@ -16,24 +16,26 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.API
             var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
 
             // BuildDate format: "BuildDate=2023-01-30T09:00:00;"
-            var buildDate = "Build-Datum nicht verfügbar";
+            string buildDate;
             try
             {
-                if (!string.IsNullOrEmpty(description) && description.Contains("BuildDate="))
+                if (string.IsNullOrEmpty(description) || !description.Contains("BuildDate="))
                 {
-                    var startIndex = description.IndexOf("BuildDate=");
-                    var endIndex = description.IndexOf(';', startIndex);
-                    var substring = description.Substring(startIndex, endIndex - startIndex);
-                    var split = substring.Split('=')[1].Split(';')[0];
-                    var date = DateTime.Parse(split);
-                    buildDate = date.ToString("dd.MM.yyyy hh:mm");
+                    throw new Exception("BuildDate not found in assembly description");
                 }
+                var startIndex = description.IndexOf("BuildDate=");
+                var endIndex = description.IndexOf(';', startIndex);
+                var substring = description.Substring(startIndex, endIndex - startIndex);
+                var split = substring.Split('=')[1].Split(';')[0];
+                var date = DateTime.Parse(split);
+                buildDate = date.ToString("dd.MM.yyyy hh:mm");
             }
             catch (Exception)
             {
+                buildDate = "Build-Datum nicht verfügbar";
             }
 
-            return new ApiVersionDTO() { Version = version, Environment = env, BuildDate = buildDate};
+            return new ApiVersionDTO() { Version = version, Environment = env, BuildDate = buildDate };
         }
     }
 }
