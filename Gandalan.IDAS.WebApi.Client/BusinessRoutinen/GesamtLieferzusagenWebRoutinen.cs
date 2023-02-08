5fbf8ad4981b2623ad33b87be7e970a958a76056
ewu-nma <niklas.martin@ewu-it.de>
2023-02-08 15:02:35 +0100

    #8864 #8995 #8865 GesamtBedarf WebRoutinen

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/GesamtLieferzusagenWebRoutinen.cs
@@ -0,0 +1,28 @@
+﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    public class GesamtLieferzusagenWebRoutinen : WebRoutinenBase
+    {
+        public GesamtLieferzusagenWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public List<GesamtLieferzusageDTO> Get(DateTime? stichTag = null)
+        {
+            if (Login())
+            {
+                return Get<List<GesamtLieferzusageDTO>>($"GesamtLieferzusagen?stichTag={stichTag?.ToString("o")}");
+            }
+
+            return null;
+        }
+
+        public async Task<List<GesamtLieferzusageDTO>> GetAsync(DateTime? stichTag = null) => await Task.Run(() => Get(stichTag));
+
+    }
+}
---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Produktion/GesamtbedarfOptionen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.DTO.Gesamtbedarf
+{
+    [Flags]
+    public enum ZusammenfassungsOptionen
+    {
+        Keine = 0,
+        Lieferdatum = 1,
+        Serienzuordnung = 2,
+        Artikelnummer_Frabe_Oberflaeche = 4,
+        Vorgang = 8
+    }
+
+    [Flags]
+    public enum SchnittoptimierungsOptionen
+    {
+        Keine = 0,
+        Lieferdatum = 1,
+        Serie = 2,
+        Frabe_Oberflaeche = 4,
+    }
+}
