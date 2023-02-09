b536d9a38bd4c805c56b4f51837c408c1ef7fa86
René Gründig <rene.gruendig@gandalan.de>
2023-02-09 11:27:11 +0100

    Added ISammelrechnungenService

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/DataServices/ISammelrechnungenService.cs
@@ -0,0 +1,18 @@
+﻿using Gandalan.IDAS.WebApi.Client.DTOs.Rechnung;
+using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.DataServices
+{
+    public interface ISammelrechnungenService
+    {
+        Task ErstelleSammelrechnungenAsync(CreateSammelrechnungDTO dto);
+        Task<List<SammelrechnungListItemDTO>> GetSammelrechnungenAsync();
+        Task<SammelrechnungDTO> GetSammelrechnungAsync(Guid guid);
+        Task<List<BelegeInfoDTO>> GetPossibleSammelrechnungRechnungenAsync();
+        Task<SammelrechnungDTO> UpdateSammelrechungAsync(SammelrechnungDTO dto);
+    }
+}
