c055864569b3d73f127d60bec221b540d6e6b6dd
René Gründig <rene.gruendig@gandalan.de>
2023-02-10 12:05:07 +0100

    Added Prop Kundennummer to SammelrechnungListItemDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/Rechnung/SammelrechnungListItemDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Rechnung/SammelrechnungListItemDTO.cs
@@ -9,6 +9,7 @@ namespace Gandalan.IDAS.WebApi.Client.DTOs.Rechnung
         public Guid SammelrechnungGuid { get; set; }
         public long SammelrechnungNummer { get; set; }
         public string Kundenname { get; set; }
+        public string Kundennummer { get; set; }
         public DateTime ErstellDatum { get; set; }
         public int AnzahlPositionen { get; set; }
         public decimal GesamtBetrag { get; set; }
