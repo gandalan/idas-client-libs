cabc6b4783911ffadbcc9ac61e856d3b821adb3e
ewu-nma <niklas.martin@ewu-it.de>
2023-03-01 15:59:26 +0100

    GesamtLieferzusageBuchungDTO

---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Produktion/GesamtLieferzusageBuchungDTO.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Gandalan.IDAS.WebApi.DTO
+{
+    public class GesamtLieferzusageBuchungDTO : ICloneable
+    {
+        public Guid GesamtLieferzusageBuchungGuid { get; set; }
+        public Guid MandantGuid { get; set; }
+        public Guid GesamtMaterialbedarfGuid { get; set; }
+        public decimal Stueckzahl { get; set; }
+        public decimal Laufmeter { get; set; }
+        public DateTime Buchungsdatum { get; set; }
+        
+        public object Clone()
+        {
+            return new GesamtLieferzusageBuchungDTO()
+            {
+                GesamtLieferzusageBuchungGuid = GesamtLieferzusageBuchungGuid,
+                MandantGuid = MandantGuid,
+                GesamtMaterialbedarfGuid = GesamtMaterialbedarfGuid,
+                Stueckzahl = Stueckzahl,
+                Laufmeter = Laufmeter,
+                Buchungsdatum = Buchungsdatum
+            };
+        }
+    }
+}
+
