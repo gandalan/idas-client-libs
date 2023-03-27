2bd4b615285a9b449b9e17be1acc460f7a0ddafe
ewu-nma <niklas.martin@ewu-it.de>
2023-03-27 09:31:41 +0200

    #8890 Zwischenstand

--- Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IOverlayNotify.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IOverlayNotify.cs
@@ -5,7 +5,8 @@
         void ShowOverlay(string message, UserNotifyMessageType type, object control);
         void ShowOverlay(object uiElement, UserNotifyMessageType type, object control);
         void HideOverlay(object control);
-        void ToogleOverlay(object control);
+        void ToggleOverlay(object control);
         void OpenOverlay(object control);
+        void CloseOverlay(object control);
     }
 }
