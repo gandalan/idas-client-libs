cd8514ce3e346c9cb3983cdba341d7728fb1d7fa
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-04-25 14:22:08 +0200

    Add IVorgangCommand

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/Vorgaenge/IVorgangCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Gandalan.IDAS.WebApi.Client.Contracts.Vorgaenge;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.Vorgaenge
+{
+    public abstract class IVorgangCommand : ICommand, INotifyPropertyChanged
+    {
+        public abstract event EventHandler CanExecuteChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public abstract string Caption { get; set; }
+        public abstract string ToolTip { get; set; }
+
+        public bool IsExecuting { get; set; } = false;
+
+        public bool CanExecute(object parameter)
+        {
+            var vorgang = parameter as VorgangDTO;
+            if (parameter == null)
+                return false;
+            return CanExecute(vorgang);
+        }
+
+        public abstract bool CanExecute(VorgangDTO vorgang);
+
+        public void Execute(object parameter)
+        {
+            var vorgang = parameter as VorgangDTO;
+            if (parameter == null)
+            {
+                throw new ArgumentNullException("Parameter muss ein VorgangDTO sein");
+            }
+            Execute(vorgang);
+        }
+
+        public abstract void Execute(VorgangDTO vorgang);
+    }
+}
