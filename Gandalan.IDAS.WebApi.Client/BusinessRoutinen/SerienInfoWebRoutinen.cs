5a0aec66d725ae2f27dc4c7d0eca8c60babf7c70
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-05-22 11:46:50 +0200

    #9432 Add SerienInfoWebRoutinen

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/SerienInfoWebRoutinen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.Client.DTOs.Belege;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
+{
+    internal class SerienInfoWebRoutinen : WebRoutinenBase
+    {
+        public SerienInfoWebRoutinen(IWebApiConfig settings) : base(settings)
+        {
+        }
+
+        public List<BelegSerienInfoDTO> GetBelegSerienInfos(List<Guid> belegGuids)
+        {
+            if (Login())
+            {
+                return Post<List<BelegSerienInfoDTO>>($"BelegSerienInfo", belegGuids);
+            }
+            return null;
+        }
+
+        public List<BelegSerienInfoDTO> GetVorgangSerienInfos(List<Guid> vorgangGuids)
+        {
+            if (Login())
+            {
+                return Post<List<BelegSerienInfoDTO>>($"BelegSerienInfo/GetByVorgangGuids", vorgangGuids);
+            }
+            return null;
+        }
+    }
+}
