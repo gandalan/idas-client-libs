1a568ba35f5391d280a59fbc3a2c603712eef4dd
Phil Reif <philipp.reif@gandalan.de>
2023-06-01 11:14:21 +0200

    Extend IFluxAction for retry/call mode handling

--- Gandalan.IDAS.WebApi.Client/Contracts/Flux/IFluxAction.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Flux/IFluxAction.cs
@@ -1,7 +1,21 @@
 ﻿namespace Gandalan.IDAS.Client.Contracts.Flux
 {
+    public enum CallMode
+    {
+        // retry until success or max retries reached, then throw exception if still not successful
+        Guaranteed,
+        // retry until success or max retries reached
+        BestEffort,
+        // try once, ignore failures
+        FireAndForget
+    };
+
     public interface IFluxAction
     {
+        CallMode CallMode { get; set; }
+        int MaxRetries { get; set; }
+        int RetryCount { get; set; }
+
         object Payload { get; }
         string Verb { get; }
 
