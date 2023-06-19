3bcf87f29c6b441e0fb32231118842e0f589464b
René Gründig <rene.gruendig@gandalan.de>
2023-06-19 08:29:20 +0200

    SerieAuslastungDTO kann Summeneintrag erstellen

--- Gandalan.IDAS.WebApi.Client/DTOs/AV/SerieAuslastungDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/AV/SerieAuslastungDTO.cs
@@ -1,7 +1,54 @@
-﻿namespace Gandalan.IDAS.WebApi.DTO
+﻿using System.Collections.Generic;
+
+namespace Gandalan.IDAS.WebApi.DTO
 {
     public class SerieAuslastungDTO
     {
+        public SerieAuslastungDTO()
+        {
+        }
+
+        public SerieAuslastungDTO(VirtualSerieWithAuslastungDTO dto)
+        {
+            Produktfamilie = "Summe:";
+            IstSumme = true;
+            foreach (var vAuslastung in dto.Auslastungen)
+            {
+                Anzahl += vAuslastung.Anzahl;
+                Reserviert += vAuslastung.Reserviert;
+                Arbeitsminuten += vAuslastung.Arbeitsminuten;
+                ArbeitsminutenReserviert += vAuslastung.ArbeitsminutenReserviert;
+                Elementgewicht += vAuslastung.Elementgewicht;
+                ElementgewichtReserviert += vAuslastung.ElementgewichtReserviert;
+                Rahmenanzahl += vAuslastung.Rahmenanzahl;
+                RahmenanzahlReserviert += vAuslastung.RahmenanzahlReserviert;
+                AnzahlMax += vAuslastung.AnzahlMax;
+                KapazitaetBelegt += vAuslastung.KapazitaetBelegt;
+                KapazitaetMax += vAuslastung.KapazitaetMax;
+            }
+        }
+
+        public SerieAuslastungDTO(IList<SerieAuslastungDTO> value)
+        {
+            Produktfamilie = "Summe:";
+            IstSumme = true;
+            foreach (var vAuslastung in value)
+            {
+                Anzahl += vAuslastung.Anzahl;
+                Reserviert += vAuslastung.Reserviert;
+                Arbeitsminuten += vAuslastung.Arbeitsminuten;
+                ArbeitsminutenReserviert += vAuslastung.ArbeitsminutenReserviert;
+                Elementgewicht += vAuslastung.Elementgewicht;
+                ElementgewichtReserviert += vAuslastung.ElementgewichtReserviert;
+                Rahmenanzahl += vAuslastung.Rahmenanzahl;
+                RahmenanzahlReserviert += vAuslastung.RahmenanzahlReserviert;
+                AnzahlMax += vAuslastung.AnzahlMax;
+                KapazitaetBelegt += vAuslastung.KapazitaetBelegt;
+                KapazitaetMax += vAuslastung.KapazitaetMax;
+            }
+        }
+
+        public bool IstSumme { get; set; }
         public string Produktfamilie { get; set; }
         public int Anzahl { get; set; }
         public int Reserviert { get; set; }
