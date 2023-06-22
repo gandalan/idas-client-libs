f03e874e6391787b93072bc80a733625dc1e7383
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-06-22 16:00:18 +0200

    Add sticky to IUserNotify

--- Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IUserNotify.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IUserNotify.cs
@@ -5,10 +5,11 @@ namespace Gandalan.Client.Contracts.UIServices
     public interface IUserNotify
     {
         bool Active { get; set; }
+        bool StickyActive { get; set; }
         UserNotifyMessageType MessageType { get; }
 
         void ShowException(Exception ex);
-        void ShowMessage(string message, UserNotifyMessageType type);
+        void ShowMessage(string message, UserNotifyMessageType type, bool sticky = false);
         void ClearMessage();
 
         event EventHandler NotificationChanged;
