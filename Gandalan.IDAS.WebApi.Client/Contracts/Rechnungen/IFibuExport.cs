59138395a8d06e64ea8e4da40869ea5143c57810
Phil Reif <philipp.reif@gandalan.de>
2023-07-28 16:16:30 +0200

    IFubExport added

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/Rechnungen/IFibuExport.cs
@@ -0,0 +1,18 @@
+﻿using Gandalan.IDAS.WebApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.Rechnungen
+{
+    public interface IFibuExport
+    {
+        void ExportFibu(IList<IFibuRecord> data);
+    }
+
+    public interface IFibuRecord
+    {
+        IList<PropertyInfo> GetExportProperties();
+    }
+}
