02a8c529b2e0e1eaa2ed28a2c92f1f287e6416d3
Phil Reif <philipp.reif@gandalan.de>
2023-07-31 07:20:16 +0200

    Extend IFibuExport

--- Gandalan.IDAS.WebApi.Client/Contracts/Rechnungen/IFibuExport.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Rechnungen/IFibuExport.cs
@@ -1,18 +1,23 @@
-﻿using Gandalan.IDAS.WebApi.DTO;
+﻿using Gandalan.IDAS.WebApi.Client.DTOs.Rechnung;
+using Gandalan.IDAS.WebApi.DTO;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Gandalan.IDAS.WebApi.Client.Contracts.Rechnungen
 {
     public interface IFibuExport
     {
-        void ExportFibu(IList<IFibuRecord> data);
+        Task ExportFibu(IList<IFibuRecord> data);
+        IFibuRecord CreateRecord();
     }
 
     public interface IFibuRecord
     {
         IList<PropertyInfo> GetExportProperties();
+        void FromSammelrechnung(SammelrechnungListItemDTO sammelrechnung);
+        void FromRechnung(BelegeInfoDTO rechnung);
     }
 }
