23dada60007e465568f87b37ae07fbf655164479
ewu-nma <niklas.martin@ewu-it.de>
2023-08-07 14:04:28 +0200

    Move NonEUCountriesExtensionHelper to idas-client-libs as CountriesExtensionHelper

---
+++ Gandalan.IDAS.WebApi.Client/Util/CountriesExtensionHelper.cs
@@ -0,0 +1,56 @@
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace Gandalan.IDAS.WebApi.Client.Util
+{
+    public static class CountriesExtensionHelper
+    {
+        #region Country names
+        private static readonly string[] _euCountryCodes = { 
+            // keep DE + AT in front for better performance with most of requests
+            "Deutschland", "DE", "D",
+            "Österreich", "AT", "A",
+
+            "Belgien", "België", "Belgique", "Belgien", "BE",
+            "Bulgarien", "България", "BG",
+            "Dänemark", "Danmark", "DK",
+            "Estland", "Eesti", "EE",
+            "Finnland", "Suomi", "Finland", "FI",
+            "Frankreich", "France", "FR", "F",
+            "Griechenland", "Ελλάδα", "Ελλάς", "GR",
+            "Irland", "Éire", "Ireland", "IE",
+            "Italien", "Italia", "IT",
+            "Kroatien", "Hrvatska", "HR",
+            "Lettland", "Latvija", "LV",
+            "Litauen", "Lietuva", "LT",
+            "Luxemburg", "Lëtzebuerg", "Luxemburg", "Luxembourg", "LU", "L",
+            "Malta", "Malta", "MT",
+            "Niederlande", "Nederland", "NL",
+            "Polen", "Polska", "PL",
+            "Portugal", "Portugal", "PT",
+            "Rumänien", "România", "RO",
+            "Schweden", "Sverige", "SE",
+            "Slowakei", "Slovensko", "SK",
+            "Slowenien", "Slovenija", "SI",
+            "Spanien", "España", "ES",
+            "Tschechien", "Česko", "CZ",
+            "Ungarn", "Magyarország", "HU",
+            "Zypern", "Κύπρος", "Kıbrıs", "CY"
+        };
+        #endregion
+        public static bool LandIsEUCountry(string land)
+        {
+            return string.IsNullOrEmpty(land) ||
+                   _euCountryCodes.Any(t => land.Equals(t, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static bool IsInland(string land, IEnumerable<string> inlandsLaender)
+        {
+            return string.IsNullOrEmpty(land) ||
+                   inlandsLaender.Any(x => land.Equals(x, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
