dff7b7fa7f391ff1db9b2641c9815e7865f95b65
René Gründig <rene.gruendig@gandalan.de>
2023-09-21 11:52:38 +0200

    Add new InheritenseMode for Settings

---
+++ Gandalan.IDAS.WebApi.Client/Contracts/Settings/InheritenseMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gandalan.IDAS.WebApi.Client.Contracts.Settings
+{
+    public interface Inheritense
+    {
+        InheritenseMode Mode { get; set; }
+    }
+
+    public enum InheritenseMode
+    {
+        Default = 0,
+        ReadOnly = 1,
+        ReadWrite = 2,
+        ReadOnlyInherited = 4,
+    }
+}
