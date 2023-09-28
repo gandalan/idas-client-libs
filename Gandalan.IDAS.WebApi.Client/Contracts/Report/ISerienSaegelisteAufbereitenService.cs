be65ae32a0b35699659f8c8721342ceb96bf41f0
ewu-sra <57943755+ewu-sra@users.noreply.github.com>
2023-09-28 08:19:55 +0200

    Erweiterung ISeriensaegelisteAufbereitenService

--- Gandalan.IDAS.WebApi.Client/Contracts/Report/ISerienSaegelisteAufbereitenService.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Report/ISerienSaegelisteAufbereitenService.cs
@@ -1,6 +1,7 @@
 ﻿using Gandalan.IDAS.WebApi.Data.DTOs.Reports;
 using Gandalan.IDAS.WebApi.DTO;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Gandalan.IDAS.Client.Contracts.Contracts.Report
@@ -10,5 +11,7 @@ namespace Gandalan.IDAS.Client.Contracts.Contracts.Report
         Task<List<SerienSaegelisteDataDTO>> Aufbereiten(SerieDTO serie, IList<BelegPositionAVDTO> elemente);
         Task<List<SerienSaegelisteDataDTO>> Aufbereiten(VorgangDTO vorgang, IList<BelegPositionAVDTO> elemente);
         Task<List<SerienSaegelisteDataDTO>> getBySerie(SerieDTO serie, IList<BelegPositionAVDTO> elemente, IList<string> winkel, string titel);
+        Task<List<SerienSaegelisteDataDTO>> getBySerie(SerieDTO serie, IList<BelegPositionAVDTO> elemente, IOrderedEnumerable<KeyValuePair<string, MaterialbedarfDTO>> orderedZuschnitte, IList<string> winkel, string titel);
+
     }
 }
