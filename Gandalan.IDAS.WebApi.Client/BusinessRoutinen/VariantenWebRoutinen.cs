cca4e9e57668c795b915422a76c17b931a6237e5
Denis König <denis.koenig@gandalan.de>
2023-10-06 10:32:41 +0200

    GetAllVariantenChanges changedSince

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VariantenWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VariantenWebRoutinen.cs
@@ -1,4 +1,4 @@
-﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.Client.Contracts.Contracts;
 using Gandalan.IDAS.Web;
 using Gandalan.IDAS.WebApi.DTO;
 using System;
@@ -10,23 +10,31 @@ namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
     {
         public VariantenWebRoutinen(IWebApiConfig settings) : base(settings) { }
 
-        public async Task<VarianteDTO[]> GetAllAsync() 
+        public async Task<VarianteDTO[]> GetAllAsync()
             => await GetAsync<VarianteDTO[]>("Variante?includeUIDefs=true&maxLevel=99");
 
         public async Task<Guid[]> GetAllGuidsAsync()
             => await GetAsync<Guid[]>("Variante/GetAllGuids");
+        public async Task<Guid[]> GetAllVariantenChanges(DateTime? changedSince = null)
+        {
+            if (changedSince.HasValue && changedSince.Value > DateTime.MinValue)
+                return await GetAsync<Guid[]>("Variante/GetAllVariantenChanges?changedSince=" + changedSince.Value.ToString("o"));
+            else
+                return await GetAsync<Guid[]>("Variante/GetAllGuids");
+        }
 
-        public async Task<VarianteDTO> GetAsync(Guid varianteGuid, bool includeUIDefs = true) 
+
+        public async Task<VarianteDTO> GetAsync(Guid varianteGuid, bool includeUIDefs = true)
             => await GetAsync<VarianteDTO>($"Variante/{varianteGuid}?includeUIDefs={includeUIDefs}&maxLevel=99");
 
-        public async Task<VarianteDTO> SaveVarianteAsync(VarianteDTO variante) 
+        public async Task<VarianteDTO> SaveVarianteAsync(VarianteDTO variante)
             => await PutAsync<VarianteDTO>($"Variante/{variante.VarianteGuid}", variante);
 
-        public async Task CacheWebJob() 
+        public async Task CacheWebJob()
             => await PostAsync("Variante/CacheWebJob", null);
 
         [Obsolete("Funktion 'SaveVarianteAsync()' verwenden")]
-        public async Task SaveAsync(VarianteDTO dto) 
+        public async Task SaveAsync(VarianteDTO dto)
             => await PutAsync("Variante/" + dto.VarianteGuid.ToString(), dto);
     }
 }
