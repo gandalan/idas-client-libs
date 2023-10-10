c17264cfac74d441a9f0be64a67a8b1419f39ca1
René Gründig <rene.gruendig@gandalan.de>
2023-10-10 09:01:14 +0200

    #10086 Refa Rechnung/Sammelrechnung

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/RechnungenWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/RechnungenWebRoutinen.cs
@@ -1,4 +1,4 @@
-﻿using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.Client.Contracts.Contracts;
 using Gandalan.IDAS.WebApi.Client.DTOs.Rechnung;
 using Gandalan.IDAS.WebApi.DTO;
 using System;
@@ -16,8 +16,14 @@ namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen
         public async Task<List<BelegeInfoDTO>> GetAllABFakturierbarAsync()
             => await GetAsync<List<BelegeInfoDTO>>("Rechnungen/GetABFakturierbar");
 
-        public async Task<List<BelegeInfoDTO>> GetAllRechnungenDruckbarAsync()
-            => await GetAsync<List<BelegeInfoDTO>>("Rechnungen/GetNotPrintedRechnungen");
+        public async Task<List<BelegeInfoDTO>> GetAllRechnungenDruckbarAsync(DateTime? printedSince = null)
+            => await GetAsync<List<BelegeInfoDTO>>($"Rechnungen/GetNotPrintedRechnungen?printedSince={printedSince}");
+        public async Task<List<BelegeInfoDTO>> GetAllRechnungenExportierbarAsync(DateTime? exportedSince = null)
+            => await GetAsync<List<BelegeInfoDTO>>($"Rechnungen/GetNotExportedRechnungen?exportedSince={exportedSince}");
+        public async Task SetBelegePrintedAsync(List<Guid> belegListe)
+            => await GetAsync<List<BelegeInfoDTO>>($"Rechnungen/SetBelegePrinted?belegListe={belegListe}");
+        public async Task SetBelegeExportedAsync(List<Guid> belegListe)
+            => await GetAsync<List<BelegeInfoDTO>>($"Rechnungen/SetBelegeExported?belegListe={belegListe}");
 
         public async Task<Dictionary<Guid, Guid>> ErstelleRechnungenAsync(List<BelegartWechselDTO> belegeWechsel) 
             => await PostAsync<Dictionary<Guid, Guid>>("Rechnungen/ErstelleRechnungen", belegeWechsel);
--- Gandalan.IDAS.WebApi.Client/Contracts/DataServices/IRechnungenService.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/DataServices/IRechnungenService.cs
@@ -1,4 +1,4 @@
-﻿using Gandalan.IDAS.WebApi.Client.DTOs.Rechnung;
+using Gandalan.IDAS.WebApi.Client.DTOs.Rechnung;
 using Gandalan.IDAS.WebApi.DTO;
 using System;
 using System.Collections.Generic;
@@ -8,9 +8,11 @@ namespace Gandalan.IDAS.WebApi.Client.Contracts.DataServices
 {
     public interface IRechnungenService
     {
-        Task<Dictionary<Guid, Guid>> ErstelleRechnungenAsync(List<BelegartWechselDTO> belegeWechsel);
         Task<List<BelegeInfoDTO>> GetABFakturierbarListeAsync();
-        Task<List<BelegeInfoDTO>> GetDruckbareRechnungenListeAsync();
-        Task SetRechnungAlsFibuUebergebenAsync(Guid rechnungGuid);
+        Task<List<BelegeInfoDTO>> GetDruckbareRechnungenListeAsync(DateTime? printedSince = null);
+        Task<List<BelegeInfoDTO>> GetExportierbareRechnungenListeAsync(DateTime? printedSince = null);
+        Task SetBelegePrintedAsync(List<Guid> belegListe);
+        Task SetBelegeExportedAsync(List<Guid> belegListe);
+        Task<Dictionary<Guid, Guid>> ErstelleRechnungenAsync(List<BelegartWechselDTO> belegeWechsel);
     }
 }
