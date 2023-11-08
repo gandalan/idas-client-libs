6f144a2634476567310417703ebdc05727ef7269
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2023-11-08 15:27:21 +0100

    Add supress printer selection to ReportAuswahl

--- Gandalan.IDAS.WebApi.Client/Contracts/Report/IReportAuswahl.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Report/IReportAuswahl.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Gandalan.IDAS.Client.Contracts.Contracts.Report;
 
 namespace Gandalan.IDAS.WebApi.Client.Contracts.Report
 {
     public interface IReportAuswahl
     {
-        IReportAuswahlResult Auswaehlen(IEnumerable<IReport> reports, string printerName = null, bool forceDialog = false, bool selectDialogOnly = false);
+        IReportAuswahlResult Auswaehlen(IEnumerable<IReport> reports, string printerName = null, bool forceDialog = false, bool selectDialogOnly = false, bool supressPrinterSelection = false);
     }
 }
