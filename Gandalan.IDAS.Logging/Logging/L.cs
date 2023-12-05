e533d084d7fa326ee2b0d0d348dcbe75709cab73
Adam Stachowicz <saibamenppl@gmail.com>
2023-12-05 15:50:01 +0100

    Print DetailedStringException in logs + new line before exception

--- Gandalan.IDAS.Logging/Logging/L.cs
+++ Gandalan.IDAS.Logging/Logging/L.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.CompilerServices;
 
 namespace Gandalan.IDAS.Logging
@@ -12,12 +12,12 @@ namespace Gandalan.IDAS.Logging
 
         public static void Fehler(Exception ex, string message, LogContext context = LogContext.Allgemein, [CallerMemberName] string sender = null)
         {
-            Fehler($"{message} {ex}", context, sender);
+            Fehler($"{message}{Environment.NewLine}{DetailedStringException(ex)}", context, sender);
         }
 
         public static void Fehler(Exception ex, LogContext context = LogContext.Allgemein, [CallerMemberName] string sender = null)
         {
-            Fehler($"{ex}", context, sender);
+            Fehler($"{DetailedStringException(ex)}", context, sender);
         }
 
         public static void Immer(string message, LogContext context = LogContext.Allgemein, [CallerMemberName] string sender = null)
@@ -38,6 +38,30 @@ namespace Gandalan.IDAS.Logging
         public static void Diagnose(string message, LogContext context = LogContext.Allgemein, [CallerMemberName] string sender = null)
         {
             Logger.GetInstance().Log(message, LogLevel.Diagnose, context, sender);
+        }
+
+        /// <summary>
+        /// Return string with detailed exception - data from RESTRoutinen.AddInfoToException.
+        /// </summary>
+        /// <param name="ex">Exception</param>
+        /// <returns>String with Data and exception</returns>
+        private static string DetailedStringException(Exception ex)
+        {
+            // Use default exception formatting
+            var exString = $"{ex}";
+            if (ex.Data.Contains("URL") && ex.Data.Contains("CallMethod") && ex.Data.Contains("StatusCode"))
+            {
+                var newLine = Environment.NewLine;
+                object response = null;
+                if (ex.Data.Contains("Response"))
+                {
+                    response = $"{newLine}Response: {ex.Data["Response"]}";
+                }
+
+                exString = $"URL: {ex.Data["URL"]}{newLine}CallMethod: {ex.Data["CallMethod"]}{newLine}StatusCode: {ex.Data["StatusCode"]}{response}{newLine}{ex}";
+            }
+
+            return exString;
         }
     }
 }
