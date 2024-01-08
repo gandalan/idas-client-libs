306b9dcec9a4ed14950c9c52dfcf993d1e0f5f10
Denis König <denis.koenig@gandalan.de>
2024-01-08 08:37:44 +0100

    ProduktionsSettings SP_WL_mit_Schraube_150329 "_06" removed

--- Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionsSettingsDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionsSettingsDTO.cs
@@ -50,7 +50,7 @@ namespace Gandalan.IDAS.WebApi.DTO
         public bool PacklisteZusammengefasst { get; set; }
         public bool Drehbandmontage { get; set; } = true;
         public bool STmitLSo_LSu_Mb85mm { get; set; }
-        public bool SP_WL_mit_Schraube_150329_06 { get; set; } = true;
+        public bool SP_WL_mit_Schraube_150329 { get; set; } = true;
         public bool LI_TE_Winkelprofil_mit_Schraube_150329_06 { get; set; } = true;
     }
 }
