2d56cf60ed1883039edc92fc1cc8a8ef18a4cf22
Denis König <denis.koenig@gandalan.de>
2024-01-11 11:01:05 +0100

    ProduktionSettingsDTO STmitLSo_LSu_Mbv_Mass as int

--- Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionsSettingsDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Settings/ProduktionsSettingsDTO.cs
@@ -48,8 +48,9 @@ namespace Gandalan.IDAS.WebApi.DTO
         public bool Gewebeeinzugsarm_122415 { get; set; }
         public bool RO4_143908 { get; set; }
         public bool PacklisteZusammengefasst { get; set; }
+        //public string FarbersetzungsTabelleModel { get; set; }
         public bool Drehbandmontage { get; set; } = true;
-        public bool STmitLSo_LSu_Mb85mm { get; set; }
+        public int STmitLSo_LSu_Mbv_Mass { get; set; } = 50;
         public bool SP_WL_mit_Schraube_150329 { get; set; } = true;
         public bool LI_TE_Winkelprofil_mit_Schraube_150329_06 { get; set; } = true;
     }
