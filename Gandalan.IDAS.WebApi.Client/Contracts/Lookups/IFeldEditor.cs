2558c109053d92b81b7b7f1c03309a8c96e581b8
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2024-02-19 15:52:34 +0100

    Update CanHandle for IFeldEditor

--- Gandalan.IDAS.WebApi.Client/Contracts/Lookups/IFeldEditor.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Lookups/IFeldEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Gandalan.IDAS.WebApi.DTO;
@@ -6,7 +7,9 @@ namespace Gandalan.Client.Contracts.Lookups
 {
     public interface IFeldEditor
     {
+        [Obsolete("Use CanHandle(BelegPositionDTO data, UIEingabeFeldDTO feldDTO = null, string tag = null) instead.")]
         bool CanHandle(BelegPositionDTO data, string tag, bool initGroup = false);
+        bool CanHandle(BelegPositionDTO data, UIEingabeFeldDTO feldDTO = null, string tag = null);
         Task<Dictionary<string, string>> ExecuteAsync(BelegPositionDTO data);
     }
 
