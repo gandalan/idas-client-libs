991f71e4d0cb409f2154bf969aa5fdb2acb56b3e
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2024-02-27 16:10:22 +0100

    Add produktionsFarbText for SerienSaegelisteProfilgruppeDataDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienSaegelisteProfilgruppeDataDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Reports/SerienSaegelisteProfilgruppeDataDTO.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Gandalan.IDAS.WebApi.Data.DTOs.Reports
 {
     public class SerienSaegelisteProfilgruppeDataDTO
     {
         public string Titel { get; set; }
+        public string ProduktionsFarbText { get; set; }
         public string Farbe { get; set; }
         public string FarbeR { get; set; }
         public string FarbeG { get; set; }
