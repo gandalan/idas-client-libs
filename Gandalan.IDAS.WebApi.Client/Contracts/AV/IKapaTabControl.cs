c69f2aeca70e042480c5fdf487402f608fc0d466
René Gründig <rene.gruendig@gandalan.de>
2024-04-09 08:54:04 +0200

    IKapaTabControl erweitert

--- Gandalan.IDAS.WebApi.Client/Contracts/AV/IKapaTabControl.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/AV/IKapaTabControl.cs
@@ -1,8 +1,13 @@
+using System.Threading.Tasks;
+
 namespace Gandalan.IDAS.Client.Contracts.AV
 {
     public interface IKapaTabControl
     {
         string TabCaption { get; }
         int Order { get; }
+        bool DisableZeitraum { get; }
+
+        Task Load();
     }
 }
