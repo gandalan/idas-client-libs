3da39949f8a7b6e1ccce0fff0393f2c1f47ffabb
Philipp Reif <philipp.reif@gandalan.de>
2024-06-13 07:53:14 +0200

    Fix some eslint hints, slow down a few eslint remarks to warning, tell eslint what globalThis is 🙈

--- WebLibs/api/fluentApi.js
+++ WebLibs/api/fluentApi.js
@@ -2,10 +2,8 @@ import { jwtDecode } from "jwt-decode";
 
 const envs = {};
 
-export async function fetchEnv(env = "") 
-{
-    if (!(env in envs))
-    {
+export async function fetchEnv(env = "") {
+    if (!(env in envs)) {
         const hubUrl = `https://connect.idas-cloudservices.net/api/Endpoints?env=${env}`;
         const r = await fetch(hubUrl);
         const data = await r.json();
@@ -14,76 +12,75 @@ export async function fetchEnv(env = "")
     return envs[env];
 }
 
-export function getRefreshToken(token)
-{
+export function getRefreshToken(token) {
     const decoded = jwtDecode(token);
     return decoded.refreshToken;
 }
 
-export function authBuilder()
-{
+export function authBuilder() {
     return {
-        authUrl : "",
-        appToken : "",
-        token : "",
-        refreshToken : "",
+        authUrl: "",
+        appToken: "",
+        token: "",
+        refreshToken: "",
 
-        useAppToken(appToken = "") { this.appToken = appToken; return this; },
-        useBaseUrl(authUrl = "") { this.authUrl = authUrl; return this; },
-        useToken(jwtToken = "") { this.token = jwtToken; return this; },
-        useRefreshToken(storedRefreshToken = "") { this.refreshToken = storedRefreshToken; return this; },
+        useAppToken(appToken = "") {
+            this.appToken = appToken; return this;
+        },
+        useBaseUrl(authUrl = "") {
+            this.authUrl = authUrl; return this;
+        },
+        useToken(jwtToken = "") {
+            this.token = jwtToken; return this;
+        },
+        useRefreshToken(storedRefreshToken = "") {
+            this.refreshToken = storedRefreshToken; return this;
+        },
 
-        async authenticate(username = "", password = "") 
-        {
-            console.log("authenticating:", this.token ? "token set, exp: " + (jwtDecode(this.token).exp-(Date.now()/1000)) : "no token,", this.refreshToken);
+        async authenticate(username = "", password = "") {
+            console.log("authenticating:", this.token ? `token set, exp: ${jwtDecode(this.token).exp - (Date.now() / 1000)}` : "no token,", this.refreshToken);
 
-            if (this.token)
-            {
-                try
-                {
+            if (this.token) {
+                try {
                     const decoded = jwtDecode(this.token);
-                    if (!decoded)
+                    if (!decoded) {
                         throw new Error("Invalid token");
+                    }
 
-                    if (decoded.exp - 60 > Date.now() / 1000)
+                    if (decoded.exp - 60 > Date.now() / 1000) {
                         return this.token;
+                    }
 
-                    if (decoded.refreshToken && !this.refreshToken)
+                    if (decoded.refreshToken && !this.refreshToken) {
                         this.refreshToken = decoded.refreshToken;
-                } 
-                catch (e)
-                {
+                    }
+                } catch (e) {
                     console.error("Error decoding token", e);
                     return null;
                 }
             }
 
-            if (this.refreshToken)
-            {
-                try
-                {   
+            if (this.refreshToken) {
+                try {
                     const temptoken = await this.tryRefreshToken(this.refreshToken);
-                    if (temptoken) 
-                    {
+                    if (temptoken) {
                         this.token = temptoken;
                         this.refreshToken = getRefreshToken(temptoken);
                     }
                 } catch {
-                    // if refresh failed, we'll return the current token 
+                    // if refresh failed, we'll return the current token
                     // - should still be valid for a while
                 }
                 return this.token;
-                
+
             }
 
-            if (username && password)
-            {
+            if (username && password) {
                 const payload = { "Email": username, "Password": password, "AppToken": this.appToken };
-                const res = await fetch(`${this.authUrl}/LoginJwt`, 
+                const res = await fetch(`${this.authUrl}/LoginJwt`,
                     { method: "POST", body: JSON.stringify(payload), headers: { "Content-Type": "application/json" } });
                 const temptoken = await res.json();
-                if (temptoken)
-                {
+                if (temptoken) {
                     this.token = temptoken;
                     return this.token;
                 }
@@ -95,36 +92,50 @@ export function authBuilder()
         async tryRefreshToken(refreshToken = "") {
             const payload = { "Token": refreshToken };
             const res = await fetch(`${this.authUrl}/LoginJwt/Refresh`,
-                { 
-                    method: "PUT", 
-                    body: JSON.stringify(payload), 
-                    headers: { "Content-Type": "application/json" } 
+                {
+                    method: "PUT",
+                    body: JSON.stringify(payload),
+                    headers: { "Content-Type": "application/json" },
                 });
             return res.ok ? await res.json() : null;
-        }
+        },
     }
 }
 
-export function api()
-{
+export function api() {
     return {
-        baseUrl : "",
-        authUrl : "",
-        env : "",
-        appToken : "",
-        storageEntry : "",
-        token : "",
-        refreshToken : "",
-        
-        useEnvironment(env = "") { this.env = env; return this; },
-        useAppToken(newApptoken = "") { this.appToken = newApptoken; return this; },
-        useBaseUrl(url = "") { this.baseUrl = url; return this; },
-        useAuthUrl(url = "") { this.authUrl = url; return this; },
-        useToken(jwtToken = "") { this.token = jwtToken; return this; },
-        useRefreshToken(storedRefreshToken = "") { this.refreshToken = storedRefreshToken; return this; },
-        useGlobalAuth() { this.token = globalThis.idas.token; this.refreshToken = globalThis.idas.refreshToken; return this; },
-        
-        async get(url = "") { 
+        baseUrl: "",
+        authUrl: "",
+        env: "",
+        appToken: "",
+        storageEntry: "",
+        token: "",
+        refreshToken: "",
+
+        useEnvironment(env = "") {
+            this.env = env; return this;
+        },
+        useAppToken(newApptoken = "") {
+            this.appToken = newApptoken; return this;
+        },
+        useBaseUrl(url = "") {
+            this.baseUrl = url; return this;
+        },
+        useAuthUrl(url = "") {
+            this.authUrl = url; return this;
+        },
+        useToken(jwtToken = "") {
+            this.token = jwtToken; return this;
+        },
+        useRefreshToken(storedRefreshToken = "") {
+            this.refreshToken = storedRefreshToken; return this;
+        },
+        useGlobalAuth() {
+            // eslint-disable-next-line no-undef
+            this.token = globalThis.idas.token; this.refreshToken = globalThis.idas.refreshToken; return this;
+        },
+
+        async get(url = "") {
             console.log("get", url);
             await this.ensureAuthenticated();
             const headers = this.token ? { "Authorization": `Bearer ${this.token}` } : {};
@@ -160,13 +171,13 @@ export function api()
             const res = await fetch(`${this.baseUrl}/${url}`, { method: "DELETE", headers });
             return await res.json();
         },
-        
-        async ensureAuthenticated() { 
+
+        async ensureAuthenticated() {
             await this.ensureBaseUrlIsSet();
-            if (!this.token && !this.refreshToken)
+            if (!this.token && !this.refreshToken) {
                 return;
-            try
-            {
+            }
+            try {
                 const temptoken = await authBuilder()
                     .useToken(this.token)
                     .useRefreshToken(this.refreshToken)
@@ -174,35 +185,38 @@ export function api()
                     .useBaseUrl(this.authUrl)
                     .authenticate() || "";
 
-                if (!temptoken)
+                if (!temptoken) {
                     throw new Error("not authenticated");
+                }
 
                 this.token = temptoken;
                 this.refreshToken = getRefreshToken(temptoken);
+                // eslint-disable-next-line no-undef
                 globalThis.idas.token = this.token;
+                // eslint-disable-next-line no-undef
                 globalThis.idas.refreshToken = this.refreshToken;
                 localStorage.setItem("idas-refresh-token", this.refreshToken);
             } catch (e) {
                 this.redirectToLogin();
-            }   
+            }
         },
 
         async ensureBaseUrlIsSet() {
-            if (this.env && (!this.baseUrl || !this.authUrl))
-            {
+            if (this.env && (!this.baseUrl || !this.authUrl)) {
                 const envInfo = await fetchEnv(this.env);
                 this.baseUrl = envInfo.idas;
                 this.authUrl = envInfo.idas;
-            }   
+            }
 
-            if (!this.baseUrl)
+            if (!this.baseUrl) {
                 throw new Error("apiBaseurl not set");
+            }
         },
 
-        redirectToLogin(authPath = "")
-        {
-            if (!window)
+        redirectToLogin(authPath = "") {
+            if (!window) {
                 return;
+            }
 
             const authEndpoint = (new URL(window.location.href).origin) + authPath;
             let authUrlCallback = `${authEndpoint}?r=%target%&j=%jwt%&m=%mandant%`;
@@ -214,12 +228,11 @@ export function api()
             let loginUrl = url.toString();
 
             window.location = loginUrl;
-        }
+        },
     };
 }
 
-export function idasApi(appToken = "") 
-{
+export function idasApi(appToken = "") {
     return api()
         .useGlobalAuth()
         .useAppToken(appToken)
