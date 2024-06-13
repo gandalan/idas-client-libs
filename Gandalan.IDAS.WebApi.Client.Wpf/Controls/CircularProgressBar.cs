495d841c42ae67f53fccf4f538ed369f59b9f4f3
Adam Stachowicz <adam.stachowicz@fingo.net>
2024-06-13 14:40:28 +0200

    Client.Wpf

--- Gandalan.IDAS.WebApi.Client.Wpf/Controls/CircularProgressBar.cs
+++ Gandalan.IDAS.WebApi.Client.Wpf/Controls/CircularProgressBar.cs
@@ -3,47 +3,46 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
 
-namespace Gandalan.IDAS.WebApi.Client.Wpf.Controls
+namespace Gandalan.IDAS.WebApi.Client.Wpf.Controls;
+
+public partial class CircularProgressBar : ProgressBar
 {
-    public partial class CircularProgressBar : ProgressBar
+    public CircularProgressBar()
     {
-        public CircularProgressBar()
-        {
-            ValueChanged += CircularProgressBar_ValueChanged;
-        }
-
-        private static void CircularProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            var bar = sender as CircularProgressBar;
-            var currentAngle = bar.Angle;
-            var targetAngle = e.NewValue / bar.Maximum * 359.999;
-
-            var anim = new DoubleAnimation(currentAngle, targetAngle, TimeSpan.FromMilliseconds(500));
-            bar.BeginAnimation(AngleProperty, anim, HandoffBehavior.SnapshotAndReplace);
-        }
-
-        public double Angle
-        {
-            get => (double)GetValue(AngleProperty);
-            set => SetValue(AngleProperty, value);
-        }
-
-        /// <summary>
-        /// Using a DependencyProperty as the backing store for Angle. This enables animation, styling, binding, etc...
-        /// </summary>
-        public static readonly DependencyProperty AngleProperty =
-            DependencyProperty.Register("Angle", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(0.0));
-
-        public double StrokeThickness
-        {
-            get => (double)GetValue(StrokeThicknessProperty);
-            set => SetValue(StrokeThicknessProperty, value);
-        }
-
-        /// <summary>
-        /// Using a DependencyProperty as the backing store for StrokeThickness. This enables animation, styling, binding, etc...
-        /// </summary>
-        public static readonly DependencyProperty StrokeThicknessProperty =
-            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(10.0));
+        ValueChanged += CircularProgressBar_ValueChanged;
     }
+
+    private static void CircularProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        var bar = sender as CircularProgressBar;
+        var currentAngle = bar.Angle;
+        var targetAngle = e.NewValue / bar.Maximum * 359.999;
+
+        var anim = new DoubleAnimation(currentAngle, targetAngle, TimeSpan.FromMilliseconds(500));
+        bar.BeginAnimation(AngleProperty, anim, HandoffBehavior.SnapshotAndReplace);
+    }
+
+    public double Angle
+    {
+        get => (double)GetValue(AngleProperty);
+        set => SetValue(AngleProperty, value);
+    }
+
+    /// <summary>
+    /// Using a DependencyProperty as the backing store for Angle. This enables animation, styling, binding, etc...
+    /// </summary>
+    public static readonly DependencyProperty AngleProperty =
+        DependencyProperty.Register("Angle", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(0.0));
+
+    public double StrokeThickness
+    {
+        get => (double)GetValue(StrokeThicknessProperty);
+        set => SetValue(StrokeThicknessProperty, value);
+    }
+
+    /// <summary>
+    /// Using a DependencyProperty as the backing store for StrokeThickness. This enables animation, styling, binding, etc...
+    /// </summary>
+    public static readonly DependencyProperty StrokeThicknessProperty =
+        DependencyProperty.Register("StrokeThickness", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(10.0));
 }
--- Gandalan.IDAS.WebApi.Client.Wpf/Dialogs/SetupDialog.xaml.cs
+++ Gandalan.IDAS.WebApi.Client.Wpf/Dialogs/SetupDialog.xaml.cs
@@ -5,64 +5,63 @@ using Gandalan.IDAS.Logging;
 using Gandalan.IDAS.Web;
 using Gandalan.IDAS.WebApi.Client.BusinessRoutinen;
 
-namespace Gandalan.IDAS.WebApi.Client.Wpf.Dialogs
+namespace Gandalan.IDAS.WebApi.Client.Wpf.Dialogs;
+
+/// <summary>
+/// Interaction logic for SetupDialog.xaml
+/// </summary>
+public partial class SetupDialog : Window, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for SetupDialog.xaml
-    /// </summary>
-    public partial class SetupDialog : Window, INotifyPropertyChanged
+    public string Email { get; set; }
+    public IWebApiConfig Settings { get; set; }
+
+    public bool InProgress { get; set; } = false;
+
+    public SetupDialog()
     {
-        public string Email { get; set; }
-        public IWebApiConfig Settings { get; set; }
+        InitializeComponent();
+    }
 
-        public bool InProgress { get; set; } = false;
+    public event PropertyChangedEventHandler PropertyChanged;
 
-        public SetupDialog()
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        DataContext = this;
+    }
+
+    private async void speichernButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Email))
         {
-            InitializeComponent();
+            MessageBox.Show("Geben Sie im Feld \"E-Mail\" Eine gültige E-Mail Adresse ein und klicken Sie diesen Button erneut, um den Produzenten zu aktivieren.",
+                "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        InProgress = true;
+        var service = new AppWebRoutinen(Settings);
+        try
         {
-            DataContext = this;
+            await service.AktiviereMandantAsync(Email.Trim());
+        }
+        catch (ApiException ex)
+        {
+            L.Fehler(ex);
+            return;
+        }
+        finally
+        {
+            InProgress = false;
         }
 
-        private async void speichernButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(Email))
-            {
-                MessageBox.Show("Geben Sie im Feld \"E-Mail\" Eine gültige E-Mail Adresse ein und klicken Sie diesen Button erneut, um den Produzenten zu aktivieren.",
-                    "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+        MessageBox.Show("Produzent wurde erfolgreich eingerichtet.", "Info vom System", MessageBoxButton.OK, MessageBoxImage.Information);
+        DialogResult = true;
+        Close();
+    }
 
-            InProgress = true;
-            var service = new AppWebRoutinen(Settings);
-            try
-            {
-                await service.AktiviereMandantAsync(Email.Trim());
-            }
-            catch (ApiException ex)
-            {
-                L.Fehler(ex);
-                return;
-            }
-            finally
-            {
-                InProgress = false;
-            }
-
-            MessageBox.Show("Produzent wurde erfolgreich eingerichtet.", "Info vom System", MessageBoxButton.OK, MessageBoxImage.Information);
-            DialogResult = true;
-            Close();
-        }
-
-        private void abbrechenButton_Click(object sender, RoutedEventArgs e)
-        {
-            DialogResult = false;
-            Close();
-        }
+    private void abbrechenButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
     }
 }
