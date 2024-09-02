c767646dc61a73416cd5da617f3a37de3d7fa0bc
Adam Stachowicz <adam.stachowicz@fingo.net>
2024-09-02 17:00:25 +0200

    Add ObsoleteAttribute and remarks

--- Gandalan.IDAS.WebApi.Client/Extensions/LinqExtensions.cs
+++ Gandalan.IDAS.WebApi.Client/Extensions/LinqExtensions.cs
@@ -1,4 +1,6 @@
+#if NETFRAMEWORK
 using System.Collections;
+#endif
 using System.Collections.Generic;
 
 namespace System.Linq;
@@ -30,16 +32,24 @@ public static class LinqExtensions
             return ngc.Count != 0;
         }
 
-        using IEnumerator<TSource> e = source.GetEnumerator();
+        using var e = source.GetEnumerator();
         return e.MoveNext();
     }
 #endif
 
 #if NET
+#pragma warning disable CS0618 // Suppress the warning for obsolete code
     /// <inheritdoc cref="Enumerable.Any{TSource}(System.Collections.Generic.IEnumerable{TSource})"/>
+    /// <remarks>
+    /// This method is available for compatibility purposes and should be used only for <c>Gandalan.IDAS.Libs</c> solution. In .NET Core or later versions, the built-in
+    /// <see cref="System.Linq.Enumerable.Any{TSource}(System.Collections.Generic.IEnumerable{TSource})"/>
+    /// method is optimized and should be used instead.
+    /// </remarks>
+    [Obsolete("Use the built-in Enumerable.Any() method in .NET Core or later for better performance.")]
     public static bool AnyOptimized<TSource>(this IEnumerable<TSource> source)
     {
         return source.Any();
     }
+#pragma warning restore CS0618 // Restore the warning for obsolete code
 #endif
 }
