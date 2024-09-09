e233c1644d64792b399850d50f50484d104117b9
René Gründig <rene.gruendig@gandalan.de>
2024-09-09 08:33:39 +0200

    Add UserAuthTokenDTO in ArtosStartSettingsDTO

--- Gandalan.IDAS.WebApi.Client/DTOs/Settings/ArtosStartSettingsDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Settings/ArtosStartSettingsDTO.cs
@@ -1,8 +1,11 @@
+using Gandalan.IDAS.WebApi.DTO;
+
 namespace Gandalan.IDAS.WebApi.Client.DTOs.Settings;
 
 public class ArtosStartSettingsDTO
 {
     public MaterialBedarfLogik MaterialBedarfLogik { get; set; }
+    public UserAuthTokenDTO UserAuthToken { get; set; }
 }
 
 public enum MaterialBedarfLogik
