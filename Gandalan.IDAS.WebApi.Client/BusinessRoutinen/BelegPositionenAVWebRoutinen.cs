b8a16f380518c3f5e01bb90d08f25bbdb53e258a
René Gründig <rene.gruendig@gandalan.de>
2024-09-23 11:39:16 +0200

    BelegPositionenAVWebRoutinen RunAVBerechnungAsync benötigt keine PreCheck für Auth, da Function AppToken generell korrekt sind.

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/BelegPositionenAVWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/BelegPositionenAVWebRoutinen.cs
@@ -11,11 +11,11 @@ public class BelegPositionenAVWebRoutinen : WebRoutinenBase
     public BelegPositionenAVWebRoutinen(IWebApiConfig settings) : base(settings) { }
 
     public async Task RunAVBerechnungAsync(Guid id, long mandantId)
-        => await PostAsync($"BelegPositionenAV/RunAVBerechnung/{id}?mandantId={mandantId}", null);
+        => await PostAsync($"BelegPositionenAV/RunAVBerechnung/{id}?mandantId={mandantId}", null, skipAuth: true);
 
     public async Task CalculateItems()
-        => await PostAsync("BelegPositionenAV/CalculateItems", null);
+        => await PostAsync("BelegPositionenAV/CalculateItems", null, skipAuth: true);
 
     public async Task<List<MandantAndBelegPosGuidDTO>> GetCalculateItemList()
-        => await GetAsync<List<MandantAndBelegPosGuidDTO>>("BelegPositionenAV/GetCalculateItemList");
+        => await GetAsync<List<MandantAndBelegPosGuidDTO>>("BelegPositionenAV/GetCalculateItemList", skipAuth: true);
 }
