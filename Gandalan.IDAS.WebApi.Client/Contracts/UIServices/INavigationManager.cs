3d9361d5db3b4d59b614f3df55e999b848e28c6f
Philipp Reif <philipp.reif@gandalan.de>
2024-11-22 15:11:13 +0100

    add RemoveNavigationItem methods to INavigationManager

--- Gandalan.IDAS.WebApi.Client/Contracts/UIServices/INavigationManager.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/UIServices/INavigationManager.cs
@@ -1,4 +1,5 @@
-using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Gandalan.Client.Contracts.Navigation;
 
@@ -7,6 +8,10 @@ namespace Gandalan.Client.Contracts.UIServices;
 public interface INavigationManager
 {
     Task AddNavigationItem(INavigationItem data);
+    Task RemoveNavigationItem(INavigationItem itemToRemove);
+    Task RemoveNavigationItem(Func<IList<INavigationItem>, INavigationItem> itemToRemove);
+    Task RemoveNavigationItems(Func<IList<INavigationItem>, IEnumerable<INavigationItem>> itemsToRemove);
+
     Task SetGroupOrder(string groupName, int order);
     Task SetGroupIcon(string groupName, object icon);
     Task SetSubGroupOrder(string groupName, string subGroupName, int order);
