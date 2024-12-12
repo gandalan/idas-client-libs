b155e3d302c9a0c24e1f60a04cb4b4a0b1d2ae4a
Adam Stachowicz <adam.stachowicz@fingo.net>
2024-12-12 14:47:48 +0100

    Check if contentType is empty only once

--- WebLibs/api/fluentRestClient.js
+++ WebLibs/api/fluentRestClient.js
@@ -119,10 +119,16 @@ export function restClient() {
         async _parseReponse(res) {
             // check if repsonse is JSON, then return parsed JSON, otherwise return text
             const contentType = res.headers.get("content-type");
-            if (contentType && contentType.includes("application/json"))
-                return await res.json();
-            if (contentType && (contentType.includes("image") || contentType.includes("application/pdf")))
-                return await res.blob();
+            if (contentType) {
+                if (contentType.includes("application/json")) {
+                    return await res.json();
+                }
+
+                if (contentType.includes("image") || contentType.includes("application/pdf")) {
+                    return await res.blob();
+                }
+            }
+
             return await res.text();
         }
     };
