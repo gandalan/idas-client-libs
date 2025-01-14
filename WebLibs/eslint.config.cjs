1b6fbc5e37bf854d7ca80eaa36280d456149666e
Adam Stachowicz <adam.stachowicz@fingo.net>
2025-01-14 15:40:38 +0100

    Update ESLint from 8 to 9

---
+++ WebLibs/eslint.config.cjs
@@ -0,0 +1,51 @@
+import js from "@eslint/js";
+import svelteParser from "svelte-eslint-parser";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+
+module.exports = [
+    // add more generic rule sets here, such as:
+    js.configs.recommended,
+	eslintPluginSvelte.configs.recommended,
+	{
+		files: ["**/*.svelte", "*.svelte"],
+		languageOptions: {
+			parser: svelteParser,
+			requireConfigFile: false,
+			sourceType: "module",
+			ecmaVersion: 2020,
+		},
+		ignorePatterns: [".DS_Store", "node_modules", "/build", "/.svelte-kit", "/package", ".env", ".env.*", "!.env.example", "pnpm-lock.yaml", "package-lock.json", "yarn.lock"],
+		rules: {
+			"yoda": "warn",
+			eqeqeq: ["warn", "smart"],
+			indent: ["warn", 4, { "SwitchCase": 1 }],
+			quotes: ["warn", "double"],
+			semi: ["off", "never"],
+			"no-multi-spaces": ["warn", { ignoreEOLComments: true }],
+			"curly": "off",
+			"comma-spacing": "warn",
+			"brace-style": ["off"],
+			"no-var": "warn",
+			"key-spacing": "warn",
+			"keyword-spacing": "warn",
+			"space-infix-ops": "warn",
+			"arrow-spacing": "warn",
+			"no-trailing-spaces": "off",
+			"space-before-blocks": "warn",
+			"no-unused-vars": ["warn", {
+				"args": "none",
+			}],
+			"no-console": "off",
+			"no-extra-boolean-cast": "off",
+			"no-multiple-empty-lines": ["warn", { "max": 1, "maxBOF": 0 }],
+			"lines-between-class-members": ["warn", "always", { exceptAfterSingleLine: true }],
+			"no-unneeded-ternary": "warn",
+			"no-else-return": ["warn", { "allowElseIf": false }],
+			"array-bracket-newline": ["warn", "consistent"],
+			"eol-last": ["warn", "always"],
+			"prefer-template": "warn",
+			"template-curly-spacing": ["warn", "never"],
+			"comma-dangle": ["off"],
+		}
+	}
+];
