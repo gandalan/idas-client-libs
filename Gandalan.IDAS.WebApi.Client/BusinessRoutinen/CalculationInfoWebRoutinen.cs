0d729ebec7b5111ab9efad9ba4991685ba4d2a32
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2025-02-10 15:11:03 +0100

    #12402 CalculationInfoDTO/Webroutinen for RunAvBerechnung refa

---
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/CalculationInfoWebRoutinen.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Gandalan.IDAS.Client.Contracts.Contracts;
+using Gandalan.IDAS.WebApi.DTO;
+
+namespace Gandalan.IDAS.WebApi.Client.BusinessRoutinen;
+
+public class CalculationInfoWebRoutinen(IWebApiConfig settings) : WebRoutinenBase(settings)
+{
+    private const string CalculationInfoApiRoute = "CalculationInfo";
+
+    public async Task PutCalculationInfoAsync(long mandantId, CalculationInfoDTO calculationInfoDTO)
+        => await PutAsync<CalculationInfoDTO>($"{CalculationInfoApiRoute}?mandantId={mandantId}", calculationInfoDTO);
+
+    public async Task<CalculationInfoDTO> GetCalculationInfoAsync(long mandantId, Guid belegPosGuid)
+        => await GetAsync<CalculationInfoDTO>($"{CalculationInfoApiRoute}?mandantId={mandantId}&belegPosGuid={belegPosGuid}");
+}
---
+++ Gandalan.IDAS.WebApi.Client/DTOs/Belege/CalculationInfoDTO.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Gandalan.IDAS.WebApi.DTO;
+
+public class CalculationInfoDTO
+{
+    public Guid MandantGuid { get; set; }
+    public Guid BelegPositionGuid { get; set; }
+    /// <summary>
+    /// The timestamp when the calculation was requested.
+    /// </summary>
+    public DateTime? CalculationRequestTimestamp { get; set; }
+    /// <summary>
+    /// The timestamp indicating for which request timestamp the calculation was performed.
+    /// </summary>
+    public DateTime? CalculatedForTimestamp { get; set; }
+}
