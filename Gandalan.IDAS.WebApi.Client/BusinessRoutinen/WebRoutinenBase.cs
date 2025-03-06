b25c9dbb4f5a8bd7ce63e0f3da2b9001e8d3ee6b
Philipp Reif <philipp.reif@gandalan.de>
2025-03-06 11:03:51 +0100

    Reintroduce non-async Initialize to WebApiConfigurations
    Properly handle IgnoreOnErrorOccured in WebRoutinenBase

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/WebRoutinenBase.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/WebRoutinenBase.cs
@@ -603,16 +603,20 @@ public class WebRoutinenBase
     /// </summary>
     /// <param name="exception">Exception to check</param>
     /// <exception cref="Exception">Throws <paramref name="exception"/>, if no <see cref="CustomExceptionHandler"/> handles the exception</exception>
-    private static void tryHandleException(Exception exception)
+    private void tryHandleException(Exception exception)
     {
         L.Fehler(exception);
 
-        if (CustomExceptionHandler != null && CustomExceptionHandler.GetInvocationList().Cast<ExceptionHandler>().Any(handler => handler(exception)))
+        if (!IgnoreOnErrorOccured)
         {
-            return;
-        }
 
-        throw exception;
+            if (CustomExceptionHandler != null && CustomExceptionHandler.GetInvocationList().Cast<ExceptionHandler>().Any(handler => handler(exception)))
+            {
+                return;
+            }
+
+            throw exception;
+        }
     }
 
     private ApiException handleWebException(HttpRequestException ex, string url, [CallerMemberName] string sender = null)
