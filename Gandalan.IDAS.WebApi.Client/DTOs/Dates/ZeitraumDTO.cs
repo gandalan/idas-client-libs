45a1b2b4d349e5cb344cf1683a90cdfdfd20318c
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2025-03-12 10:02:47 +0100

    Convert DateTimes to Unspecified in ZeitraumDTO to avoid timezone issues

--- Gandalan.IDAS.WebApi.Client/DTOs/Dates/ZeitraumDTO.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/Dates/ZeitraumDTO.cs
@@ -3,41 +3,35 @@ using System;
 namespace Gandalan.IDAS.WebApi.Client.DTOs.Dates;
 
 /// <summary>
-/// Repräsentiert einen Zeitraum mit einem Start- und Enddatum inklusive Uhrzeit.
-/// Das Startdatum wird auf 00:00 Uhr gesetzt und das Enddatum auf 23:59:59 des angegebenen Enddatums.
+/// Represents a time range with a start and end date including time.
+/// Both start and end date have an unspecified kind to avoid timezone issues.
 /// </summary>
 public class ZeitraumDTO
 {
-    /// <summary>
-    /// Startdatum und -uhrzeit des Zeitraums.
-    /// </summary>
     public DateTime Von { get; set; }
 
-    /// <summary>
-    /// Enddatum und -uhrzeit des Zeitraums.
-    /// </summary>
     public DateTime Bis { get; set; }
 
-    /// <summary>
-    /// Erstellt einen Zeitraum mit dem angegebenen Start- und Enddatum.
-    /// </summary>
-    /// <param name="von">Startdatum und -uhrzeit</param>
-    /// <param name="bis">Enddatum und -uhrzeit</param>
-    /// <exception cref="ArgumentException">Falls das Startdatum nach dem Enddatum liegt</exception>
+    /// <exception cref="ArgumentException">If the start date is after the end date</exception>
     private ZeitraumDTO(DateTime von, DateTime bis)
     {
-        if (von > bis) throw new ArgumentException("Start date must be before end date.");
-        Von = von;
-        Bis = bis;
+        if (von > bis)
+        {
+            throw new ArgumentException("Start date must be before end date.");
+        }
+        // Create Dates with kind set to Unspecified to avoid timezone issues
+        Von = new DateTime(von.Year, von.Month, von.Day, 0, 0, 0);
+        Bis = new DateTime(bis.Year, bis.Month, bis.Day, 23, 59, 59);
     }
 
     /// <summary>
-    /// Erstellt einen Zeitraum, bei dem das Startdatum auf 00:00 Uhr gesetzt wird und das Enddatum auf 23:59:59 des angegebenen Enddatums.
+    /// Creates a time range with the specified start and end date.
+    /// Both start and end date have an unspecified kind to avoid timezone issues.
     /// </summary>
     /// <param name="von">Startdatum (wird auf 00:00:00 gesetzt)</param>
     /// <param name="bis">Enddatum (wird auf 23:59:59 gesetzt)</param>
     public static ZeitraumDTO CreateFullDayRange(DateTime von, DateTime bis)
-        => new(von.Date, bis.Date.AddDays(1).AddSeconds(-1));
+        => new(von.Date, bis.Date);
 
     public override string ToString()
         => $"{Von:yyyy-MM-dd HH:mm:ss} - {Bis:yyyy-MM-dd HH:mm:ss}";
