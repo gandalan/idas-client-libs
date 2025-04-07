dd8909568b4a72a0a17b40fd5a718a0ab43a7220
ewu-nma <niklas.martin@ewu-it.de>
2025-04-07 10:18:03 +0200

    Extend ITagInfoService + fix namespace

--- Gandalan.IDAS.WebApi.Client/Contracts/DataServices/ITagInfoService.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/DataServices/ITagInfoService.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Gandalan.IDAS.WebApi.Client.DTOs.UI;
 
-namespace Gandalan.Client.Common.Data;
+namespace Gandalan.Client.Contracts.DataServices;
 
 public interface ITagInfoService
 {
-    Task<IList<TagInfoDTO>> GetAllAsync(DateTime? changedSince);
+    Task<IList<TagInfoDTO>> GetAllAsync(DateTime? changedSince = null);
+    Task<IList<TagInfoDTO>> GetTagInfoSuggestionsAsync(DateTime? changedSince = null);
+    Task<IList<TagInfoDTO>> GetTagInfoAsync(Guid objectGuid);
+    Task<IDictionary<Guid, IEnumerable<TagInfoDTO>>> GetTagInfoAsync(IEnumerable<Guid> guidList);
+    Task<IDictionary<Guid, IEnumerable<TagInfoDTO>>> GetTagInfoFremdfertigungAsync(IEnumerable<Guid> guidList);
+    Task AddOrUpdateTagInfoAsync(TagInfoDTO dto);
+    Task DeleteTagInfoAsync(TagInfoDTO dto);
 }
