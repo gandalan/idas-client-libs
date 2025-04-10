82db1f003594a040ca9997b2645f08e494560211
René Gründig <rene.gruendig@gandalan.de>
2025-04-10 09:00:49 +0200

    IProduktionsfreigabeItem PositionsGuids zu IList geändert

--- Gandalan.IDAS.WebApi.Client/Contracts/AV/IProduktionsfreigabeItem.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/AV/IProduktionsfreigabeItem.cs
@@ -8,5 +8,5 @@ public interface IProduktionsfreigabeItem
     Guid ABBelegGuid { get; set; }
     Guid VorgangGuid { get; set; }
     long VorgangsNummer { get; set; }
-    List<Guid> PositionsGuids { get; set; }
+    IList<Guid> PositionsGuids { get; set; }
 }
