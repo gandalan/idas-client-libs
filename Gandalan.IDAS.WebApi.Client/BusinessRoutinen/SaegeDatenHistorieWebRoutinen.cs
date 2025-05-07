109b4d3d5c6cdd71277afad395578e07a9237d2b
sra <sebastian.ratzek@ewu-it.de>
2025-05-07 11:20:19 +0200

    #12889 - Anpassungen SaegeDatenHistorie

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/SaegeDatenHistorieWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/SaegeDatenHistorieWebRoutinen.cs
@@ -13,16 +13,28 @@ public class SaegeDatenHistorieWebRoutinen : WebRoutinenBase
     {
     }
 
-    public async Task<SaegeDatenHistorieDTO> GetSaegeDatenHistorieAsync(Guid saegeDatenHistorieGuid, bool includeSaegeDatei = true,
+    public async Task<SaegeDatenHistorieDTO> GetSaegeDatenHistorieAsync(Guid saegeDatenHistorieGuid,
+        bool includeSaegeDatei = true,
         bool includeMeldungen = true)
     {
-        var result = await GetAsync<IList<SaegeDatenHistorieDTO>>($"SaegeDatenHistorie/?saegeDatenHistorieGuid={saegeDatenHistorieGuid}&includeSaegeDatei={includeSaegeDatei}&includeMeldungen={includeMeldungen}");
+        var result = await GetAsync<IList<SaegeDatenHistorieDTO>>(
+            $"SaegeDatenHistorie/?saegeDatenHistorieGuid={saegeDatenHistorieGuid}&includeSaegeDatei={includeSaegeDatei}&includeMeldungen={includeMeldungen}");
         return result.FirstOrDefault();
     }
 
-    public async Task<IList<SaegeDatenHistorieDTO>> GetSaegeDatenHistorieForSerieAsync(Guid serieGuid, bool includeSaegeDatei = false, bool includeMeldungen = false)
-    => await GetAsync<IList<SaegeDatenHistorieDTO>>($"SaegeDatenHistorie/?serieGuid={serieGuid}&includeSaegeDatei={includeSaegeDatei}&includeMeldungen={includeMeldungen}");
+    public async Task<IList<SaegeDatenHistorieDTO>> GetSaegeDatenHistorieForSerieAsync(Guid serieGuid,
+        bool includeSaegeDatei = false, bool includeMeldungen = false)
+        => await GetAsync<IList<SaegeDatenHistorieDTO>>(
+            $"SaegeDatenHistorie/?serieGuid={serieGuid}&includeSaegeDatei={includeSaegeDatei}&includeMeldungen={includeMeldungen}");
 
-    public async Task<IList<SaegeDatenHistorieDTO>> GetSaegeDatenHistorieAsync(DateTime sinceWhen, bool includeSaegeDatei = false, bool includeMeldungen = false)
-        => await GetAsync<IList<SaegeDatenHistorieDTO>>($"SaegeDatenHistorie/?createdSince={sinceWhen:o}&includeSaegeDatei={includeSaegeDatei}&includeMeldungen={includeMeldungen}");
+    public async Task<IList<SaegeDatenHistorieDTO>> GetSaegeDatenHistorieAsync(DateTime sinceWhen,
+        bool includeSaegeDatei = false, bool includeMeldungen = false)
+        => await GetAsync<IList<SaegeDatenHistorieDTO>>(
+            $"SaegeDatenHistorie/?createdSince={sinceWhen:o}&includeSaegeDatei={includeSaegeDatei}&includeMeldungen={includeMeldungen}");
+
+    public async Task<SaegeDatenResultDTO> SaveSaegeDatenHistorieAsync(SaegeDatenHistorieDTO dto)
+        => await PutAsync<SaegeDatenResultDTO>("SaegeDatenHistorie", dto);
+
+    public async Task SaveSaegeDatenHistorieAsync(IList<SaegeDatenHistorieDTO> dtos)
+        => await PutAsync("SaegeDatenHistorie/SaveBulk", dtos);
 }
