ad7621a7289a2b7af59ad267eacffd592fef84ac
Dominik Reiß-Wöltche <dominik.reiss-woeltche@gandalan.de>
2025-05-28 15:36:07 +0200

    Extend AvReportAvPositionenRequestDto

--- Gandalan.IDAS.WebApi.Client/DTOs/AV/Reports/Requests/AvReportAvPositionenRequestDto.cs
+++ Gandalan.IDAS.WebApi.Client/DTOs/AV/Reports/Requests/AvReportAvPositionenRequestDto.cs
@@ -11,10 +11,23 @@ public abstract class AvReportAvBelegPositionRequestDto
 
 public class AvReportSerieAvPositionRequestDto : AvReportAvBelegPositionRequestDto
 {
+    public static AvReportSerieAvPositionRequestDto ForProduktionslieferschein(Guid serieGuid) => new()
+    {
+        SerieGuid = serieGuid,
+        IncludeMaterial = true,
+    };
+
     public Guid SerieGuid { get; set; }
 }
 
 public class AvReportVorgangAvPositionenRequestDto : AvReportAvBelegPositionRequestDto
 {
+    public static AvReportVorgangAvPositionenRequestDto ForProduktionslieferschein(List<Guid> vorgangGuids) => new()
+    {
+        VorgangGuids = vorgangGuids,
+        IncludeMaterial = true,
+    };
+
     public List<Guid> VorgangGuids { get; set; }
+
 }
