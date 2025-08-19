f683ecc49aad64b6809cba23ea9ef80e25d183b3
ewu-nma <niklas.martin@ewu-it.de>
2025-08-19 13:05:30 +0200

    Trim and ToLower UserName from Login and SavedAuthTokens

--- Gandalan.IDAS.WebApi.Client.Wpf/Dialogs/LoginWindow_v2.xaml.cs
+++ Gandalan.IDAS.WebApi.Client.Wpf/Dialogs/LoginWindow_v2.xaml.cs
@@ -97,7 +97,7 @@ public partial class LoginWindow_v2 : Window
         // reflect change to env as from selected dropdown
         _webApiSettings.CopyToThis(_viewModel.ServerEnvironment);
         _webApiSettings.UserAgent = userAgent;
-        _webApiSettings.UserName = _viewModel.UserName;
+        _webApiSettings.UserName = _viewModel.UserName.Trim().ToLower();
         _webApiSettings.Passwort = passwordBox.Password;
         _webApiSettings.AuthToken = null;
 
