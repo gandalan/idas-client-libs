c4e337f559b5e19d0cc42fc003b18dc542eff066
ewu-nma <niklas.martin@ewu-it.de>
2025-09-03 14:09:20 +0200

    #11453 IErrorDialog.ShowError: add isFatal param

--- Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IErrorDialog.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/UIServices/IErrorDialog.cs
@@ -4,5 +4,5 @@ namespace Gandalan.Client.Contracts.UIServices;
 
 public interface IErrorDialog
 {
-    bool ShowError(string title = null, string message = null, Exception exception = null);
+    bool ShowError(string title = null, string message = null, Exception exception = null, bool isFatal = false);
 }
