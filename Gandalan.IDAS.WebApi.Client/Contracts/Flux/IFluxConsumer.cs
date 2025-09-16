84d1522e9a389768a452597b0427d60873b70e4a
Erich Brunner <erich.brunner@gandalan.de>
2025-09-16 14:55:16 +0200

    #13619 Migrated to FluxConsumerExtensions.cs (Part IV)

--- Gandalan.IDAS.WebApi.Client/Contracts/Flux/IFluxConsumer.cs
+++ Gandalan.IDAS.WebApi.Client/Contracts/Flux/IFluxConsumer.cs
@@ -16,7 +16,7 @@ public interface IFluxConsumer
     /// <remarks>
     /// <para>
     /// Ordering applies to transient consumers that are collected and invoked in ascending
-    /// <see cref="ConsumerPriority"/> order. Persistent consumers are invoked in their registration order.
+    /// <see cref="FluxConsumerPriority"/> order. Persistent consumers are invoked in their registration order.
     /// </para>
     /// <para>
     /// Priority influences only the notification ordering within a single store dispatch; it does not
@@ -27,7 +27,7 @@ public interface IFluxConsumer
     /// non-critical or UI-related handlers.
     /// </para>
     /// </remarks>
-    int ConsumerPriority { get; }
+    int FluxConsumerPriority { get; }
 
     /// <summary>
     /// Maps action verbs to asynchronous handler functions that process those actions.
@@ -73,7 +73,7 @@ public interface IFluxConsumer
     /// };
     /// ]]>
     /// </example>
-    IDictionary<string, Func<IFluxStore, IFluxAction, Task>> ConsumerHandlerMap { get; set; }
+    IDictionary<string, Func<IFluxStore, IFluxAction, Task>> FluxConsumerHandlerMap { get; set; }
 
 
     /// <summary>
@@ -93,5 +93,5 @@ public interface IFluxConsumer
     /// and handle targeted actions, facilitating modular and reactive application design.
     /// </para>
     /// </remarks>
-    IFluxStore[] RegisteredStores { get; }
+    IFluxStore[] RegisteredFluxStores { get; }
 }
