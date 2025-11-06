b1c6532a562c304524be5ed6fd6b36228871a951
Konstantin Haupt <konstantin.haupt@gandalan.de>
2025-11-06 14:25:33 +0100

    #14049 add includeAdditionalProperties parameter to VorangListeWebRoutine and IVorgangService

--- Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangListeWebRoutinen.cs
+++ Gandalan.IDAS.WebApi.Client/BusinessRoutinen/VorgangListeWebRoutinen.cs
@@ -11,17 +11,17 @@ public class VorgangListeWebRoutinen : WebRoutinenBase
     {
     }
 
-    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(int jahr, bool includeASP = false)
-        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?jahr={jahr}&includeASP={includeASP}");
+    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(int jahr, bool includeASP = false, bool includeAdditionalProperties = false)
+        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?jahr={jahr}&includeASP={includeASP}&includeAdditionalProperties={includeAdditionalProperties}");
 
-    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(string status, int jahr, bool includeASP = false)
-        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&includeASP={includeASP}");
+    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(string status, int jahr, bool includeASP = false, bool includeAdditionalProperties = false)
+        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&includeASP={includeASP}&includeAdditionalProperties={{includeAdditionalProperties}}\"");
 
-    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(string status, int jahr, DateTime changedSince, bool includeASP = false)
-        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince:o}&includeASP={includeASP}");
+    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(string status, int jahr, DateTime changedSince, bool includeASP = false, bool includeAdditionalProperties = false)
+        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince:o}&includeASP={includeASP}&includeAdditionalProperties={{includeAdditionalProperties}}\"");
 
-    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(int jahr, string status, DateTime changedSince, string art = "", bool includeArchive = false, bool includeOthersData = false, string search = "", bool includeASP = false)
-        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince:o}&art={art}&includeArchive={includeArchive}&includeOthersData={includeOthersData}&search={search}&includeASP={includeASP}");
+    public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(int jahr, string status, DateTime changedSince, string art = "", bool includeArchive = false, bool includeOthersData = false, string search = "", bool includeASP = false, bool includeAdditionalProperties = false)
+        => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?status={status}&jahr={jahr}&changedSince={changedSince:o}&art={art}&includeArchive={includeArchive}&includeOthersData={includeOthersData}&search={search}&includeASP={includeASP}&includeAdditionalProperties={includeAdditionalProperties}");
 
     public async Task<VorgangListItemDTO[]> LadeVorgangsListeAsync(Guid kundeGuid)
         => await GetAsync<VorgangListItemDTO[]>($"VorgangListe/?kundeGuid={kundeGuid}");
