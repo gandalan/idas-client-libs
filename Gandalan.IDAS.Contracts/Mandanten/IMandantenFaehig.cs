e7e84643b4905e109f816803ebd53ee169d5d290
Erich Brunner <erich.brunner@gandalan.de>
2025-11-06 15:36:27 +0100

    #13990 ported EF6 domain logic in EFCore Lib (part V) and IntegrationTests

---
+++ Gandalan.IDAS.Contracts/Mandanten/IMandantenFaehig.cs
@@ -0,0 +1,10 @@
+﻿namespace Gandalan.IDAS.Contracts.Mandanten;
+
+/// <summary>
+/// Interface for entities that support multi-tenancy
+/// Replaces the complex [MandantenFaehig] attribute approach from EF 6
+/// </summary>
+public interface IMandantenFaehig
+{
+    long MandantId { get; set; }
+}
