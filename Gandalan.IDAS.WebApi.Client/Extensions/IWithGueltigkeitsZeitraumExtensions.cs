90a194e3b3d631ec65bd8bf2f9516f5a34c62776
ewu-nma <niklas.martin@ewu-it.de>
2026-01-14 10:06:29 +0100

    Fix compile in rider

--- Gandalan.IDAS.WebApi.Client/Extensions/IWithGueltigkeitsZeitraumExtensions.cs
+++ Gandalan.IDAS.WebApi.Client/Extensions/IWithGueltigkeitsZeitraumExtensions.cs
@@ -4,16 +4,13 @@ namespace System;
 
 public static class IWithGueltigkeitsZeitraumExtensions
 {
-    extension(IWithGueltigkeitsZeitraum gueltig)
-    {
-        public bool IstGueltig(DateTime? referenceDate = null) =>
-            gueltig switch
-            {
-                null => false,
-                { GueltigAb: null, GueltigBis: null } => true,
-                _ => isWithinValidityPeriod(gueltig, (referenceDate ?? DateTime.UtcNow).Date)
-            };
-    }
+    public static bool IstGueltig(this IWithGueltigkeitsZeitraum gueltig, DateTime? referenceDate = null) =>
+        gueltig switch
+        {
+            null => false,
+            { GueltigAb: null, GueltigBis: null } => true,
+            _ => isWithinValidityPeriod(gueltig, (referenceDate ?? DateTime.UtcNow).Date)
+        };
 
     private static bool isWithinValidityPeriod(IWithGueltigkeitsZeitraum gueltig, DateTime refDate) =>
         (gueltig.GueltigAb is null || gueltig.GueltigAb.Value.Date <= refDate)
